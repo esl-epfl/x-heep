839a86445719d10abeada0b29a64086ae625441c
Schiavone Pasquale Davide <schiavon@eslsrv13.intranet.epfl.ch>
2022-04-25 18:29:09 +0200

    add more things to dc script

--- scripts/synthesis/dc_shell/dc_script.tcl
+++ scripts/synthesis/dc_shell/dc_script.tcl
@@ -6,12 +6,33 @@
 set SET_LIBS ${SCRIPT_DIR}/set_libs.tcl
 set CONSTRAINTS ${SCRIPT_DIR}/set_constraints.tcl
 
+remove_design -all
+
 source ${SET_LIBS}
 
 source ${READ_SOURCES}.tcl
 
+elaborate ${TOP_MODULE}
+link
+
+write -f ddc -hierarchy -output precompiled.ddc
+
 source ${CONSTRAINTS}
 
-elaborate ${TOP_MODULE}
+report_clocks > ${REPORT_DIR}/clocks.rpt
+report_timing -loop -max_pahs 10 > ${REPORT_DIR}/timing_loop.rpt
 
-link
+
+compile_ultra -no_autoungroup -no_boundary_optimization -timing -gate_clock
+
+write -f ddc -hierarchy -output compiled.ddc
+
+change_names -rules verilog -hier
+
+write -format verilog -hier -o netlist.v
+
+report_timing -nosplit > ${REPORT_DIR}/timing.rpt
+report_area -hier -nosplit > ${REPORT_DIR}/area.rpt
+report_resources -hierarchy > ${REPORT_DIR}/resources.rpt
+
+
