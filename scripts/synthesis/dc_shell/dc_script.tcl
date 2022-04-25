19f1920259963fe0c38f375487505ec73c41130d
Schiavone Pasquale Davide <schiavon@eslsrv13.intranet.epfl.ch>
2022-04-25 18:00:37 +0200

    improved dc_script

--- scripts/synthesis/dc_shell/dc_script.tcl
+++ scripts/synthesis/dc_shell/dc_script.tcl
@@ -2,7 +2,15 @@
 # Solderpad Hardware License, Version 2.1, see LICENSE.md for details.
 # SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 
+
+set SET_LIBS ${SCRIPT_DIR}/set_libs.tcl
+set CONSTRAINTS ${SCRIPT_DIR}/set_constraints.tcl
+
+source ${SET_LIBS}
+
 source ${READ_SOURCES}.tcl
+
+source ${CONSTRAINTS}
 
 elaborate ${TOP_MODULE}
 
