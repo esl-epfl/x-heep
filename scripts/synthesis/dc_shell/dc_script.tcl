2badd2866f179555caf4e899f696f15b12df701d
davide schiavone <davide@openhwgroup.org>
2022-04-25 14:43:24 +0200

    start adding dc script

---
+++ scripts/synthesis/dc_shell/dc_script.tcl
@@ -0,0 +1,5 @@
+# Copyright 2022 OpenHW Group
+# Solderpad Hardware License, Version 2.1, see LICENSE.md for details.
+# SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+
+source ${READ_SOURCES}.tcl
