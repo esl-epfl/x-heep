59a627cea8dcc87894f592cebcdcc13b77012028
machetti <simone.machetti@epfl.ch>
2022-06-14 15:07:55 +0200

    Done a parse with Verible.

--- hw/fpga/xilinx_core_v_mini_mcu_wrapper.sv
+++ hw/fpga/xilinx_core_v_mini_mcu_wrapper.sv
@@ -7,8 +7,8 @@ module xilinx_core_v_mini_mcu_wrapper #(
     parameter FPU        = 0,
     parameter PULP_ZFINX = 0
 ) (
-    input  logic clk_i,
-    input  logic rst_ni,
+    input logic clk_i,
+    input logic rst_ni,
 
     input  logic jtag_tck_i,
     input  logic jtag_tms_i,
@@ -24,28 +24,28 @@ module xilinx_core_v_mini_mcu_wrapper #(
     output logic exit_valid_o
 );
 
-  logic             clk_gen;
-  logic      [31:0] exit_value;
+  logic        clk_gen;
+  logic [31:0] exit_value;
 
 
   xilinx_clk_wizard_wrapper xilinx_clk_wizard_wrapper_i (
-      .clk_125MHz (clk_i),
-      .clk_out1_0 (clk_gen)
+      .clk_125MHz(clk_i),
+      .clk_out1_0(clk_gen)
   );
 
   core_v_mini_mcu core_v_mini_mcu_i (
-      .clk_i          (clk_gen),
-      .rst_ni         (rst_ni),
-      .jtag_tck_i     (jtag_tck_i),
-      .jtag_tms_i     (jtag_tms_i),
-      .jtag_trst_ni   (jtag_trst_ni),
-      .jtag_tdi_i     (jtag_tdi_i),
-      .jtag_tdo_o     (jtag_tdo_o),
-      .uart_rx_i      (uart_rx_i),
-      .uart_tx_o      (uart_tx_o),
-      .fetch_enable_i (fetch_enable_i),
-      .exit_value_o   (exit_value),
-      .exit_valid_o   (exit_valid_o)
+      .clk_i         (clk_gen),
+      .rst_ni        (rst_ni),
+      .jtag_tck_i    (jtag_tck_i),
+      .jtag_tms_i    (jtag_tms_i),
+      .jtag_trst_ni  (jtag_trst_ni),
+      .jtag_tdi_i    (jtag_tdi_i),
+      .jtag_tdo_o    (jtag_tdo_o),
+      .uart_rx_i     (uart_rx_i),
+      .uart_tx_o     (uart_tx_o),
+      .fetch_enable_i(fetch_enable_i),
+      .exit_value_o  (exit_value),
+      .exit_valid_o  (exit_valid_o)
   );
 
   assign exit_value_o = exit_value[0];
