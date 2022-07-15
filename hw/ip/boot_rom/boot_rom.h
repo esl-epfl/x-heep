5c979106fe0c1083d3e2b644e9dcda11ec753824
Schiavone Pasquale Davide <davide@openhwgroup.org>
2022-07-15 15:03:21 +0200

    rom with nop at the end for prefetching

--- hw/ip/boot_rom/boot_rom.h
+++ hw/ip/boot_rom/boot_rom.h
@@ -1,6 +1,6 @@
 // Auto-generated code
 
-const int reset_vec_size = 10;
+const int reset_vec_size = 12;
 
 uint32_t reset_vec[reset_vec_size] = {
     0x200005b7,
@@ -12,5 +12,7 @@ uint32_t reset_vec[reset_vec_size] = {
     0x000580e7,
     0x400005b7,
     0x22458593,
-    0x000580e7
+    0x000580e7,
+    0x00000013,
+    0x00000013
 };
