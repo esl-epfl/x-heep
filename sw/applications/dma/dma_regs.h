2bb7715db21a8738e5c57a36d4146e7902fb7898
Benoît Denkinger <benoit.denkinger@epfl.ch>
2022-07-29 11:43:29 +0200

    DMA RTL and software update.

--- sw/applications/dma/dma_regs.h
+++ sw/applications/dma/dma_regs.h
@@ -16,13 +16,14 @@ extern "C" {
 // Register width
 #define DMA_PARAM_REG_WIDTH 32
 
-// Input data pointer
+// Input data pointer (word aligned)
 #define DMA_PTR_IN_REG_OFFSET 0x0
 
-// Output data pointer
+// Output data pointer (word aligned)
 #define DMA_PTR_OUT_REG_OFFSET 0x4
 
-// Number of bytes to copy - Once a value is written, the copy starts
+// Number of words (4 bytes) to copy - Once a value is written, the copy
+// starts
 #define DMA_DMA_START_REG_OFFSET 0x8
 
 // Register set to 1 when copy is done
