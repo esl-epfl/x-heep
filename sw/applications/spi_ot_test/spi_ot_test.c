dac1197b0435894f67e1f18896663485d4fb0696
Benoît Denkinger <benoit.denkinger@epfl.ch>
2022-08-18 23:13:37 +0200

    SPI sw example update. TB not executing from flash by default (othwerwise SPI mux is forced to SPIMEMIO).

--- sw/applications/spi_ot_test/spi_ot_test.c
+++ sw/applications/spi_ot_test/spi_ot_test.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 
 #include "core_v_mini_mcu.h"
+#include "soc_ctrl.h"
 #include "spi.h"
 
 
@@ -14,6 +15,13 @@ int main(int argc, char *argv[])
 {
     spi_host_t spi_host;
     spi_host.base_addr = mmio_region_from_addr((uintptr_t)SPI_HOST_START_ADDRESS);
+
+    soc_ctrl_t soc_ctrl;
+    soc_ctrl.base_addr = mmio_region_from_addr((uintptr_t)SOC_CTRL_START_ADDRESS);
+
+
+    // Select SPI host as SPI output
+    soc_ctrl_select_spi_host(&soc_ctrl);
 
     spi_set_enable(&spi_host, true);
 
@@ -37,47 +45,98 @@ int main(int argc, char *argv[])
     volatile uint32_t flash_data = 0x049237ab;
     uint32_t* data_dst = &flash_data;
 
-    // Fill TX FIFO with TX data (Regular old read command + 3B address)
-    const uint8_t cmd = 0x03;
-    spi_write_word(&spi_host, 0x049237ab);
+    // Power up flash
+    const uint8_t powerup_byte_cmd = 0xab;
+    spi_write_word(&spi_host, powerup_byte_cmd);
+    // Load command FIFO with command (1 Byte at single speed)
+    const uint32_t cmd_powerup = spi_create_command((spi_command_t){
+        .len        = 3,
+        .csaat      = false,
+        .speed      = kSpiSpeedStandard,
+        .direction  = kSpiDirTxOnly
+    });
+    spi_set_command(&spi_host, cmd_powerup);
+    spi_wait_for_ready(&spi_host);
+
+    const uint8_t read_byte_cmd = 0x03;
+    // Fill TX FIFO with TX data (read command + 3B address)
+    spi_write_word(&spi_host, read_byte_cmd);
     // Wait for readiness to process commands
     spi_wait_for_ready(&spi_host);
-    // Load command FIFO with command (1 Byte at single speed)
-    const uint32_t cmd_cmd = spi_create_command((spi_command_t){
-        .len        = 0,
-        .csaat      = true,
-        .speed      = kSpiModeStandard,
-        .direction  = kSpiDirTxOnly
-    });
-    spi_set_command(&spi_host, cmd_cmd);
-    spi_wait_for_ready(&spi_host);
-    // Load command FIFO with address (3 Bytes at single speed)
-    const uint32_t cmd_addr = spi_create_command((spi_command_t){
-        .len        = 2,
-        .csaat      = true,
-        .speed      = kSpiModeStandard,
-        .direction  = kSpiDirTxOnly
-    });
-    spi_set_command(&spi_host, cmd_addr);
-    spi_wait_for_ready(&spi_host);
-    // Load command FIFO with read cycles (always 256 cycles)
+    // Load command FIFO with read command (1 Byte at single speed)
     const uint32_t cmd_read = spi_create_command((spi_command_t){
-        .len        = 0,
-        .csaat      = false,
-        .speed      = kSpiModeStandard,
-        .direction  = kSpiDirRxOnly
+        .len        = 3,
+        .csaat      = true,
+        .speed      = kSpiSpeedStandard,
+        .direction  = kSpiDirTxOnly
     });
     spi_set_command(&spi_host, cmd_read);
     spi_wait_for_ready(&spi_host);
+    // // Load command FIFO with address (3 Bytes at single speed)
+    // const uint32_t cmd_addr = spi_create_command((spi_command_t){
+    //     .len        = 2,
+    //     .csaat      = true,
+    //     .speed      = kSpiSpeedStandard,
+    //     .direction  = kSpiDirTxOnly
+    // });
+    // spi_set_command(&spi_host, cmd_addr);
+    // spi_wait_for_ready(&spi_host);
+    // Load command FIFO with read cycles
+    const uint32_t cmd_read_rx = spi_create_command((spi_command_t){
+        .len        = 3,
+        .csaat      = false,
+        .speed      = kSpiSpeedStandard,
+        .direction  = kSpiDirRxOnly
+    });
+    spi_set_command(&spi_host, cmd_read_rx);
+    spi_wait_for_ready(&spi_host);
+    // Wait transaction is finished
+    spi_wait_for_rx_watermark(&spi_host);
+    // Read data from SPI RX FIFO
     spi_read_word(&spi_host, data_dst);
-    // // Read in 32-byte chunks
-    // for (int k = 0; k < 256/32; ++k) {
-    //     spi_wait_for_rx_watermark(&spi_host);
-    //     spi_read_chunk_32B(&spi_host, code_dst + 8*k + 64*i);
-    // }
 
-    printf("Write %x to flash.", 0x049237ab);
-    printf("Read  %x from flash.", *data_dst);
+    // // Fill TX FIFO with TX data (Regular old read command + 3B address)
+    // printf("Write %x to flash.\n", 0x049237ab);
+    // const uint8_t cmd = 0x03;
+    // spi_write_word(&spi, ((0 & 0xFF) << 16) | (0 & 0xFF00) | cmd);
+    // // Wait for readiness to process commands
+    // spi_wait_for_ready(&spi_host);
+    // // Load command FIFO with command (1 Byte at single speed)
+    // const uint32_t cmd_cmd = spi_create_command((spi_command_t){
+    //     .len        = 0,
+    //     .csaat      = true,
+    //     .speed      = kSpiSpeedStandard,
+    //     .direction  = kSpiDirTxOnly
+    // });
+    // spi_set_command(&spi_host, cmd_cmd);
+    // spi_wait_for_ready(&spi_host);
+    // // Load command FIFO with address (3 Bytes at single speed)
+    // const uint32_t cmd_addr = spi_create_command((spi_command_t){
+    //     .len        = 2,
+    //     .csaat      = true,
+    //     .speed      = kSpiSpeedStandard,
+    //     .direction  = kSpiDirTxOnly
+    // });
+    // spi_set_command(&spi_host, cmd_addr);
+    // spi_wait_for_ready(&spi_host);
+    // // Load command FIFO with read cycles (always 256 cycles)
+    // const uint32_t cmd_read = spi_create_command((spi_command_t){
+    //     .len        = 3,
+    //     .csaat      = false,
+    //     .speed      = kSpiSpeedStandard,
+    //     .direction  = kSpiDirRxOnly
+    // });
+    // spi_set_command(&spi_host, cmd_read);
+    // spi_wait_for_ready(&spi_host);
+    // spi_wait_for_rx_watermark(&spi_host);
+    // spi_read_word(&spi_host, data_dst);
+    // // // Read in 32-byte chunks
+    // // for (int k = 0; k < 256/32; ++k) {
+    // //     spi_wait_for_rx_watermark(&spi_host);
+    // //     spi_read_chunk_32B(&spi_host, code_dst + 8*k + 64*i);
+    // // }
+
+    printf("Read %x from flash.\n", *data_dst);
 
     /* write something to stdout */
     printf("hello world!\n");
--- sw/device/lib/drivers/spi/spi.h
+++ sw/device/lib/drivers/spi/spi.h
@@ -38,13 +38,13 @@ typedef struct spi_ch_status {
 } spi_ch_status_t;
 
 /**
-* SPI mode type
+* SPI speed type
 */
 typedef enum {
-    kSpiModeStandard   = 0,
-    kSpiModeDual       = 1,
-    kSpiModeQuad       = 2
-} spi_mode_e;
+    kSpiSpeedStandard   = 0,
+    kSpiSpeedDual       = 1,
+    kSpiSpeedQuad       = 2
+} spi_speed_e;
 
 /**
 * SPI directionality
@@ -76,7 +76,7 @@ typedef struct spi_configopts {
 typedef struct spi_command {
     uint16_t    len         : 9;
     bool        csaat       : 1;
-    spi_mode_e speed       : 2;
+    spi_speed_e speed       : 2;
     spi_dir_e   direction   : 2;
 } spi_command_t;
 
