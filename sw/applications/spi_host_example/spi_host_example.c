0de9f78a57a2f35fc46d15df950673ada7576e3e
Benoît Denkinger <benoit.denkinger@epfl.ch>
2022-08-19 11:48:31 +0200

    File renaming and SPI host example app update.

--- sw/applications/spi_host_example/spi_host_example.c
+++ sw/applications/spi_host_example/spi_host_example.c
@@ -7,74 +7,25 @@
 #include <stdlib.h>
 
 #include "core_v_mini_mcu.h"
-#include "csr.h"
-#include "hart.h"
-#include "handler.h"
-#include "rv_plic.h"
-#include "rv_plic_regs.h"
 #include "soc_ctrl.h"
-#include "spi_host.h"
+#include "spi.h"
 
-// Simple example to check the SPI host peripheral is working. It checks the ram and flash have the same content
-#define DATA_CHUNK_ADDR 0x00008000
-
-int8_t spi_intr_flag;
-
-// Interrupt controller variables
-dif_plic_params_t rv_plic_params;
-dif_plic_t rv_plic;
-dif_plic_result_t plic_res;
-dif_plic_irq_id_t intr_num;
-
-spi_host_t spi_host;
-
-void handler_irq_external(void) {
-    // Claim/clear interrupt
-    plic_res = dif_plic_irq_claim(&rv_plic, 0, &intr_num);
-    if (plic_res == kDifPlicOk && intr_num == SPI_INTR_EVENT) {
-        spi_intr_flag = 1;
-        // Disable SPI interrupt otherwise it stays on until RX FIFO is read
-        dif_plic_irq_set_enabled(&rv_plic, SPI_INTR_EVENT, 0, kDifPlicToggleDisabled);
-    }
-}
-
-uint32_t flash_data[8];
 
 int main(int argc, char *argv[])
 {
-    // spi_host_t spi_host;
+    spi_host_t spi_host;
     spi_host.base_addr = mmio_region_from_addr((uintptr_t)SPI_HOST_START_ADDRESS);
 
     soc_ctrl_t soc_ctrl;
     soc_ctrl.base_addr = mmio_region_from_addr((uintptr_t)SOC_CTRL_START_ADDRESS);
 
-    // Init the PLIC
-    rv_plic_params.base_addr = mmio_region_from_addr((uintptr_t)PLIC_START_ADDRESS);
-    plic_res = dif_plic_init(rv_plic_params, &rv_plic);
-    // Set SPI interrupt priority to 1
-    plic_res = dif_plic_irq_set_priority(&rv_plic, SPI_INTR_EVENT, 1);
-    // Enable SPI interrupt
-    plic_res = dif_plic_irq_set_enabled(&rv_plic, SPI_INTR_EVENT, 0, kDifPlicToggleEnabled);
-    // Enable interrupt on processor side
-    // Enable global interrupt for machine-level interrupts
-    CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
-    // Set mie.MEIE bit to one to enable machine-level external interrupts
-    const uint32_t mask = 1 << 11;// ;
-    CSR_SET_BITS(CSR_REG_MIE, mask);
-    spi_intr_flag = 0;
 
     // Select SPI host as SPI output
     soc_ctrl_select_spi_host(&soc_ctrl);
-    // Enable SPI host device
+
     spi_set_enable(&spi_host, true);
 
-    // Enable event interrupt
-    spi_enable_evt_intr(&spi_host, true);
-    // Enable RX watermark interrupt
-    spi_enable_rxwm_intr(&spi_host, true);
-
-    // Configure chip 0 (flash memory)
-    // Max 50 MHz core SPI clock --> Max 25 MHz SCK
+    // Configure chip 0 (Max 50 MHz core SPI clock --> Max 25 MHz SCK) and select it.
     // Single data IO; keep timing as safe as possible.
     const uint32_t chip_cfg = spi_create_configopts((spi_configopts_t){
         .clkdiv     = 1,
@@ -88,13 +39,14 @@ int main(int argc, char *argv[])
     spi_set_configopts(&spi_host, 0, chip_cfg);
     spi_set_csid(&spi_host, 0);
 
-    // Set RX watermark to 8 word
-    spi_set_rx_watermark(&spi_host, 8);
+    // Set RX watermark to 1 word
+    spi_set_rx_watermark(&spi_host, 1);
 
-    uint32_t *flash_data_ptr = flash_data[0];
+    volatile uint32_t flash_data = 0x049237ab;
+    uint32_t* data_dst = &flash_data;
 
     // Power up flash
-    const uint32_t powerup_byte_cmd = 0xab;
+    const uint8_t powerup_byte_cmd = 0xab;
     spi_write_word(&spi_host, powerup_byte_cmd);
     // Load command FIFO with command (1 Byte at single speed)
     const uint32_t cmd_powerup = spi_create_command((spi_command_t){
@@ -106,38 +58,11 @@ int main(int argc, char *argv[])
     spi_set_command(&spi_host, cmd_powerup);
     spi_wait_for_ready(&spi_host);
 
-    volatile uint32_t data_addr = DATA_CHUNK_ADDR;
-
-    const uint32_t read_byte_cmd = ((DATA_CHUNK_ADDR << 8) | 0x03);
-
+    const uint8_t read_byte_cmd = 0x03;
     // Fill TX FIFO with TX data (read command + 3B address)
     spi_write_word(&spi_host, read_byte_cmd);
     // Wait for readiness to process commands
     spi_wait_for_ready(&spi_host);
-
-    ////////////////////////////////////////////////////////////////
-
-    // // Load command FIFO with read command (1 Byte at single speed)
-    // const uint32_t cmd_read = spi_create_command((spi_command_t){
-    //     .len        = 0,
-    //     .csaat      = true,
-    //     .speed      = kSpiSpeedStandard,
-    //     .direction  = kSpiDirTxOnly
-    // });
-    // spi_set_command(&spi_host, cmd_read);
-    // spi_wait_for_ready(&spi_host);
-    // // Load command FIFO with read address (3 Byte at single speed)
-    // const uint32_t cmd_addr = spi_create_command((spi_command_t){
-    //     .len        = 2,
-    //     .csaat      = true,
-    //     .speed      = kSpiSpeedStandard,
-    //     .direction  = kSpiDirTxOnly
-    // });
-    // spi_set_command(&spi_host, cmd_addr);
-    // spi_wait_for_ready(&spi_host);
-
-    // OR
-
     // Load command FIFO with read command (1 Byte at single speed)
     const uint32_t cmd_read = spi_create_command((spi_command_t){
         .len        = 3,
@@ -147,49 +72,73 @@ int main(int argc, char *argv[])
     });
     spi_set_command(&spi_host, cmd_read);
     spi_wait_for_ready(&spi_host);
-
-    ////////////////////////////////////////////////////////////////
-
+    // // Load command FIFO with address (3 Bytes at single speed)
+    // const uint32_t cmd_addr = spi_create_command((spi_command_t){
+    //     .len        = 2,
+    //     .csaat      = true,
+    //     .speed      = kSpiSpeedStandard,
+    //     .direction  = kSpiDirTxOnly
+    // });
+    // spi_set_command(&spi_host, cmd_addr);
+    // spi_wait_for_ready(&spi_host);
+    // Load command FIFO with read cycles
     const uint32_t cmd_read_rx = spi_create_command((spi_command_t){
-        .len        = 31,
+        .len        = 3,
         .csaat      = false,
         .speed      = kSpiSpeedStandard,
         .direction  = kSpiDirRxOnly
     });
     spi_set_command(&spi_host, cmd_read_rx);
     spi_wait_for_ready(&spi_host);
-
-    // Wait transaction is finished (polling register)
-    // spi_wait_for_rx_watermark(&spi_host);
-    // or wait for SPI interrupt
-    while(spi_intr_flag==0) {
-        wait_for_interrupt();
-    }
-
-    // Complete interrupt
-    plic_res = dif_plic_irq_complete(&rv_plic, 0, &intr_num);
-
+    // Wait transaction is finished
+    spi_wait_for_rx_watermark(&spi_host);
     // Read data from SPI RX FIFO
-    for (int i=0; i<8; i++) {
-        spi_read_word(&spi_host, &flash_data[i]);
-    }
+    spi_read_word(&spi_host, data_dst);
 
-    printf("flash vs ram...\n");
+    // // Fill TX FIFO with TX data (Regular old read command + 3B address)
+    // printf("Write %x to flash.\n", 0x049237ab);
+    // const uint8_t cmd = 0x03;
+    // spi_write_word(&spi, ((0 & 0xFF) << 16) | (0 & 0xFF00) | cmd);
+    // // Wait for readiness to process commands
+    // spi_wait_for_ready(&spi_host);
+    // // Load command FIFO with command (1 Byte at single speed)
+    // const uint32_t cmd_cmd = spi_create_command((spi_command_t){
+    //     .len        = 0,
+    //     .csaat      = true,
+    //     .speed      = kSpiSpeedStandard,
+    //     .direction  = kSpiDirTxOnly
+    // });
+    // spi_set_command(&spi_host, cmd_cmd);
+    // spi_wait_for_ready(&spi_host);
+    // // Load command FIFO with address (3 Bytes at single speed)
+    // const uint32_t cmd_addr = spi_create_command((spi_command_t){
+    //     .len        = 2,
+    //     .csaat      = true,
+    //     .speed      = kSpiSpeedStandard,
+    //     .direction  = kSpiDirTxOnly
+    // });
+    // spi_set_command(&spi_host, cmd_addr);
+    // spi_wait_for_ready(&spi_host);
+    // // Load command FIFO with read cycles (always 256 cycles)
+    // const uint32_t cmd_read = spi_create_command((spi_command_t){
+    //     .len        = 3,
+    //     .csaat      = false,
+    //     .speed      = kSpiSpeedStandard,
+    //     .direction  = kSpiDirRxOnly
+    // });
+    // spi_set_command(&spi_host, cmd_read);
+    // spi_wait_for_ready(&spi_host);
+    // spi_wait_for_rx_watermark(&spi_host);
+    // spi_read_word(&spi_host, data_dst);
+    // // // Read in 32-byte chunks
+    // // for (int k = 0; k < 256/32; ++k) {
+    // //     spi_wait_for_rx_watermark(&spi_host);
+    // //     spi_read_chunk_32B(&spi_host, code_dst + 8*k + 64*i);
+    // // }
 
-    uint32_t errors = 0;
-    uint32_t* ram_ptr = DATA_CHUNK_ADDR;
-    for (int i=0; i<8; i++) {
-        if(flash_data[i] != *ram_ptr) {
-            printf("@%x : %x != %x\n", DATA_CHUNK_ADDR+i*4, flash_data[i], *ram_ptr);
-            errors++;
-        }
-        ram_ptr++;
-    }
+    printf("Read %x from flash.\n", *data_dst);
 
-    if (errors == 0) {
-        printf("success!\n");
-    } else {
-        printf("failure, %d errors!\n", errors);
-    }
+    /* write something to stdout */
+    printf("hello world!\n");
     return EXIT_SUCCESS;
 }
