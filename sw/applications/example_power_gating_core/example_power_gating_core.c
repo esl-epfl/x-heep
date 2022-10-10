37c926a81035de2f0c76288c5abed98ac555bffc
Machetti Simone <machetti@eslsrv13.intranet.epfl.ch>
2022-10-11 00:19:48 +0200

    Updated example_power_gating_core.c app.

--- sw/applications/example_power_gating_core/example_power_gating_core.c
+++ sw/applications/example_power_gating_core/example_power_gating_core.c
@@ -15,7 +15,8 @@
 #include "rv_plic_regs.h"
 #include "gpio.h"
 
-static rv_timer_t timer;
+static rv_timer_t timer_0_1;
+static rv_timer_t timer_2_3;
 static const uint64_t kTickFreqHz = 1000 * 1000; // 1 MHz
 
 static power_manager_t power_manager;
@@ -39,11 +40,15 @@ int main(int argc, char *argv[])
     soc_ctrl.base_addr = mmio_region_from_addr((uintptr_t)SOC_CTRL_START_ADDRESS);
     uint32_t freq_hz = soc_ctrl_get_frequency(&soc_ctrl);
 
-    // Setup rv_timer
-    mmio_region_t timer_reg = mmio_region_from_addr(RV_TIMER_AO_START_ADDRESS);
-    rv_timer_init(timer_reg, (rv_timer_config_t){.hart_count = 2, .comparator_count = 1}, &timer);
+    // Setup rv_timer_0_1
+    mmio_region_t timer_0_1_reg = mmio_region_from_addr(RV_TIMER_AO_START_ADDRESS);
+    rv_timer_init(timer_0_1_reg, (rv_timer_config_t){.hart_count = 2, .comparator_count = 1}, &timer_0_1);
     rv_timer_tick_params_t tick_params;
     rv_timer_approximate_tick_params(freq_hz, kTickFreqHz, &tick_params);
+
+    // Setup rv_timer_2_3
+    mmio_region_t timer_2_3_reg = mmio_region_from_addr(RV_TIMER_START_ADDRESS);
+    rv_timer_init(timer_2_3_reg, (rv_timer_config_t){.hart_count = 2, .comparator_count = 1}, &timer_2_3);
 
     // Setup gpio
     gpio_params_t gpio_params;
@@ -58,10 +63,10 @@ int main(int argc, char *argv[])
     }
 
     // Power-gate and wake-up due to timer_0
-    rv_timer_set_tick_params(&timer, 0, tick_params);
-    rv_timer_irq_enable(&timer, 0, 0, kRvTimerEnabled);
-    rv_timer_arm(&timer, 0, 0, 1024);
-    rv_timer_counter_set_enabled(&timer, 0, kRvTimerEnabled);
+    rv_timer_set_tick_params(&timer_0_1, 0, tick_params);
+    rv_timer_irq_enable(&timer_0_1, 0, 0, kRvTimerEnabled);
+    rv_timer_arm(&timer_0_1, 0, 0, 1024);
+    rv_timer_counter_set_enabled(&timer_0_1, 0, kRvTimerEnabled);
 
     if (power_gate_core(&power_manager, kTimer_0_e, &power_manager_cpu_counters) != kPowerManagerOk_e)
     {
@@ -70,12 +75,36 @@ int main(int argc, char *argv[])
     }
 
     // Power-gate and wake-up due to timer_1
-    rv_timer_set_tick_params(&timer, 1, tick_params);
-    rv_timer_irq_enable(&timer, 1, 0, kRvTimerEnabled);
-    rv_timer_arm(&timer, 1, 0, 1024);
-    rv_timer_counter_set_enabled(&timer, 1, kRvTimerEnabled);
+    rv_timer_set_tick_params(&timer_0_1, 1, tick_params);
+    rv_timer_irq_enable(&timer_0_1, 1, 0, kRvTimerEnabled);
+    rv_timer_arm(&timer_0_1, 1, 0, 1024);
+    rv_timer_counter_set_enabled(&timer_0_1, 1, kRvTimerEnabled);
 
     if (power_gate_core(&power_manager, kTimer_1_e, &power_manager_cpu_counters) != kPowerManagerOk_e)
+    {
+        printf("Error: power manager fail.\n");
+        return EXIT_FAILURE;
+    }
+
+    // Power-gate and wake-up due to timer_2
+    rv_timer_set_tick_params(&timer_2_3, 0, tick_params);
+    rv_timer_irq_enable(&timer_2_3, 0, 0, kRvTimerEnabled);
+    rv_timer_arm(&timer_2_3, 0, 0, 1024);
+    rv_timer_counter_set_enabled(&timer_2_3, 0, kRvTimerEnabled);
+
+    if (power_gate_core(&power_manager, kTimer_2_e, &power_manager_cpu_counters) != kPowerManagerOk_e)
+    {
+        printf("Error: power manager fail.\n");
+        return EXIT_FAILURE;
+    }
+
+    // Power-gate and wake-up due to timer_3
+    rv_timer_set_tick_params(&timer_2_3, 1, tick_params);
+    rv_timer_irq_enable(&timer_2_3, 1, 0, kRvTimerEnabled);
+    rv_timer_arm(&timer_2_3, 1, 0, 1024);
+    rv_timer_counter_set_enabled(&timer_2_3, 1, kRvTimerEnabled);
+
+    if (power_gate_core(&power_manager, kTimer_3_e, &power_manager_cpu_counters) != kPowerManagerOk_e)
     {
         printf("Error: power manager fail.\n");
         return EXIT_FAILURE;
