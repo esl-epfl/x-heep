1c58d0f6e3799b10029afdf4b3ae6370f2bd7555
rurodrig <ruben.rodriguezalvarez@epfl.ch>
2022-10-12 15:58:44 +0200

    SW example code with DMI-SPI write and read to flash

--- sw/applications/spi_host_dma_example/spi_host_dma_example.c
+++ sw/applications/spi_host_dma_example/spi_host_dma_example.c
@@ -19,10 +19,12 @@
 // Un-comment this line to use the SPI FLASH instead of the default SPI
 #define USE_SPI_FLASH
 
-#define COPY_DATA_BYTES 15
+#define COPY_DATA_BYTES 16
 #define SPI_BYTES (4 * (uint32_t)((COPY_DATA_BYTES-1) / 4 + 1)) // Only sends data when an entire word has been received
 
-// int8_t spi_intr_flag;
+#define FLASH_ADDR 0x00008500
+
+int8_t spi_intr_flag;
 int8_t dma_intr_flag;
 
 // Interrupt controller variables
@@ -40,6 +42,24 @@ void handler_irq_external(void) {
     if (plic_res == kDifPlicOk && intr_num == DMA_INTR_DONE) {
         dma_intr_flag = 1;
     }
+    // SPI Interrupt
+    bool intr_spi;
+    #ifndef USE_SPI_FLASH
+        intr_spi = intr_num == SPI_INTR_EVENT;
+    #else
+        intr_spi = intr_num == SPI_FLASH_INTR_EVENT;
+    #endif
+
+    if (plic_res == kDifPlicOk && intr_spi) {
+        spi_intr_flag = 1;
+        // Disable SPI interrupt otherwise it stays on until TX FIFO is writen
+        #ifndef USE_SPI_FLASH
+            dif_plic_irq_set_enabled(&rv_plic, SPI_INTR_EVENT, 0, kDifPlicToggleDisabled);
+        #else
+            dif_plic_irq_set_enabled(&rv_plic, SPI_FLASH_INTR_EVENT, 0, kDifPlicToggleDisabled);
+        #endif
+    }
+    plic_res = dif_plic_irq_complete(&rv_plic, 0, &intr_num);
 }
 
 // Reserve memory array
@@ -54,6 +74,10 @@ int main(int argc, char *argv[])
         spi_host.base_addr = mmio_region_from_addr((uintptr_t)SPI_FLASH_START_ADDRESS);
     #endif
 
+    // dma peripheral structure to access the registers
+    dma_t dma;
+    dma.base_addr = mmio_region_from_addr((uintptr_t)DMA_START_ADDRESS);
+
     soc_ctrl_t soc_ctrl;
     soc_ctrl.base_addr = mmio_region_from_addr((uintptr_t)SOC_CTRL_START_ADDRESS);
 
@@ -64,16 +88,22 @@ int main(int argc, char *argv[])
         printf("Unable to set the PLIC\n;");
         return EXIT_FAILURE;
     }
-
-    // Set DMA priority to 1 (target threshold is by default 0) to trigger an interrupt to the target (the processor)
-    plic_res = dif_plic_irq_set_priority(&rv_plic, DMA_INTR_DONE, 1);
+    // Set SPI interrupt priority to 1
+    #ifndef USE_SPI_FLASH
+        plic_res = dif_plic_irq_set_priority(&rv_plic, SPI_INTR_EVENT, 1);
+    #else
+        plic_res = dif_plic_irq_set_priority(&rv_plic, SPI_FLASH_INTR_EVENT, 1);
+    #endif
     if (plic_res != kDifPlicOk) {
         printf("Unable to set the PLIC priority\n;");
         return EXIT_FAILURE;
     }
-
-    // Enable DMA interrupt
-    plic_res = dif_plic_irq_set_enabled(&rv_plic, DMA_INTR_DONE, 0, kDifPlicToggleEnabled);
+    // Enable SPI interrupt
+    #ifndef USE_SPI_FLASH
+        plic_res = dif_plic_irq_set_enabled(&rv_plic, SPI_INTR_EVENT, 0, kDifPlicToggleEnabled);
+    #else
+        plic_res = dif_plic_irq_set_enabled(&rv_plic, SPI_FLASH_INTR_EVENT, 0, kDifPlicToggleEnabled);
+    #endif
     if (plic_res != kDifPlicOk) {
         printf("Unable to enable the PLIC irq\n;");
         return EXIT_FAILURE;
@@ -85,39 +115,22 @@ int main(int argc, char *argv[])
     // Set mie.MEIE bit to one to enable machine-level external interrupts
     const uint32_t mask = 1 << 11;// ;
     CSR_SET_BITS(CSR_REG_MIE, mask);
-    // spi_intr_flag = 0;
-    dma_intr_flag = 0;
+    spi_intr_flag = 0;
 
     // Select SPI host as SPI output
     soc_ctrl_select_spi_host(&soc_ctrl);
     // Enable SPI host device
     spi_set_enable(&spi_host, true);
 
+    // Enable event interrupt
+    spi_enable_evt_intr(&spi_host, true);
+    // Enable TX empty interrupt
+    spi_enable_txempty_intr(&spi_host, true);
+
     // SPI and SPI_HOST are the same IP so same register map
     uint32_t *fifo_ptr = spi_host.base_addr.base + SPI_HOST_DATA_REG_OFFSET;
 
-    // DMA CONFIGURATION --
-    // dma peripheral structure to access the registers
-    dma_t dma;
-    dma.base_addr = mmio_region_from_addr((uintptr_t)DMA_START_ADDRESS);
-    dma_set_read_ptr_inc(&dma, (uint32_t) 0); // Do not increment address when reading from the SPI (Pop from FIFO)
-    dma_set_read_ptr(&dma, (uint32_t) fifo_ptr); // SPI RX FIFO addr
-    dma_set_write_ptr(&dma, (uint32_t) copy_data); // copy data address
-
-    // Set the correct SPI-DMA mode:
-    // (0) disable
-    // (1) receive from SPI (use SPI_START_ADDRESS for spi_host pointer)
-    // (2) send to SPI (use SPI_START_ADDRESS for spi_host pointer)
-    // (3) receive from SPI FLASH (use SPI_FLASH_START_ADDRESS for spi_host pointer)
-    // (4) send to SPI FLASH (use SPI_FLASH_START_ADDRESS for spi_host pointer)
-    #ifndef USE_SPI_FLASH
-        dma_set_spi_mode(&dma, (uint32_t) 1); // The DMA will wait for the SPI rx FIFO valid signal
-    #else
-        dma_set_spi_mode(&dma, (uint32_t) 3); // The DMA will wait for the SPI rx FIFO valid signal
-    #endif
-
-    // ---------------------
-
+    // -- SPI CONFIGURATION -- 
     // Configure chip 0 (flash memory)
     // Max 50 MHz core SPI clock --> Max 25 MHz SCK
     // Single data IO; keep timing as safe as possible.
@@ -133,26 +146,186 @@ int main(int argc, char *argv[])
     spi_set_configopts(&spi_host, 0, chip_cfg);
     spi_set_csid(&spi_host, 0);
 
+    /////////////// WRITE ////////////////
+
+    const uint32_t cmd_dummy = spi_create_command((spi_command_t){
+        .len        = 2,
+        .csaat      = false,
+        .speed      = kSpiSpeedStandard,
+        .direction  = kSpiDirTxOnly
+    });
+
+    const uint32_t cmd_dummy_read = spi_create_command((spi_command_t){
+        .len        = 2,
+        .csaat      = false,
+        .speed      = kSpiSpeedStandard,
+        .direction  = kSpiDirRxOnly
+    });
+
+    printf("Reset...\n");
+
+    // Reset
+    const uint32_t reset_cmd = 0xFFFFFFFF;
+    spi_write_word(&spi_host, reset_cmd);
+    const uint32_t cmd_reset = spi_create_command((spi_command_t){
+        .len        = 3,
+        .csaat      = false,
+        .speed      = kSpiSpeedStandard,
+        .direction  = kSpiDirTxOnly
+    });
+    spi_set_command(&spi_host, cmd_reset);
+    spi_wait_for_ready(&spi_host);
+    spi_set_rx_watermark(&spi_host,1);
+
+    printf("power up...\n");
+
     // Power up flash
     const uint32_t powerup_byte_cmd = 0xab;
     spi_write_word(&spi_host, powerup_byte_cmd);
-    // Load command FIFO with command (1 Byte at single speed)
     const uint32_t cmd_powerup = spi_create_command((spi_command_t){
-        .len        = 3,
+        .len        = 0,
         .csaat      = false,
         .speed      = kSpiSpeedStandard,
         .direction  = kSpiDirTxOnly
     });
     spi_set_command(&spi_host, cmd_powerup);
     spi_wait_for_ready(&spi_host);
+    printf("Wait...\n");
+    spi_set_command(&spi_host, cmd_dummy);
+    spi_wait_for_ready(&spi_host);
+
+    printf("Write enable...\n");
+
+    // Write enable
+    const uint32_t write_enable_cmd = 0x06;
+    spi_write_word(&spi_host, write_enable_cmd);
+    const uint32_t cmd_write_en = spi_create_command((spi_command_t){
+        .len        = 0,
+        .csaat      = false,
+        .speed      = kSpiSpeedStandard,
+        .direction  = kSpiDirTxOnly
+    });
+    spi_set_command(&spi_host, cmd_write_en);
+    spi_wait_for_ready(&spi_host);
+    printf("Wait...\n");
+    spi_set_command(&spi_host, cmd_dummy);
+    spi_wait_for_ready(&spi_host);
+
+    printf("Write cmd...\n");
+
+    // // Erase memory
+    // const uint32_t write_byte_cmd = 0xD8; // ((FLASH_ADDR << 8) | 0x02);
+    // spi_write_word(&spi_host, write_byte_cmd);
+    // const uint32_t cmd_write = spi_create_command((spi_command_t){
+    //     .len        = 3,
+    //     .csaat      = false,
+    //     .speed      = kSpiSpeedStandard,
+    //     .direction  = kSpiDirTxOnly
+    // });
+    // spi_set_command(&spi_host, cmd_write);
+    // spi_wait_for_ready(&spi_host); 
+
+    // --
+
+    // Write command
+    const uint32_t write_byte_cmd = ((FLASH_ADDR << 8) | 0x02); // Program Page + addr
+    spi_write_word(&spi_host, write_byte_cmd);
+    const uint32_t cmd_write = spi_create_command((spi_command_t){
+        .len        = 3,
+        .csaat      = true,
+        .speed      = kSpiSpeedStandard,
+        .direction  = kSpiDirTxOnly
+    });
+    spi_set_command(&spi_host, cmd_write);
+    spi_wait_for_ready(&spi_host); 
+
+    // -- DMA CONFIGURATION --
+    dma_set_read_ptr_inc(&dma, (uint32_t) 4); // Do not increment address when reading from the SPI (Pop from FIFO)
+    dma_set_write_ptr_inc(&dma, (uint32_t) 0); // Do not increment address when reading from the SPI (Pop from FIFO)
+    dma_set_read_ptr(&dma, (uint32_t) flash_data); // SPI RX FIFO addr
+    dma_set_write_ptr(&dma, (uint32_t) fifo_ptr); // copy data address
+    // Set the correct SPI-DMA mode:
+    // (0) disable
+    // (1) receive from SPI (use SPI_START_ADDRESS for spi_host pointer)
+    // (2) send to SPI (use SPI_START_ADDRESS for spi_host pointer)
+    // (3) receive from SPI FLASH (use SPI_FLASH_START_ADDRESS for spi_host pointer)
+    // (4) send to SPI FLASH (use SPI_FLASH_START_ADDRESS for spi_host pointer)
+    #ifndef USE_SPI_FLASH
+        dma_set_spi_mode(&dma, (uint32_t) 2); // The DMA will wait for the SPI tx FIFO ready signal
+    #else
+        dma_set_spi_mode(&dma, (uint32_t) 4); // The DMA will wait for the SPI tx FIFO ready signal
+    #endif
+    dma_set_cnt_start(&dma, (uint32_t) COPY_DATA_BYTES); // Size of data received by SPI
+
+    const uint32_t cmd_write_tx = spi_create_command((spi_command_t){
+        .len        = SPI_BYTES - 1,
+        .csaat      = false,
+        .speed      = kSpiSpeedStandard,
+        .direction  = kSpiDirTxOnly
+    });
+    spi_set_command(&spi_host, cmd_write_tx);
+    spi_wait_for_ready(&spi_host); 
+
+    // Wait for SPI interrupt
+    printf("Waiting for the SPI interrupt...\n");
+    while(spi_intr_flag == 0) {
+        // spi_wait_for_tx_empty(&spi_host);
+        // spi_intr_flag = 1;
+        wait_for_interrupt();
+    }
+    printf("triggered!\n");
+
+    // Disable TX empty interrupt
+    spi_enable_txempty_intr(&spi_host, false);
+    // Disable event interrupt
+    spi_enable_evt_intr(&spi_host, false);
+
+    // Check status register status waiting for ready
+    bool flash_busy = true;
+    uint8_t flash_resp[4] = {0xff,0xff,0xff,0xff};
+    while(flash_busy){
+        uint32_t flash_cmd = 0x00000005; // [CMD] Read status register
+        spi_write_word(&spi_host, flash_cmd); // Push TX buffer
+        uint32_t spi_bi_cmd = spi_create_command((spi_command_t){
+            .len        = 1,
+            .csaat      = false,
+            .speed      = kSpiSpeedStandard,
+            .direction  = kSpiDirBidir
+        });
+        spi_set_command(&spi_host, spi_bi_cmd);
+        spi_wait_for_ready(&spi_host);
+        spi_wait_for_rx_watermark(&spi_host);
+        spi_read_word(&spi_host, &flash_resp[0]);
+        if ((flash_resp[0] & 0x01) == 0) flash_busy = false;
+    }
+
+    // Fix bug with Bidirectional additional byte
+    spi_set_command(&spi_host, cmd_dummy_read);
+    spi_wait_for_ready(&spi_host);
+    spi_wait_for_rx_watermark(&spi_host);
+    spi_read_word(&spi_host, &flash_resp[0]);
+
+    printf("Reading...\n");
+
+    /////////////// READ /////////////////
+
+    // Set DMA priority to 1 (target threshold is by default 0) to trigger an interrupt to the target (the processor)
+    plic_res = dif_plic_irq_set_priority(&rv_plic, DMA_INTR_DONE, 1);
+    if (plic_res != kDifPlicOk) {
+        printf("Unable to set the PLIC priority\n;");
+        return EXIT_FAILURE;
+    }
+
+    // Enable DMA interrupt
+    plic_res = dif_plic_irq_set_enabled(&rv_plic, DMA_INTR_DONE, 0, kDifPlicToggleEnabled);
+    if (plic_res != kDifPlicOk) {
+        printf("Unable to enable the PLIC irq\n;");
+        return EXIT_FAILURE;
+    }
 
     // The address bytes sent through the SPI to the Flash are in reverse order
-    uint32_t flash_data_addr = flash_data;
-    uint32_t read_byte_cmd = (flash_data_addr >> 16); 
-    read_byte_cmd = (read_byte_cmd | (flash_data_addr & 0xff00));
-    read_byte_cmd = (read_byte_cmd | ((flash_data_addr & 0xff) << 16));
     // Add read command in the first byte sent
-    read_byte_cmd = ((read_byte_cmd << 8) | 0x03);
+    uint32_t read_byte_cmd = ((FLASH_ADDR << 8) | 0x03);
 
     // Fill TX FIFO with TX data (read command + 3B address)
     spi_write_word(&spi_host, read_byte_cmd);
@@ -178,22 +351,47 @@ int main(int argc, char *argv[])
     spi_set_command(&spi_host, cmd_read_rx);
     spi_wait_for_ready(&spi_host);
 
+    // -- DMA CONFIGURATION --
+    dma_set_read_ptr_inc(&dma, (uint32_t) 0); // Do not increment address when reading from the SPI (Pop from FIFO)
+    dma_set_write_ptr_inc(&dma, (uint32_t) 4); // Do not increment address when reading from the SPI (Pop from FIFO)
+    dma_set_read_ptr(&dma, (uint32_t) fifo_ptr); // SPI RX FIFO addr
+    dma_set_write_ptr(&dma, (uint32_t) copy_data); // copy data address
+    // Set the correct SPI-DMA mode:
+    // (0) disable
+    // (1) receive from SPI (use SPI_START_ADDRESS for spi_host pointer)
+    // (2) send to SPI (use SPI_START_ADDRESS for spi_host pointer)
+    // (3) receive from SPI FLASH (use SPI_FLASH_START_ADDRESS for spi_host pointer)
+    // (4) send to SPI FLASH (use SPI_FLASH_START_ADDRESS for spi_host pointer)
+    #ifndef USE_SPI_FLASH
+        dma_set_spi_mode(&dma, (uint32_t) 1); // The DMA will wait for the SPI rx FIFO valid signal
+    #else
+        dma_set_spi_mode(&dma, (uint32_t) 3); // The DMA will wait for the SPI rx FIFO valid signal
+    #endif
+    dma_intr_flag = 0;
     dma_set_cnt_start(&dma, (uint32_t) COPY_DATA_BYTES); // Size of data received by SPI
-
-    ////////////////////////////////////////////////////////////////
 
     // Wait for DMA interrupt
     printf("Waiting for the DMA interrupt...\n");
-    while(dma_intr_flag==0) {
+    while(dma_intr_flag == 0) {
         wait_for_interrupt();
     }
     printf("triggered!\n");
 
-    // Complete interrupt
-    plic_res = dif_plic_irq_complete(&rv_plic, 0, &intr_num);
-    if (plic_res != kDifPlicOk || intr_num != DMA_INTR_DONE) {
-        printf("IRQ complete incorrect\n;");
-    }
+    printf("Power down flash...\n");
+    // Power down flash
+    const uint32_t powerdown_byte_cmd = 0xb9;
+    spi_write_word(&spi_host, powerdown_byte_cmd);
+    const uint32_t cmd_powerdown = spi_create_command((spi_command_t){
+        .len        = 0,
+        .csaat      = false,
+        .speed      = kSpiSpeedStandard,
+        .direction  = kSpiDirTxOnly
+    });
+    spi_set_command(&spi_host, cmd_powerdown);
+    spi_wait_for_ready(&spi_host);
+    printf("Wait...\n");
+    spi_set_command(&spi_host, cmd_dummy);
+    spi_wait_for_ready(&spi_host);
 
     // The data is already in memory -- Check results
     printf("flash vs ram...\n");
