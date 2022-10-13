e949c223cffe1bbb0cdafa3dc4abe72f83736a4f
davide schiavone <davide@openhwgroup.org>
2022-10-13 17:11:19 +0200

    add outfile name

--- util/mcu_gen.py
+++ util/mcu_gen.py
@@ -182,12 +182,16 @@ def string2int(hex_json_string):
     return (hex_json_string.split('x')[1]).split(',')[0]
 
 
-def write_template(tpl_path, outdir, **kwargs):
+def write_template(tpl_path, outdir, outfile, **kwargs):
     if tpl_path:
         tpl_path = pathlib.Path(tpl_path).absolute()
         if tpl_path.exists():
             tpl = Template(filename=str(tpl_path))
-            with open(outdir / tpl_path.with_suffix("").name, "w") as file:
+            if outfile == None:
+                filename = outdir / tpl_path.with_suffix("").name
+            else:
+                filename = outfile
+            with open(filename, "w") as file:
                 code = tpl.render_unicode(**kwargs)
                 code = re_trailws.sub("", code)
                 file.write(code)
@@ -205,10 +209,17 @@ def main():
                         help="A configuration file")
 
     parser.add_argument("--outdir",
-                        "-o",
+                        "-of",
                         type=pathlib.Path,
                         required=True,
                         help="Target directory.")
+
+    parser.add_argument("--outfile",
+                        "-o",
+                        type=pathlib.Path,
+                        required=False,
+                        help="Target filename, if omitted the template basename is taken.")
+
     # Parse arguments.
 
     parser.add_argument("--cpu",
@@ -276,6 +287,8 @@ def main():
 
     outdir = args.outdir
     outdir.mkdir(parents=True, exist_ok=True)
+
+    outfile = args.outfile
 
     if args.cpu != None and args.cpu != '':
         cpu_type = args.cpu
@@ -672,16 +685,16 @@ def main():
     # Package #
     ###########
     if args.pkg_sv != None:
-        write_template(args.pkg_sv, outdir, **kwargs)
+        write_template(args.pkg_sv, outdir, outfile, **kwargs)
 
     if args.header_c != None:
-        write_template(args.header_c, outdir, **kwargs)
+        write_template(args.header_c, outdir, outfile, **kwargs)
 
     if args.tpl_sv != None:
-        write_template(args.tpl_sv, outdir, **kwargs)
+        write_template(args.tpl_sv, outdir, outfile, **kwargs)
 
     if args.linker_script != None:
-        write_template(args.linker_script, outdir, **kwargs)
+        write_template(args.linker_script, outdir, outfile, **kwargs)
 
 
 if __name__ == "__main__":
