81e93e8d29235a4884fcf3d060c9559ee5716ba8
Schiavone Pasquale Davide <davide@openhwgroup.org>
2022-10-29 08:18:54 +0200

    update spi host dma power gate

--- sw/applications/spi_host_dma_power_gate_example/spi_host_dma_power_gate_example.c
+++ sw/applications/spi_host_dma_power_gate_example/spi_host_dma_power_gate_example.c
@@ -20,8 +20,12 @@
 // Un-comment this line to use the SPI FLASH instead of the default SPI
 #define USE_SPI_FLASH
 
-#define COPY_DATA_BYTES 15
+#define COPY_DATA_BYTES 16
 #define SPI_BYTES (4 * (uint32_t)((COPY_DATA_BYTES-1) / 4 + 1)) // Only sends data when an entire word has been received
+
+#define FLASH_CLK_MAX_HZ (133*1000*1000) // In Hz (133 MHz for the flash w25q128jvsim used in the EPFL Programmer)
+
+#define REVERT_24b_ADDR(addr) ((((uint32_t)addr & 0xff0000) >> 16) | ((uint32_t)addr & 0xff00) | (((uint32_t)addr & 0xff) << 16))
 
 int8_t dma_intr_flag;
 spi_host_t spi_host;
@@ -51,11 +55,22 @@ int main(int argc, char *argv[])
 
     soc_ctrl_t soc_ctrl;
     soc_ctrl.base_addr = mmio_region_from_addr((uintptr_t)SOC_CTRL_START_ADDRESS);
+    uint32_t core_clk = soc_ctrl_get_frequency(&soc_ctrl);
+
+    // dma peripheral structure to access the registers
+    dma_t dma;
+    dma.base_addr = mmio_region_from_addr((uintptr_t)DMA_START_ADDRESS);
 
     // Setup power_manager
     mmio_region_t power_manager_reg = mmio_region_from_addr(POWER_MANAGER_START_ADDRESS);
     power_manager.base_addr = power_manager_reg;
     power_manager_counters_t power_manager_cpu_counters;
+    // Init cpu_subsystem's counters
+    if (power_gate_counters_init(&power_manager_cpu_counters, 40, 40, 30, 30, 20, 20, 0, 0) != kPowerManagerOk_e)
+    {
+        printf("Error: power manager fail. Check the reset and powergate counters value\n");
+        return EXIT_FAILURE;
+    }
 
     // Enable interrupt on processor side
     // Enable global interrupt for machine-level interrupts
@@ -73,14 +88,11 @@ int main(int argc, char *argv[])
     // SPI and SPI_HOST are the same IP so same register map
     uint32_t *fifo_ptr = spi_host.base_addr.base + SPI_HOST_DATA_REG_OFFSET;
 
-    // DMA CONFIGURATION --
-    // dma peripheral structure to access the registers
-    dma_t dma;
-    dma.base_addr = mmio_region_from_addr((uintptr_t)DMA_START_ADDRESS);
+     // -- DMA CONFIGURATION --
     dma_set_read_ptr_inc(&dma, (uint32_t) 0); // Do not increment address when reading from the SPI (Pop from FIFO)
+    dma_set_write_ptr_inc(&dma, (uint32_t) 4); // Do not increment address when reading from the SPI (Pop from FIFO)
     dma_set_read_ptr(&dma, (uint32_t) fifo_ptr); // SPI RX FIFO addr
     dma_set_write_ptr(&dma, (uint32_t) copy_data); // copy data address
-
     // Set the correct SPI-DMA mode:
     // (0) disable
     // (1) receive from SPI (use SPI_START_ADDRESS for spi_host pointer)
@@ -88,18 +100,23 @@ int main(int argc, char *argv[])
     // (3) receive from SPI FLASH (use SPI_FLASH_START_ADDRESS for spi_host pointer)
     // (4) send to SPI FLASH (use SPI_FLASH_START_ADDRESS for spi_host pointer)
     #ifndef USE_SPI_FLASH
-        dma_set_spi_mode(&dma, (uint32_t) 1); // The DMA will wait for the SPI rx FIFO valid signal
+        dma_set_spi_mode(&dma, (uint32_t) 1); // The DMA will wait for the SPI RX FIFO valid signal
     #else
-        dma_set_spi_mode(&dma, (uint32_t) 3); // The DMA will wait for the SPI rx FIFO valid signal
+        dma_set_spi_mode(&dma, (uint32_t) 3); // The DMA will wait for the SPI FLASH RX FIFO valid signal
     #endif
 
-    // ---------------------
-
+    // Configure SPI clock
+    // SPI clk freq = 1/2 core clk freq when clk_div = 0
+    // SPI_CLK = CORE_CLK/(2 + 2 * CLK_DIV) <= CLK_MAX => CLK_DIV > (CORE_CLK/CLK_MAX - 2)/2
+    uint16_t clk_div = 0;
+    if(FLASH_CLK_MAX_HZ < core_clk/2){
+        clk_div = (core_clk/(FLASH_CLK_MAX_HZ) - 2)/2; // The value is truncated
+        if (core_clk/(2 + 2 * clk_div) > FLASH_CLK_MAX_HZ) clk_div += 1; // Adjust if the truncation was not 0
+    }
+    // SPI Configuration
     // Configure chip 0 (flash memory)
-    // Max 50 MHz core SPI clock --> Max 25 MHz SCK
-    // Single data IO; keep timing as safe as possible.
     const uint32_t chip_cfg = spi_create_configopts((spi_configopts_t){
-        .clkdiv     = 1,
+        .clkdiv     = clk_div,
         .csnidle    = 0xF,
         .csntrail   = 0xF,
         .csnlead    = 0xF,
@@ -110,26 +127,43 @@ int main(int argc, char *argv[])
     spi_set_configopts(&spi_host, 0, chip_cfg);
     spi_set_csid(&spi_host, 0);
 
+    // The actual implementation of the SPI HOST has HW bugs when sending single bytes per transaction,
+    // This dummy read and write commands are used to send empty commands to the device to fush and discard internal TX and RX words
+    const uint32_t cmd_dummy_write = spi_create_command((spi_command_t){
+        .len        = 1,
+        .csaat      = false,
+        .speed      = kSpiSpeedStandard,
+        .direction  = kSpiDirTxOnly
+    });
+
+    // Reset
+    const uint32_t reset_cmd = 0xFFFFFFFF;
+    spi_write_word(&spi_host, reset_cmd);
+    const uint32_t cmd_reset = spi_create_command((spi_command_t){
+        .len        = 3,
+        .csaat      = false,
+        .speed      = kSpiSpeedStandard,
+        .direction  = kSpiDirTxOnly
+    });
+    spi_set_command(&spi_host, cmd_reset);
+    spi_wait_for_ready(&spi_host);
+
     // Power up flash
     const uint32_t powerup_byte_cmd = 0xab;
     spi_write_word(&spi_host, powerup_byte_cmd);
-    // Load command FIFO with command (1 Byte at single speed)
     const uint32_t cmd_powerup = spi_create_command((spi_command_t){
-        .len        = 3,
+        .len        = 0,
         .csaat      = false,
         .speed      = kSpiSpeedStandard,
         .direction  = kSpiDirTxOnly
     });
     spi_set_command(&spi_host, cmd_powerup);
     spi_wait_for_ready(&spi_host);
+    spi_set_command(&spi_host, cmd_dummy_write);
+    spi_wait_for_ready(&spi_host);
 
     // The address bytes sent through the SPI to the Flash are in reverse order
-    uint32_t flash_data_addr = flash_data;
-    uint32_t read_byte_cmd = (flash_data_addr >> 16);
-    read_byte_cmd = (read_byte_cmd | (flash_data_addr & 0xff00));
-    read_byte_cmd = (read_byte_cmd | ((flash_data_addr & 0xff) << 16));
-    // Add read command in the first byte sent
-    read_byte_cmd = ((read_byte_cmd << 8) | 0x03);
+    int32_t read_byte_cmd = ((REVERT_24b_ADDR(flash_data) << 8) | 0x03);
 
     // Fill TX FIFO with TX data (read command + 3B address)
     spi_write_word(&spi_host, read_byte_cmd);
@@ -155,14 +189,8 @@ int main(int argc, char *argv[])
     spi_set_command(&spi_host, cmd_read_rx);
     spi_wait_for_ready(&spi_host);
 
+    dma_intr_flag = 0;
     dma_set_cnt_start(&dma, (uint32_t) COPY_DATA_BYTES); // Size of data received by SPI
-
-    // Init cpu_subsystem's counters
-    if (power_gate_counters_init(&power_manager_cpu_counters, 40, 40, 30, 30, 20, 20, 0, 0) != kPowerManagerOk_e)
-    {
-        printf("Error: power manager fail. Check the reset and powergate counters value\n");
-        return EXIT_FAILURE;
-    }
 
     // Power gate core and wait for fast DMA interrupt
     CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);
@@ -179,6 +207,21 @@ int main(int argc, char *argv[])
         wait_for_interrupt();
     }
     printf("triggered!\n");
+
+    printf("Power down flash...\n");
+    // Power down flash
+    const uint32_t powerdown_byte_cmd = 0xb9;
+    spi_write_word(&spi_host, powerdown_byte_cmd);
+    const uint32_t cmd_powerdown = spi_create_command((spi_command_t){
+        .len        = 0,
+        .csaat      = false,
+        .speed      = kSpiSpeedStandard,
+        .direction  = kSpiDirTxOnly
+    });
+    spi_set_command(&spi_host, cmd_powerdown);
+    spi_wait_for_ready(&spi_host);
+    spi_set_command(&spi_host, cmd_dummy_write);
+    spi_wait_for_ready(&spi_host);
 
     // The data is already in memory -- Check results
     printf("flash vs ram...\n");
