5f033f76eae208656373183e0bcdb55f55627835
Saverio Nasturzio <s.nasturzio@libero.it>
2022-12-14 21:33:33 +0100

    Added patch to fix verilator linting in tech_cells_generic

--- hw/vendor/pulp_platform_tech_cells_generic/src/rtl/tc_clk.sv
+++ hw/vendor/pulp_platform_tech_cells_generic/src/rtl/tc_clk.sv
@@ -8,6 +8,8 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
+/* verilator lint_off UNUSED */
+
 module tc_clk_and2 (
   input  logic clk0_i,
   input  logic clk1_i,
