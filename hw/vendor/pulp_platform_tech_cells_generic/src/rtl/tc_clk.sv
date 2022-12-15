ee0095f71bce4fd4b0ad0eb9fdc2e147d8fa73d5
Saverio Nasturzio <s.nasturzio@libero.it>
2022-12-15 11:52:37 +0100

    Changed patch to waiver for verilator linter

--- hw/vendor/pulp_platform_tech_cells_generic/src/rtl/tc_clk.sv
+++ hw/vendor/pulp_platform_tech_cells_generic/src/rtl/tc_clk.sv
@@ -8,7 +8,6 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-/* verilator lint_off UNUSED */
 
 module tc_clk_and2 (
   input  logic clk0_i,
