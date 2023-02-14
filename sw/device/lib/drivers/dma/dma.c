db9d814395b7c8db16b2b45dd21cbba2c919d427
Sapriza Juan <juan.sapriza@epfl.ch>
2023-02-14 17:34:06 +0100

    Moved the implementation of the inline functions of the dma hal to its header.

--- sw/device/lib/drivers/dma/dma.c
+++ sw/device/lib/drivers/dma/dma.c
@@ -52,7 +52,7 @@ Description : Original version.
 #include "dma_regs.h"     // Generated
 
 #include "mmio.h"
-#include "core_v_mini_mcu.h" //juan r: ok? Considerar incluir el core-v-mini-mcu.h en el xxx_reg.h
+#include "core_v_mini_mcu.h" // @ToDo: Include this inside _regs.h
 
 
 /****************************************************************************/
@@ -61,7 +61,7 @@ Description : Original version.
 /**                                                                        **/
 /****************************************************************************/
 
-// juan: remove this, is just a placeholder until real assert can be included
+// ToDo: Juan - remove this, is just a placeholder until real assert can be included
 #define assert_me_please(x)
 
 // source: https://stackoverflow.com/questions/600293/how-to-check-if-a-number-is-a-power-of-2
@@ -96,7 +96,7 @@ Description : Original version.
  *  Control Block (CB) of the DMA peripheral. 
  * Has variables and constant necessary/useful for its control. 
  */
-static struct 
+struct 
 {
   /**
    * Control variables for the DMA peripheral 
@@ -106,7 +106,7 @@ static struct
     * The base address for the soc_ctrl hardware registers.
    */
   mmio_region_t baseAdd; 
-} dma_cb; // juan: not a good name, specially if the stefano_structs struct is going to be called ctrl
+} dma_cb; 
 
 
 /****************************************************************************/
@@ -117,85 +117,15 @@ static struct
 
 void dma_init()
 {
-  // juan: This should be deprecated. base address should be obtained from.....
+  // @ToDo: This should be deprecated. base address should be obtained from.....
   dma_cb.baseAdd = mmio_region_from_addr((uintptr_t)DMA_START_ADDRESS);
   // juan: prob some more stuff should go here.
   // e.g. this function could return something
 }
 
-// juan r: is it ok to inline exported function? Put definition with prototype in .h
-void dma_set_src( uint32_t * p_src ) // juan q: what is the {} standard? 
-{                                           // juan q: naming standards? Let put of accuerd. Review whats already done. 
-  assert_me_please( p_src < DMA_MEM_PTR_MAX );        
-  mmio_region_write32(dma_cb.baseAdd, (ptrdiff_t)(DMA_PTR_IN_REG_OFFSET), p_src);
-}                                                   // juan r: can we rename this as src_ptr?? for consistency
-                                                     //               - no, make the change in the function name, and leave a ToDo. 
-                                                    // juan r: in dma_regs.h #19: "// Input data pointer (word aligned)"  what is word aligned here?  
-
-void dma_set_dst( uint32_t * p_dst )
-{
-  assert_me_please( p_dst < DMA_MEM_PTR_MAX );
-  mmio_region_write32(dma_cb.baseAdd, (ptrdiff_t)(DMA_PTR_OUT_REG_OFFSET), p_dst);
-}
-
-void dma_set_cnt_start( uint32_t * p_copySize_du)
-{
-  assert_me_please( p_copySize < DMA_MEM_SIZE_MAX );
-  mmio_region_write32(dma_cb.baseAdd, (ptrdiff_t)(DMA_DMA_START_REG_OFFSET), p_copySize_du);
-}
-
-// juan r: what could be returned? this way the function name could be more explicit.  Es bool ver abajo
-int32_t dma_is_done()
-{
-  return mmio_region_read32(dma_cb.baseAdd, (ptrdiff_t)(DMA_DONE_REG_OFFSET));
-}
-
-/*
- // juan r: "you can set Half-Word data size for the peripheral to access its data register and set Word data size
-           for the Memory to gain in access time. Each two half words will be packed and written in
-           a single access to a Word in the Memory." 
- *          "When FIFO is disabled, it is not allowed to configure different Data Sizes for Source
-           and Destination. In this case the Peripheral Data Size will be applied to both Source
-           and Destination."
- *          STM32 DMA driver: https://github.com/bkht/STM32-HAL-DMA-Interrupt/blob/master/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dma.c
- *          Line 63
- * 
- * This would be a difference in increments? Ya esta respondido
- * 
- * 
- * 
- */
-
-
-void dma_set_src_ptr_inc( uint32_t p_inc )
-{
-  assert_me_please( p_inc <= DMA_MEM_INC_MAX && IS_POWER_OF_2(p_inc) );
-  mmio_region_write32(dma_cb.baseAdd, (ptrdiff_t)(DMA_SRC_PTR_INC_REG_OFFSET), p_inc);
-}
-
-void dma_set_dst_ptr_inc( uint32_t p_inc )
-{
-  assert_me_please( p_inc <= DMA_MEM_INC_MAX && IS_POWER_OF_2(p_inc) );
-  mmio_region_write32(dma_cb.baseAdd, (ptrdiff_t)(DMA_DST_PTR_INC_REG_OFFSET), p_inc);
-}
-
-void dma_set_spi_mode( dma_spi_mode_t p_mode)
-{
-  assert_me_please( p_mode < DMA_SPI_MODE__size );
-  mmio_region_write32(dma_cb.baseAdd, (ptrdiff_t)(DMA_SPI_MODE_REG_OFFSET), (uint32_t) p_mode);
-}
-
-void dma_set_data_type( dma_data_type_t p_type)
-{
-  assert_me_please( p_type < DMA_DATA_TYPE__size );
-  mmio_region_write32(dma_cb.baseAdd, (ptrdiff_t)(DMA_DATA_TYPE_REG_OFFSET), ( uint32_t ) p_type);
-} 
 
 
 
-// juan r: should add a enable/disable interrupt? where is interrupt set?  Yes see notebook
-// juan r: should the DMA be enabled/disabled? no
-// juan r: should get counter with a function? Yes please
 
 /****************************************************************************/
 /**                                                                        **/
@@ -208,23 +138,3 @@ void dma_set_data_type( dma_data_type_t p_type)
 /*                                 EOF                                      */
 /**                                                                        **/
 /****************************************************************************/
-
-
-lib/drv/inc/
-
-
-// juan r: is it ok if I use netbeans? pleantearlo en la biweekly
-// juan r: is there a script to create the new c/h file from template? Ask stefano
-//          it could save preset information (author, institution, copyright, project) 
-// juan r: should we rename this module as drv_dma.c/h?  ask jose in biweekly
-// juan r: way to distinguish between register and region in abbreviation? stay aleert
-
-// juan r: I will need different streams >>> which information is needed for each?  
-
-// juan r: is the way we are hierarchically organizing the project IDE-friendly': Decide which IDE in the biweekly.
-
-
-// juan: for example DMA-peripheral firstly just toogle a GPIO and sniff it in the oscilloscope. 
-//       it could be compared vs. doing the same thing w/ the CPU
-
-// juan : Hacer que done tome solo el bit 0 y retorne bool ??? Esto deber'a ser un toDo. Lo del bool no.  
--- sw/device/lib/drivers/dma/dma.h
+++ sw/device/lib/drivers/dma/dma.h
@@ -102,8 +102,12 @@ typedef enum
 /**                                                                        **/
 /****************************************************************************/
 
+// juan q jose: Would it be smart to make all these functions inline if then I need to extern this control block?
+// Id rather have the cb static'd in the source to guarantee no manipulation.
+// Otherwise, it can be a way of giving control to the user... but hmmmm....
+extern dma_cb;
 
-// juan q: what would this do? 
+// juan q jose: what would this do? 
 // #ifndef _TEMPLATE_C_SRC
 // #endif  /* _TEMPLATE_C_SRC */
 
@@ -129,13 +133,23 @@ void dma_init();
  * @brief Write to the read (source) pointer register of the DMA.
  * @param p_src Any valid memory address.
  */
-inline void dma_set_src( uint32_t * p_src );
+inline void dma_set_src( uint32_t * p_src ) // juan q: what is the {} standard? 
+{                                           // juan q: naming standards? Let put of accuerd. Review whats already done. 
+  assert_me_please( p_src < DMA_MEM_PTR_MAX );        
+  mmio_region_write32(dma_cb.baseAdd, (ptrdiff_t)(DMA_PTR_IN_REG_OFFSET), p_src);
+} // @ToDo: Rename DMA_PTR_IN_*  ->  DMA_SRC_PTR_* 
+
 
 /**
  * @brief Write to the write (destination) pointer register of the DMA.
  * @param p_src Any valid memory address.
  */
-inline void dma_set_dst( uint32_t * p_dst );
+inline void dma_set_dst( uint32_t * p_dst )
+{
+  assert_me_please( p_dst < DMA_MEM_PTR_MAX );
+  mmio_region_write32(dma_cb.baseAdd, (ptrdiff_t)(DMA_PTR_OUT_REG_OFFSET), p_dst);
+} // @ToDo: Rename DMA_PTR_OUT_*  ->  DMA_DST_PTR_*
+
 
 /**
  * @brief Write to the count/start register of the DMA // juan q: review this explanation.
@@ -143,25 +157,70 @@ inline void dma_set_dst( uint32_t * p_dst );
  *                      Number of data units (du) = copy size in bytes / size of the data type.
  *                      e.g. If 16 Half Words (DMA_DATA_TYPE_HALF_WORD) are to be copied then p_copySize_du = 16.   
  */                       // juan q: in bytes or in data units?
-inline void dma_set_cnt_start( uint32_t * p_copySize_du);
+inline void dma_set_cnt_start( uint32_t * p_copySize_du)
+{
+  assert_me_please( p_copySize < DMA_MEM_SIZE_MAX );
+  mmio_region_write32(dma_cb.baseAdd, (ptrdiff_t)(DMA_DMA_START_REG_OFFSET), p_copySize_du);
+} // @ToDo: Rename DMA_DMA_START_*  ->  DMA_CNT_START_*
+// juan q ruben: Does the count keep the amount left to transmit? So if cnt == 0, done == 1 ? 
+
+
+/**
+ * @brief Gets the number of data units not yet transferred. 
+ * @return Number of data units.
+ */
+inline uint32_t dma_get_cnt_du()
+{
+    uint32_t ret = mmio_region_read32(dma_cb.baseAdd, (ptrdiff_t)(DMA_DMA_START_REG_OFFSET));
+    assert_me_please( /* juan q ruben: what could be asserted in this case? */ );
+    return ret;
+}
+
 
 /**
  * @brief Read from the done register of the DMA.
- * @return juan q: no idea what is returned
+ * @return Whether the DMA is working or not. 
+ * @retval 0 - DMA is working.   
+ * @retval 1 - DMA has finished the transmission. DMA is idle. 
  */
-inline int32_t dma_is_done();
+inline uint32_t dma_is_done()
+{
+  uint32_t ret = mmio_region_read32(dma_cb.baseAdd, (ptrdiff_t)(DMA_DONE_REG_OFFSET));
+  assert_me_please( ret == 0 || ret == 1 );
+  return ret;
+  /* In case a return wants to be forced in case of an error, there are 2 alternatives: 
+   *    1) Consider any value != 0 to be a valid 1 using a LOGIC AND: 
+   *            return ( 1 && mmio_region_read32(dma_cb.baseAdd, (ptrdiff_t)(DMA_DONE_REG_OFFSET)));
+   *    2) Consider only the LSB == 1 to be a valid 1 using a BITWISE AND. 
+   *            return ( 1 &  mmio_region_read32(dma_cb.baseAdd, (ptrdiff_t)(DMA_DONE_REG_OFFSET)));
+   * */   
+}   // juan q jose: What to do in the above case
+    // juan q ruben: if cnt== 0 => done == 1 ?? For how long? 
+    // @ToDo: Rename DONE_*  ->  IDLE_*
+// @ToDo: Make register DONE a 1 bit field in hw/ip/dma/data/dma.hjson. Watch out for compatibility/sync with hardware.  
+
 
 /**
  * @brief Write to the source-pointer-increment register of the DMA.
  * @param p_src Any valid memory address.
  */
-inline void dma_set_src_ptr_inc( uint32_t p_inc );
+inline void dma_set_src_ptr_inc( uint32_t p_inc )
+{
+  assert_me_please( p_inc <= DMA_MEM_INC_MAX && IS_POWER_OF_2(p_inc) );
+  mmio_region_write32(dma_cb.baseAdd, (ptrdiff_t)(DMA_SRC_PTR_INC_REG_OFFSET), p_inc);
+}
+
 
 /**
  * @brief Write to the destination-pointer-increment register of the DMA.
  * @param p_src Any valid memory address.
  */
-inline void dma_set_dst_ptr_inc( uint32_t p_inc );
+inline void dma_set_dst_ptr_inc( uint32_t p_inc )
+{
+  assert_me_please( p_inc <= DMA_MEM_INC_MAX && IS_POWER_OF_2(p_inc) );
+  mmio_region_write32(dma_cb.baseAdd, (ptrdiff_t)(DMA_DST_PTR_INC_REG_OFFSET), p_inc);
+}
+
 
 /**
  * @brief Write to the SPI mode register of the DMA.
@@ -172,13 +231,22 @@ inline void dma_set_dst_ptr_inc( uint32_t p_inc );
                 - DMA_SPI_MODE_FLASH_RX   : Receive from SPI Flash.     
                 - DMA_SPI_MODE_FLASH_TX   : Send to SPI Flash. 
  */
-inline void dma_set_spi_mode( dma_spi_mode_t p_mode);
+inline void dma_set_spi_mode( dma_spi_mode_t p_mode)
+{
+  assert_me_please( p_mode < DMA_SPI_MODE__size );
+  mmio_region_write32(dma_cb.baseAdd, (ptrdiff_t)(DMA_SPI_MODE_REG_OFFSET), (uint32_t) p_mode);
+}
+
 
 /**
  * @brief Write to the data type register of the DMA.
  * @param p_src A valid data type. // juan: specify which, they are in dma_regs.h
  */
-inline void dma_set_data_type( dma_data_type_t p_type);
+inline vvoid dma_set_data_type( dma_data_type_t p_type)
+{
+  assert_me_please( p_type < DMA_DATA_TYPE__size );
+  mmio_region_write32(dma_cb.baseAdd, (ptrdiff_t)(DMA_DATA_TYPE_REG_OFFSET), ( uint32_t ) p_type);
+} 
 
 
 #endif /* _DMA_DRIVER_H */
@@ -187,3 +255,20 @@ inline void dma_set_data_type( dma_data_type_t p_type);
 /**                                EOF                                     **/
 /**                                                                        **/
 /****************************************************************************/
+
+                                                 
+                                                     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
