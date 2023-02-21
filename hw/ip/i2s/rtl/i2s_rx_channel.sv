37910874cc4ee1d16c6e395c2b622a0f9a0b1198
Tim Frey <tim.frey@epfl.ch>
2023-02-21 08:38:23 +0100

    renaming rx_channel

--- hw/ip/i2s/rtl/i2s_rx_channel.sv
+++ hw/ip/i2s/rtl/i2s_rx_channel.sv
@@ -31,56 +31,56 @@ module i2s_rx_channel #(
 );
 
 
-  logic                    r_ws_old;
-  logic                    s_ws_edge;
+  logic r_ws_old;
+  logic s_ws_edge;
 
-  logic [ SampleWidth-1:0] r_shiftreg_ch0;
-  logic [ SampleWidth-1:0] s_shiftreg_ch0;
-  logic [ SampleWidth-1:0] r_shiftreg_ch0_shadow;
+  logic [SampleWidth-1:0] r_shiftreg;
+  logic [SampleWidth-1:0] s_shiftreg;
+  logic [SampleWidth-1:0] r_shadow;
 
   logic [CounterWidth-1:0] r_count_bit;
 
-  logic                    s_word_done;
+  logic s_word_done;
 
-  logic                    r_started;
-  logic                    r_started_dly;
+  logic r_started;
+  logic r_started_dly;  // "delayed" by one extra cycle as the ws edge comes one early 
 
-  logic                    r_ch0_valid;
+  logic r_valid;
 
   assign s_ws_edge = ws_i ^ r_ws_old;
 
   assign s_word_done = r_count_bit == cfg_sample_width_i;
 
-  assign fifo_rx_data_o = r_ch0_valid ? r_shiftreg_ch0_shadow : {(SampleWidth) {1'b0}};
-  assign fifo_rx_data_valid_o = r_ch0_valid;
-  assign fifo_rx_err_o = r_ch0_valid & ~fifo_rx_data_ready_i & s_word_done;
+  assign fifo_rx_data_o = r_valid ? r_shadow : {(SampleWidth) {1'b0}};
+  assign fifo_rx_data_valid_o = r_valid;
+  assign fifo_rx_err_o = r_valid & ~fifo_rx_data_ready_i & s_word_done;
 
   always_comb begin : proc_shiftreg
-    s_shiftreg_ch0 = r_shiftreg_ch0;
+    s_shiftreg = r_shiftreg;
     if (cfg_lsb_first_i) begin
-      s_shiftreg_ch0 = {1'b0, r_shiftreg_ch0[SampleWidth-1:1]};
-      s_shiftreg_ch0[cfg_sample_width_i] = sd_i;
+      s_shiftreg = {1'b0, r_shiftreg[SampleWidth-1:1]};
+      s_shiftreg[cfg_sample_width_i] = sd_i;
     end else begin
-      s_shiftreg_ch0 = {r_shiftreg_ch0[SampleWidth-2:0], sd_i};
+      s_shiftreg = {r_shiftreg[SampleWidth-2:0], sd_i};
     end
   end
 
   always_ff @(posedge sck_i, negedge rst_ni) begin
     if (rst_ni == 1'b0) begin
-      r_shiftreg_ch0 <= 'h0;
-      r_shiftreg_ch0_shadow <= 'h0;
-      r_ch0_valid <= 1'b0;
+      r_shiftreg <= 'h0;
+      r_shadow <= 'h0;
+      r_valid <= 1'b0;
     end else begin
       if (r_started_dly) begin
-        r_shiftreg_ch0 <= s_shiftreg_ch0;
+        r_shiftreg <= s_shiftreg;
         if (s_word_done) begin
-          r_shiftreg_ch0_shadow <= r_shiftreg_ch0;
-          r_ch0_valid <= 1'b1;
+          r_shadow <= r_shiftreg;
+          r_valid  <= 1'b1;
         end
       end
-      if (r_ch0_valid) begin
+      if (r_valid) begin
         if (fifo_rx_data_ready_i) begin
-          r_ch0_valid <= 1'b0;
+          r_valid <= 1'b0;
         end
       end
     end
