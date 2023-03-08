b74c67ab454dad55170357c5858eeae8b5111729
Juan Sapriza <juan.sapriza@epfl.ch>
2023-03-08 15:44:42 +0100

    Moved explanation inside external.mk higher up.

--- external.mk
+++ external.mk
@@ -14,25 +14,6 @@
 
 # Author: Juan Sapriza (juan.sapriza@epfl.ch)
 
-MAKE	= make
-
-# Furthermore, a variable HEEP_DIR with the relative path between that directory and the X-HEEP base directory (where this file is lcoated) needs to be exported. 
-# This will compute the opposite relative path (from the X-HEEP base directory to where this file is included).
-HEEP_REL_PATH = $(shell realpath --relative-to=$(HEEP_DIR) ".")
-
-# This assumes that you are including this file from a directory where you have a "sw" directory.
-# When this path is provided to sw/Makefile, it will be the relative path from there to the uppermost sw directory. 
-SW_TO_SW_REL_PATH = ../$(HEEP_REL_PATH)/sw/
-
-# If a custom path wants to be provided (i.e. software sources will not be inside a sw directory), it should be specified by setting this variable when calling make. 
-SOURCE ?= $(SW_TO_SW_REL_PATH)
-
-# Any target that was not present on the uppermost Makefile (the one in which this file is included) will be passed to the X-HEEP Makefile. 
-%:
-	@echo Your relative sources path is $(SOURCE)
-	$(MAKE) -C $(HEEP_DIR) $(MAKECMDGOALS) SOURCE=$(SOURCE)
-    
-
 # Example Makefile that uses this external.mk file to access X-HEEP targets and build software from directories outside X-HEEP.
 # ---------------------------------------------------------------------
 # MAKE     = make
@@ -51,3 +32,21 @@ SOURCE ?= $(SW_TO_SW_REL_PATH)
 #
 # It is crucial that the 'include' directive is located after all your rules as it will catch any target that was not defined in your Makefile. 
 
+
+MAKE	= make
+
+# Furthermore, a variable HEEP_DIR with the relative path between that directory and the X-HEEP base directory (where this file is lcoated) needs to be exported. 
+# This will compute the opposite relative path (from the X-HEEP base directory to where this file is included).
+HEEP_REL_PATH = $(shell realpath --relative-to=$(HEEP_DIR) ".")
+
+# This assumes that you are including this file from a directory where you have a "sw" directory.
+# When this path is provided to sw/Makefile, it will be the relative path from there to the uppermost sw directory. 
+SW_TO_SW_REL_PATH = ../$(HEEP_REL_PATH)/sw/
+
+# If a custom path wants to be provided (i.e. software sources will not be inside a sw directory), it should be specified by setting this variable when calling make. 
+SOURCE ?= $(SW_TO_SW_REL_PATH)
+
+# Any target that was not present on the uppermost Makefile (the one in which this file is included) will be passed to the X-HEEP Makefile. 
+%:
+	@echo Your relative sources path is $(SOURCE)
+	$(MAKE) -C $(HEEP_DIR) $(MAKECMDGOALS) SOURCE=$(SOURCE)
