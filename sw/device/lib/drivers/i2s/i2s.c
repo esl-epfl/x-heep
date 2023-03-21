1e4c544ae9f1b98f829f0499188b00be14177889
Tim Frey <tim.frey@epfl.ch>
2023-03-21 10:26:04 +0100

    fix order

--- sw/device/lib/drivers/i2s/i2s.c
+++ sw/device/lib/drivers/i2s/i2s.c
@@ -9,10 +9,11 @@
 #include "i2s_structs.h"
 
 void i2s_setup(bool enable, bool gen_clk, uint16_t div_value, bool intr_en, i2s_datawidth_t data_width, uint32_t intr_reach_count) {
+  i2s_peri->CLKDIVIDX = div_value;
+  i2s_peri->Watermark = intr_reach_count;
+
   i2s_peri->cfg = (enable << I2S_CFG_EN_BIT)
   + (gen_clk << I2S_CFG_GEN_CLK_WS_BIT)
   + (intr_en << I2S_CFG_INTR_EN_BIT)
   + (data_width << I2S_CFG_DATA_WIDTH_OFFSET);
-  i2s_peri->CLKDIVIDX = div_value;
-  i2s_peri->Watermark = intr_reach_count;
 }
