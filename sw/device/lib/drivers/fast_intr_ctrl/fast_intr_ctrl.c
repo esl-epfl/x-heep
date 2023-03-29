4036a7913e0998dee2501d030e0be03b0e4c12b2
Taji Hossein <taji@eslpc2.intranet.epfl.ch>
2023-03-29 11:01:01 +0200

    adding the FIC handlers, clearing the bits while intr recieved, and providing weak functions for peripherals using FIC

--- sw/device/lib/drivers/fast_intr_ctrl/fast_intr_ctrl.c
+++ sw/device/lib/drivers/fast_intr_ctrl/fast_intr_ctrl.c
@@ -38,6 +38,11 @@
 #include "fast_intr_ctrl_regs.h"  // Generated.
 #include "fast_intr_ctrl_structs.h"
 
+/* interrupts */
+#include "handler.h"
+#include "csr.h"
+#include "stdasm.h"
+
 /****************************************************************************/
 /**                                                                        **/
 /*                        DEFINITIONS AND MACROS                            */
@@ -81,12 +86,193 @@ fast_intr_ctrl_result_t clear_fast_interrupt(fast_intr_ctrl_fast_interrupt_t fas
     return kFastIntrCtrlOk_e;
 }
 
+__attribute__((weak)) void fic_irq_timer_1(void){
+    volatile uint8_t i;
+    i++;
+}
+
+__attribute__((weak)) void fic_irq_timer_2(void){
+    volatile uint8_t i;
+    i++;
+}
+
+__attribute__((weak)) void fic_irq_timer_3(void){
+    volatile uint8_t i;
+    i++;
+}
+
+__attribute__((weak)) void fic_irq_dma(void){
+    volatile uint8_t i;
+    i++;
+}
+
+__attribute__((weak)) void fic_irq_spi(void){
+    volatile uint8_t i;
+    i++;
+}
+
+__attribute__((weak)) void fic_irq_spi_flash(void){
+    volatile uint8_t i;
+    i++;
+}
+
+__attribute__((weak)) void fic_irq_gpio_0(void){
+    volatile uint8_t i;
+    i++;
+}
+
+__attribute__((weak)) void fic_irq_gpio_1(void){
+    volatile uint8_t i;
+    i++;
+}
+
+__attribute__((weak)) void fic_irq_gpio_2(void){
+    volatile uint8_t i;
+    i++;
+}
+
+__attribute__((weak)) void fic_irq_gpio_3(void){
+    volatile uint8_t i;
+    i++;
+}
+
+__attribute__((weak)) void fic_irq_gpio_4(void){
+    volatile uint8_t i;
+    i++;
+}
+
+__attribute__((weak)) void fic_irq_gpio_5(void){
+    volatile uint8_t i;
+    i++;
+}
+
+__attribute__((weak)) void fic_irq_gpio_6(void){
+    volatile uint8_t i;
+    i++;
+}
+
+__attribute__((weak)) void fic_irq_gpio_7(void){
+    volatile uint8_t i;
+    i++;
+}
+
 /****************************************************************************/
 /**                                                                        **/
 /*                            LOCAL FUNCTIONS                               */
 /**                                                                        **/
 /****************************************************************************/
 
+void handler_irq_fast_timer_1(void)
+{
+    // The interrupt is cleared.
+    clear_fast_interrupt(kTimer_1_fic_e);
+    // call the weak fic handler
+    fic_irq_timer_1();
+}
+
+void handler_irq_fast_timer_2(void)
+{
+    // The interrupt is cleared.
+    clear_fast_interrupt(kTimer_2_fic_e);
+    // call the weak fic handler
+    fic_irq_timer_2();
+}
+
+void handler_irq_fast_timer_3(void)
+{
+    // The interrupt is cleared.
+    clear_fast_interrupt(kTimer_3_fic_e);
+    // call the weak fic handler
+    fic_irq_timer_3();
+}
+
+void handler_irq_fast_dma(void)
+{
+    // The interrupt is cleared.
+    clear_fast_interrupt(kDma_fic_e);
+    // call the weak fic handler
+    fic_irq_dma();
+}
+
+void handler_irq_fast_spi(void)
+{
+    // The interrupt is cleared.
+    clear_fast_interrupt(kSpi_fic_e);
+    // call the weak fic handler
+    fic_irq_spi();
+}
+
+void handler_irq_fast_spi_flash(void)
+{
+    // The interrupt is cleared.
+    clear_fast_interrupt(kSpiFlash_fic_e);
+    // call the weak fic handler
+    fic_irq_spi_flash();
+}
+
+void handler_irq_fast_gpio_0(void)
+{
+    // The interrupt is cleared.
+    clear_fast_interrupt(kGpio_0_fic_e);
+    // call the weak fic handler
+    fic_irq_gpio_0();
+}
+
+void handler_irq_fast_gpio_1(void)
+{
+    // The interrupt is cleared.
+    clear_fast_interrupt(kGpio_1_fic_e);
+    // call the weak fic handler
+    fic_irq_gpio_1();
+}
+
+void handler_irq_fast_gpio_2(void)
+{
+    // The interrupt is cleared.
+    clear_fast_interrupt(kGpio_2_fic_e);
+    // call the weak fic handler
+    fic_irq_gpio_2();
+}
+
+void handler_irq_fast_gpio_3(void)
+{
+    // The interrupt is cleared.
+    clear_fast_interrupt(kGpio_3_fic_e);
+    // call the weak fic handler
+    fic_irq_gpio_3();
+}
+
+void handler_irq_fast_gpio_4(void)
+{
+    // The interrupt is cleared.
+    clear_fast_interrupt(kGpio_4_fic_e);
+    // call the weak fic handler
+    fic_irq_gpio_4();
+}
+
+void handler_irq_fast_gpio_5(void)
+{
+    // The interrupt is cleared.
+    clear_fast_interrupt(kGpio_5_fic_e);
+    // call the weak fic handler
+    fic_irq_gpio_5();
+}
+
+void handler_irq_fast_gpio_6(void)
+{
+    // The interrupt is cleared.
+    clear_fast_interrupt(kGpio_6_fic_e);
+    // call the weak fic handler
+    fic_irq_gpio_6();
+}
+
+void handler_irq_fast_gpio_7(void)
+{
+    // The interrupt is cleared.
+    clear_fast_interrupt(kGpio_7_fic_e);
+    // call the weak fic handler
+    fic_irq_gpio_7();
+}
 
 /****************************************************************************/
 /**                                                                        **/
