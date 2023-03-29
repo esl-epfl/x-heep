b7a023a607040c0567c5249fe2cf3a06692530fc
Taji Hossein <taji@eslpc2.intranet.epfl.ch>
2023-03-29 15:33:34 +0200

    moving handler declarations to source file from header

--- sw/device/lib/drivers/fast_intr_ctrl/fast_intr_ctrl.c
+++ sw/device/lib/drivers/fast_intr_ctrl/fast_intr_ctrl.c
@@ -49,6 +49,26 @@
 /**                                                                        **/
 /****************************************************************************/
 
+/**
+ * The RISC-V interrupt vector will not include the addresses of the handlers,
+ * instead, it includes (uncompressed) instructions. Thus the interrupt vector
+ * will include `j <interrupt handler name>` for each handler.
+ * 
+ * The only requirement on the symbol in the jump is that it must be correctly
+ * aligned. If the processor supports the C extension, this can be 2-byte
+ * aligned, but 4-byte aligned is compatible with all RISC-V processors.
+ * 
+ * If the processor is not using interrupt vectoring, then there will be a
+ * single address where interrupts jump to, which will either contain a function
+ * (which will need to be aligned), or will contain a jump to a function, again
+ * which will need to be aligned.
+ * 
+ * You only need to use this ABI for handlers that are the first function called
+ * in an interrupt handler. Subsequent functions can just use the regular RISC-V
+ * calling convention.
+ */
+#define INTERRUPT_HANDLER_ABI __attribute__((aligned(4), interrupt))
+
 /****************************************************************************/
 /**                                                                        **/
 /*                        TYPEDEFS AND STRUCTURES                           */
@@ -60,6 +80,118 @@
 /*                      PROTOTYPES OF LOCAL FUNCTIONS                       */
 /**                                                                        **/
 /****************************************************************************/
+
+/**
+ * @brief Fast timer 1 irq handler. The first entry point when timer 1 interrupt
+ * is recieved through fic.
+ * This function clear the responsible bit in FAST_INTR_PENDING then call a 
+ * function that can be overriden inside peripherals.
+ */
+INTERRUPT_HANDLER_ABI void handler_irq_fast_timer_1(void);
+
+/**
+ * @brief Fast timer 2 irq handler. The first entry point when timer 2 interrupt
+ * is recieved through fic.
+ * This function clear the responsible bit in FAST_INTR_PENDING then call a 
+ * function that can be overriden inside peripherals.
+ */
+INTERRUPT_HANDLER_ABI void handler_irq_fast_timer_2(void);
+
+/**
+ * @brief Fast timer 3 irq handler. The first entry point when timer 3 interrupt
+ * is recieved through fic.
+ * This function clear the responsible bit in FAST_INTR_PENDING then call a 
+ * function that can be overriden inside peripherals.
+ */
+INTERRUPT_HANDLER_ABI void handler_irq_fast_timer_3(void);
+
+/**
+ * @brief Fast dma irq handler. The first entry point when dma interrupt
+ * is recieved through fic.
+ * This function clear the responsible bit in FAST_INTR_PENDING then call a 
+ * function that can be overriden inside peripherals.
+ */
+INTERRUPT_HANDLER_ABI void handler_irq_fast_dma(void);
+
+/**
+ * @brief Fast spi irq handler. The first entry point when spi interrupt
+ * is recieved through fic.
+ * This function clear the responsible bit in FAST_INTR_PENDING then call a 
+ * function that can be overriden inside peripherals.
+ */
+INTERRUPT_HANDLER_ABI void handler_irq_fast_spi(void);
+
+/**
+ * @brief Fast spi flash irq handler. The first entry point when spi flash 
+ * interrupt is recieved through fic.
+ * This function clear the responsible bit in FAST_INTR_PENDING then call a 
+ * function that can be overriden inside peripherals.
+ */
+INTERRUPT_HANDLER_ABI void handler_irq_fast_spi_flash(void);
+
+/**
+ * @brief Fast gpio 0 irq handler. The first entry point when gpio 0 interrupt
+ * is recieved through fic.
+ * This function clear the responsible bit in FAST_INTR_PENDING then call a 
+ * function that can be overriden inside peripherals.
+ */
+INTERRUPT_HANDLER_ABI void handler_irq_fast_gpio_0(void);
+
+/**
+ * @brief Fast gpio 1 irq handler. The first entry point when gpio 1 interrupt
+ * is recieved through fic.
+ * This function clear the responsible bit in FAST_INTR_PENDING then call a 
+ * function that can be overriden inside peripherals.
+ */
+INTERRUPT_HANDLER_ABI void handler_irq_fast_gpio_1(void);
+
+/**
+ * @brief Fast gpio 2 irq handler. The first entry point when gpio 2 interrupt
+ * is recieved through fic.
+ * This function clear the responsible bit in FAST_INTR_PENDING then call a 
+ * function that can be overriden inside peripherals.
+ */
+INTERRUPT_HANDLER_ABI void handler_irq_fast_gpio_2(void);
+
+/**
+ * @brief Fast gpio 3 irq handler. The first entry point when gpio 3 interrupt
+ * is recieved through fic.
+ * This function clear the responsible bit in FAST_INTR_PENDING then call a 
+ * function that can be overriden inside peripherals.
+ */
+INTERRUPT_HANDLER_ABI void handler_irq_fast_gpio_3(void);
+
+/**
+ * @brief Fast gpio 4 irq handler. The first entry point when gpio 4 interrupt
+ * is recieved through fic.
+ * This function clear the responsible bit in FAST_INTR_PENDING then call a 
+ * function that can be overriden inside peripherals.
+ */
+INTERRUPT_HANDLER_ABI void handler_irq_fast_gpio_4(void);
+
+/**
+ * @brief Fast gpio 5 irq handler. The first entry point when gpio 5 interrupt
+ * is recieved through fic.
+ * This function clear the responsible bit in FAST_INTR_PENDING then call a 
+ * function that can be overriden inside peripherals.
+ */
+INTERRUPT_HANDLER_ABI void handler_irq_fast_gpio_5(void);
+
+/**
+ * @brief Fast gpio 6 irq handler. The first entry point when gpio 6 interrupt
+ * is recieved through fic.
+ * This function clear the responsible bit in FAST_INTR_PENDING then call a 
+ * function that can be overriden inside peripherals.
+ */
+INTERRUPT_HANDLER_ABI void handler_irq_fast_gpio_6(void);
+
+/**
+ * @brief Fast gpio 7 irq handler. The first entry point when gpio 7 interrupt
+ * is recieved through fic.
+ * This function clear the responsible bit in FAST_INTR_PENDING then call a 
+ * function that can be overriden inside peripherals.
+ */
+INTERRUPT_HANDLER_ABI void handler_irq_fast_gpio_7(void);
 
 /****************************************************************************/
 /**                                                                        **/
