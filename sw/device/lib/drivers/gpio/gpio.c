4c13714bcda141225cc7c2398ca4999abf679efb
Hossein <hossein.taji@epfl.ch>
2023-04-04 09:37:37 +0200

    Adding gpio functionalities

--- sw/device/lib/drivers/gpio/gpio.c
+++ sw/device/lib/drivers/gpio/gpio.c
@@ -30,6 +30,11 @@
 /**                                                                        **/
 /****************************************************************************/
 
+#include "gpio.h"
+#include "gpio_regs.h"  // Generated.
+#include "gpio_structs.h"
+#include "mmio.h"
+
 /****************************************************************************/
 /**                                                                        **/
 /*                        DEFINITIONS AND MACROS                            */
@@ -47,7 +52,30 @@
 /*                      PROTOTYPES OF LOCAL FUNCTIONS                       */
 /**                                                                        **/
 /****************************************************************************/
+/**
+ * @brief get some bitfield of a 32b reg.
+ * @param reg the register that bitwise field selects from.
+ * @param mask the mask w/o offset.
+ * @param sel this the offset used for mask and value.
+ */
+static inline uint32_t getBitfield( uint32_t reg, uint32_t  mask, uint8_t sel )
+{
+    return (reg & ( mask << sel ));
+}
 
+/**
+ * @brief do a bitwise operation on 32b register.
+ * @param reg the register pointer that bitwise operation is done on it.
+ * @param val the value should be written in part of reg w/o offset.
+ * @param mask the mask w/o offset.
+ * @param sel this the offset used for mask and value.
+ */
+static inline void setBitfield( uint32_t *reg, uint32_t  val, 
+    uint32_t mask, uint8_t sel )
+{
+    *reg           &= ~( mask << sel );
+    *reg           |= (val & mask) << sel;
+}
 /****************************************************************************/
 /**                                                                        **/
 /*                           EXPORTED VARIABLES                             */
@@ -66,11 +94,549 @@
 /**                                                                        **/
 /****************************************************************************/
 
+gpio_result_t gpio_set_mode (gpio_pin_number_t pin, gpio_mode_t mode)
+{
+    if (pin >= 0 && pin < 16)
+    {
+        setBitfield(&(gpio_peri->GPIO_MODE0), mode, 0b11, 2*pin);
+        return GpioOk;
+    }
+    else if (pin >= 16 && pin <32)
+    {
+        reg = setBit(&(gpio_peri->GPIO_MODE1), mode, 0b11, 2*(pin-16));
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_en_input (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->GPIO_EN0), 1, 0b1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_dis_input (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->GPIO_EN0), 0, 0b1, pin);
+        return GpioOk;
+    }
+    else
+    { 
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_pin_reset (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        gpio_intr_mode (0);
+        gpio_set_mode (pin, GpioModeIn);
+        gpio_dis_input (pin);
+        setBitfield(&(gpio_peri->GPIO_CLEAR0), 0, 0b1, pin);
+        setBitfield(&(gpio_peri->GPIO_SET0),   0, 0b1, pin);
+        gpio_intr_dis_all(pin);
+        gpio_intr_clear_stat(pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_reset_all (void)
+{
+    setBitfield(&(gpio_peri->GPIO_CLEAR0), 0, 0b1, 0);
+    gpio_peri->GPIO_MODE0 = 0;
+    gpio_peri->GPIO_MODE1 = 0;
+    gpio_peri->GPIO_EN0 = 0;
+    gpio_peri->GPIO_CLEAR0 = 0;
+    gpio_peri->GPIO_SET0 = 0;
+    gpio_peri->GPIO_TOGGLE0 = 0;
+    gpio_peri->INTRPT_RISE_EN0 = 0;
+    gpio_peri->INTRPT_FALL_EN0 = 0;
+    gpio_peri->INTRPT_LVL_HIGH_EN0 = 0;
+    gpio_peri->INTRPT_LVL_LOW_EN0 = 0;
+    gpio_peri->INTRPT_STATUS0 = 0xFFFFFFFF;
+}
+
+gpio_result_t gpio_pin_read (gpio_pin_number_t pin, bool *val)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        if ( getBitfield(gpio_peri->GPIO_IN0, 0b1, pin) == 1)
+            *val = true;
+        else
+            *val = false;
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+// gpio_result_t gpio_pin_set (gpio_pin_number_t pin)
+// {
+//     if (pin >= 0 && pin < 32)
+//     {
+//         gpio_peri->GPIO_SET0 = 1 << pin;
+//         gpio_peri->GPIO_OUT0 = 1 << pin;
+//         return GpioOk;
+//     }
+//     else
+//     {
+//         return GpioError;
+//     } 
+// }
+
+// gpio_result_t gpio_pin_clear (gpio_pin_number_t pin)
+// {
+//     if (pin >= 0 && pin < 32)
+//     {
+//         gpio_peri->GPIO_CLEAR0 = 1 << pin;
+//         gpio_peri->GPIO_OUT0 &= ~(1 << pin);
+//         return GpioOk;
+//     }
+//     else
+//     {
+//         return GpioError;
+//     } 
+// }
+
+//todo: check to see its toggling or just writing one
+gpio_result_t gpio_pin_toggle (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        gpio_peri->GPIO_TOGGLE0 = 1 << pin;
+        gpio_peri->GPIO_OUT0 = 1 << pin;
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_pin_write (gpio_pin_number_t pin, gpio_value val)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        if (val == true)
+            setBitfield(&(gpio_peri->GPIO_OUT0), 1, 1, pin);
+        else
+            setBitfield(&(gpio_peri->GPIO_OUT0), 0, 1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_en_rise (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->INTRPT_RISE_EN0), 1, 1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_en_fall (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->INTRPT_FALL_EN0), 1, 1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_en_lvl_high (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->INTRPT_LVL_HIGH_EN0), 1, 1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_en_lvl_low (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->INTRPT_LVL_LOW_EN0), 1, 1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_dis_rise (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->INTRPT_RISE_EN0), 0, 1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_dis_fall (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->INTRPT_FALL_EN0), 0, 1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_dis_lvl_high (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->INTRPT_LVL_HIGH_EN0), 0, 1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_dis_lvl_low (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->INTRPT_LVL_LOW_EN0), 0, 1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_dis_all (gpio_pin_number_t pin){
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->INTRPT_RISE_EN0),     0, 1, pin);
+        setBitfield(&(gpio_peri->INTRPT_FALL_EN0),     0, 1, pin);
+        setBitfield(&(gpio_peri->INTRPT_LVL_HIGH_EN0), 0, 1, pin);
+        setBitfield(&(gpio_peri->INTRPT_LVL_LOW_EN0),  0, 1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+
+}
+
+gpio_result_t gpio_intr_en (gpio_pin_number_t pin, gpio_intr_type_t type)
+{
+    if (pin >= 0 && pin < 32)
+    { 
+        gpio_intr_dis_all(pin);
+
+        switch(type)
+        {
+        case GpioIntrEdgeRising:
+            gpio_intr_en_rise(pin);
+            break;
+
+        case GpioIntrEdgeFalling:
+            gpio_intr_en_fall(pin);
+            break;
+
+        case GpioIntrLevelLow:
+            gpio_intr_en_lvl_low(pin);
+            break;
+
+        case GpioIntrLevelHigh:
+            gpio_intr_en_lvl_high(pin);
+            break;
+
+        case GpioIntrEdgeRisingFalling:
+            gpio_intr_en_rise(pin);
+            gpio_intr_en_fall(pin);
+            break;
+
+        case GpioIntrEdgeRisingLevelLow:
+            gpio_intr_en_rise(pin);
+            gpio_intr_en_lvl_low(pin);
+            break;
+
+        case GpioIntrEdgeFallingLevelHigh:
+            gpio_intr_en_fall(pin);
+            gpio_intr_en_lvl_high(pin);
+            break;
+
+        default:
+            return GpioError;
+        }
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+// gpio_result_t gpio_intr_dis (gpio_pin_number_t pin, gpio_intr_type_t type)
+// {
+//     if (pin >= 0 && pin < 32)
+//     {    
+//         switch(type)
+//         {
+//         case GpioIntrEdgeRising:
+//             gpio_intr_dis_rise(pin);
+//             break;
+
+//         case GpioIntrEdgeFalling:
+//             gpio_intr_dis_fall(pin);
+//             break;
+
+//         case GpioIntrLevelLow:
+//             gpio_intr_dis_lvl_low(pin);
+//             break;
+
+//         case GpioIntrLevelHigh:
+//             gpio_intr_dis_lvl_high(pin);
+//             break;
+
+//         case GpioIntrEdgeRisingFalling:
+//             gpio_intr_dis_rise(pin);
+//             gpio_intr_dis_fall(pin);
+//             break;
+
+//         case GpioIntrEdgeRisingLevelLow:
+//             gpio_intr_dis_rise(pin);
+//             gpio_intr_dis_lvl_low(pin);
+//             break;
+
+//         case GpioIntrEdgeFallingLevelHigh:
+//             gpio_intr_dis_fall(pin);
+//             gpio_intr_dis_lvl_high(pin);
+//             break;
+
+//         default:
+//             return GpioError;
+//         }
+//         return GpioOk;  
+//     }
+//     else
+//     {
+//         return GpioError;
+//     }
+// }
+
+gpio_result_t gpio_intr_check_stat_rise (gpio_pin_number_t pin, bool *is_pending)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        if (getBitfield(gpio_peri->INTRPT_RISE_STATUS0, 1, pin) == 1)
+            *is_pending = true;
+        else
+            *is_pending = false;
+        return GpioOk;
+    }
+    else
+    {
+        *is_pending = false;
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_check_stat_fall (gpio_pin_number_t pin, bool *is_pending)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        if (getBitfield(gpio_peri->INTRPT_FALL_STATUS0, 1, pin) == 1)
+            *is_pending = true;
+        else
+            *is_pending = false;
+        return GpioOk;
+    }
+    else
+    {
+        *is_pending = false;
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_check_stat_lvl_low (gpio_pin_number_t pin, bool *is_pending)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        if (getBitfield(gpio_peri->INTRPT_LVL_LOW_STATUS0, 1, pin) == 1)
+            *is_pending = true;
+        else
+            *is_pending = false;
+        return GpioOk;
+    }
+    else
+    {
+        *is_pending = false;
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_check_stat_lvl_high (gpio_pin_number_t pin, bool *is_pending)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        if (getBitfield(gpio_peri->INTRPT_LVL_HIGH_STATUS0, 1, pin) == 1)
+            *is_pending = true;
+        else
+            *is_pending = false;
+        return GpioOk;
+    }
+    else
+    {
+        *is_pending = false;
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_check_stat (gpio_pin_number_t pin, bool *is_pending)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        if (getBitfield(gpio_peri->INTRPT_STATUS0, 1, pin) == 1)
+            *is_pending = true;
+        else
+            *is_pending = false;
+        return GpioOk;
+    }
+    else
+    {
+        *is_pending = false;
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_clear_stat_rise (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->INTRPT_RISE_STATUS0), 1, 1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_clear_stat_fall (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->INTRPT_FALL_STATUS0), 1, 1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_clear_stat_lvl_low (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->INTRPT_LVL_LOW_STATUS0), 1, 1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_clear_stat_lvl_high (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->INTRPT_LVL_HIGH_STATUS0), 1, 1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_clear_stat (gpio_pin_number_t pin)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        setBitfield(&(gpio_peri->INTRPT_STATUS0), 1, 1, pin);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
+
+gpio_result_t gpio_intr_mode (bool gpio_intr_mode)
+{
+    if (pin >= 0 && pin < 32)
+    {
+        if (gpio_intr_mode)
+            setBitfield(&(gpio_peri->CFG), 1, 1, 0);
+        else
+            setBitfield(&(gpio_peri->CFG), 0, 1, 0);
+        return GpioOk;
+    }
+    else
+    {
+        return GpioError;
+    }
+}
 /****************************************************************************/
 /**                                                                        **/
 /*                            LOCAL FUNCTIONS                               */
 /**                                                                        **/
 /****************************************************************************/
+
 
 /****************************************************************************/
 /**                                                                        **/
@@ -85,9 +651,7 @@
 // Modified version for core-v-mini-mcu
 // original at: https://github.com/lowRISC/opentitan/blob/master/sw/
 
-#include "gpio.h"
 
-#include "gpio_regs.h"  // Generated.
 
 /**
  * Gives the mask that corresponds to the given bit index.
--- sw/device/lib/drivers/gpio/gpio.h
+++ sw/device/lib/drivers/gpio/gpio.h
@@ -48,11 +48,70 @@ extern "C" {
 /**                                                                        **/
 /****************************************************************************/
 
+/**
+ * The format use for pin number. As the gpio pins are only 32 number, 
+ * uint8_t would be enough.
+ */
+typedef uint8_t gpio_pin_number_t;
+
+/**
+ * gpio mode. 
+ * 0 configure GPIO as input. 1 configures GPIO as a push-pull output. 
+ * 2 configures the GPIO to be in open_drain0 (0 ->  High-Z, 1 -> Drive High) 
+ * mode. 3 configures the GPIO to be in open_drain1 (0 -> Drive Low, 1 -> 
+ * High-Z) mode.
+ */
+typedef enum gpio_mode 
+{
+    GpioModeIn            = 0,  /*!< input. */
+    GpioModeOutPushPull   = 1,  /*!< push-pull output. */
+    GpioModeoutOpenDrain0 = 2,  /*!< open_drain0 (0->High-Z, 1->Drive High) mode. */
+    GpioModeoutOpenDrain1 = 3,  /*!< open_drain1 (0->Drive Low, 1->High-Z) mode. */
+} gpio_mode_t;
+
+/**
+ * This type is used in almost all the operations, and it is returned when
+ * there is a problem with functions given parameters or operation for example 
+ * pin number not being in the range of accepting pins.
+ */
+typedef enum gpio_result 
+{
+    GpioOk = 0,     /*!< The operation was ok. */
+    GpioError = 1,  /*!< There is a problem. */
+} gpio_result_t;
+
+// /**
+//  * Gpio pin values: low or high
+//  */
+// typedef enum gpio_value
+// {
+//     GpioLow = 0, 
+//     GpioHigh = 1, 
+// } gpio_value_t;
+
+/**
+ * 
+ */
+typedef enum gpio_intr_type
+{
+    GpioIntrEdgeRising,         /*!< Trigger on rising edge. */
+    GpioIntrEdgeFalling,        /*!< Trigger on falling edge. */
+    GpioIntrLevelLow,           /*!< Trigger when input is low. */
+    GpioIntrLevelHigh,          /*!< rigger when input is high. */
+    GpioIntrEdgeRisingFalling,  /*!< Trigger on rising and falling edges. */
+    GpioIntrEdgeRisingLevelLow, /*!< Trigger on rising edge or when the 
+    input is low. */
+    GpioIntrEdgeFallingLevelHigh,/*!< Trigger on falling edge or when the 
+    input is high. */
+} gpio_intr_type_t;
+
+
 /****************************************************************************/
 /**                                                                        **/
 /**                       TYPEDEFS AND STRUCTURES                          **/
 /**                                                                        **/
 /****************************************************************************/
+
 
 /****************************************************************************/
 /**                                                                        **/
@@ -60,11 +119,232 @@ extern "C" {
 /**                                                                        **/
 /****************************************************************************/
 
+
 /****************************************************************************/
 /**                                                                        **/
 /**                          EXPORTED FUNCTIONS                            **/
 /**                                                                        **/
 /****************************************************************************/
+
+/**
+ * @brief setting the a pins mode by writing in GPIO_MODE0 and GPIO_MODE1.
+ * @param gpio_pin_number_t specify the pin.
+ * @param gpio_mode_t specify pin mode: 0 as input, 1 push-pull as output, 
+ * 2 as open_drain0, and 3 as open_drain1.
+ * @retval GpioOk if there is no problem with the given parameters or 
+ * operation.
+ * @retval GpioError if there is an error with the given parameters or 
+ * operation.
+ */
+gpio_result_t gpio_set_mode (gpio_pin_number_t pin, gpio_mode_t mode);
+
+/**
+ * @brief enable sampling as input by writing one in GPIO_EN. If disables 
+ * (0) the corresponding GPIO will not sample the inputs (saves power) and 
+ * will not generate any interrupts.
+ */
+gpio_result_t gpio_en_input (gpio_pin_number_t pin);
+
+/**
+ * @brief disable sampling as input by writing zero in GPIO_EN.
+ */
+gpio_result_t gpio_dis_input (gpio_pin_number_t pin);
+
+/**
+ * @brief reset completely all the configurations set for the pin
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_pin_reset (gpio_pin_number_t pin);
+
+/**
+ * @brief reset completely all the configurations for all the pins
+ */
+gpio_result_t gpio_reset_all (void);
+
+
+/**
+ * @brief reading from a gpio pin, which is done by reading from GPIO_IN reg
+ * @param gpio_pin_number_t specify pin number
+ * @return gpio value as GpioLow (false) or GpioHigh (true)
+ */
+gpio_result_t gpio_pin_read (gpio_pin_number_t pin);
+
+// /**
+//  * @brief set a pin as high. Using masking through GPIO_SET, and then writing
+//  * to GPIO_OUT 
+//  * @param gpio_pin_number_t specify pin number
+//  */
+// gpio_result_t gpio_pin_set (gpio_pin_number_t pin);
+
+// /**
+//  * @brief clear a pin as low. Using masking through GPIO_CLEAR, and then 
+//  * writing to GPIO_OUT 
+//  * @param gpio_pin_number_t specify pin number
+//  */
+// gpio_result_t gpio_pin_clear (gpio_pin_number_t pin);
+
+/**
+ * @brief toggle a pin. Using masking through GPIO_TOGGLE, and then 
+ * writing to GPIO_OUT 
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_pin_toggle (gpio_pin_number_t pin);
+
+/**
+ * @brief write to a pin. using gpio_set and gpio_clear functions.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_pin_write (gpio_pin_number_t pin, gpio_value val);
+
+/**
+ * @brief enable rising edge interrupt for the given pin.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_en_rise (gpio_pin_number_t pin);
+
+/**
+ * @brief disable rising edge interrupt for the given pin.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_dis_rise (gpio_pin_number_t pin);
+
+/**
+ * @brief enable falling edge interrupt for the given pin.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_en_fall (gpio_pin_number_t pin);
+
+/**
+ * @brief disable falling edge interrupt for the given pin.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_dis_fall (gpio_pin_number_t pin);
+
+/**
+ * @brief enable logic-high level-sensitive interrupt for the given pin.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_en_lvl_high (gpio_pin_number_t pin);
+
+/**
+ * @brief disable logic-high level-sensitive interrupt for the given pin.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_dis_lvl_high (gpio_pin_number_t pin);
+
+/**
+ * @brief enable logic-low level-sensitive interrupt for the given pin.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_en_lvl_low (gpio_pin_number_t pin);
+
+/**
+ * @brief disable logic-low level-sensitive interrupt for the given pin.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_dis_lvl_low (gpio_pin_number_t pin);
+
+/**
+ * @brief enable the interrupt for the given pin. Type of interrupt 
+ * specified by user.
+ * @param gpio_pin_number_t specify pin number
+ * @param gpio_intr_type_t specify the type of the interrupt
+ */
+gpio_result_t gpio_intr_en (gpio_pin_number_t pin, gpio_intr_type_t type);
+
+// /**
+//  * @brief disable the given type of interrupt on the given pin.
+//  * @param gpio_pin_number_t specify pin number
+//  */
+// gpio_result_t gpio_intr_dis (gpio_pin_number_t pin, gpio_intr_type_t type);
+
+/**
+ * @brief disable all the types of interrupt on the given pin.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_dis_all (gpio_pin_number_t pin);
+
+/**
+ * @brief Each bit indicates if there is a pending rising-edge interrupt 
+ * on the corresponding GPIO.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_check_stat_rise (gpio_pin_number_t pin, bool *is_pending);
+
+/**
+ * @brief Each bit indicates if there is a pending falling-edge interrupt 
+ * on the corresponding GPIO.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_check_stat_fall (gpio_pin_number_t pin, bool *is_pending);
+
+/**
+ * @brief Each bit indicates if there is a pending low level-sensitive 
+ * interrupt on the corresponding GPIO.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_check_stat_lvl_low (gpio_pin_number_t pin, bool *is_pending);
+
+/**
+ * @brief Each bit indicates if there is a pending high level-sensitive 
+ * interrupt on the corresponding GPIO.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_check_stat_lvl_high (gpio_pin_number_t pin, bool *is_pending);
+
+/**
+ * @brief Each bit indicates if there is a pending interrupt on the 
+ * corresponding GPIO in any form (rise, fall, low level, and high level)
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_check_stat (gpio_pin_number_t pin, bool *is_pending);
+
+/**
+ * @brief Clearing interrupt rise status. Writing a 1 to a specific bit 
+ * clears the interrupt for the corresponding GPIO.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_clear_stat_rise (gpio_pin_number_t pin);
+
+/**
+ * @brief Clearing interrupt fall status. Writing a 1 to a specific bit 
+ * clears the interrupt for the corresponding GPIO.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_clear_stat_fall (gpio_pin_number_t pin);
+
+/**
+ * @brief Clearing interrupt low level-sensitive status. Writing a 1 to a 
+ * specific bit clears the interrupt for the corresponding GPIO.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_clear_stat_lvl_low (gpio_pin_number_t pin);
+
+/**
+ * @brief Clearing interrupt high level-sensitive status. Writing a 1 to a 
+ * specific bit clears the interrupt for the corresponding GPIO.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_clear_stat_lvl_high (gpio_pin_number_t pin);
+
+/**
+ * @brief Clearing interrupt status regardless of its type. Writing a 1 to 
+ * a specific bit clears the interrupt for the corresponding GPIO.
+ * @param gpio_pin_number_t specify pin number
+ */
+gpio_result_t gpio_intr_clear_stat (gpio_pin_number_t pin);
+
+/**
+ * @brief Controls the interrupt mode of the gpios.
+ * @gpio_intr_mode If true, keep the interrupt line asserted until all 
+ * interrupts for all GPIOs are cleared. If false, generate one cycle wide 
+ * pulses for every new interrupt.
+ */
+gpio_result_t gpio_intr_mode (bool gpio_intr_mode);
+
+
+
+
 
 /****************************************************************************/
 /**                                                                        **/
@@ -72,17 +352,19 @@ extern "C" {
 /**                                                                        **/
 /****************************************************************************/
 
+
+
 /****************************************************************************/
 /**                                                                        **/
 /**                                EOF                                     **/
 /**                                                                        **/
 /****************************************************************************/
+
 #ifdef __cplusplus
 }
 #endif
 
 #endif  // _GPIO_H_
-
 
 
 
