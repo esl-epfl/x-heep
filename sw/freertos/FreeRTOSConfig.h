d8d6e2a032cb405b9b048b2b878cb44b07f698f7
jmiranda <mirandacalero.joseangel@gmail.com>
2023-04-05 04:25:33 +0200

    Re-adjust ram. Re-size also stack and heap for on-chip linker

--- sw/freertos/FreeRTOSConfig.h
+++ sw/freertos/FreeRTOSConfig.h
@@ -73,7 +73,7 @@
 #define configTICK_RATE_HZ	 ((TickType_t)1000)
 #define configMAX_PRIORITIES	 (5)
 /* Can be as low as 60 but some of the demo tasks that use this constant require it to be higher. */
-#define configMINIMAL_STACK_SIZE ((unsigned short)100)
+#define configMINIMAL_STACK_SIZE ((unsigned short)80)
 /* we want to put the heap into special section */
 #define configAPPLICATION_ALLOCATED_HEAP 1
 #define configTOTAL_HEAP_SIZE		 ((size_t)(6 * 1024))
