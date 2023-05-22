828b9241746e89784f1379580b16ac5a52269f1f
davide schiavone <davide@openhwgroup.org>
2023-05-22 12:37:28 +0200

    add mat add floating point

---
+++ sw/applications/matfadd/matrixAdd32.h
@@ -0,0 +1,13 @@
+#ifndef MatrixAdd32_H_
+#define MatrixAdd32_H_
+
+float m_a[2*2] = {1.0f, 2.0f, 3.0f, 4.0f};
+
+float m_b[2*2] = {0.1f, 0.01f, 0.001f, 0.0001f};
+
+float m_exp[2*2] = {1.1f, 2.01f, 3.001f, 4.0001f};
+
+#define WIDTH 2
+#define HEIGHT 2
+
+#endif  // MatrixAdd32_H_
