20fe94f5a97a41e8db9161c92ebb434402d52304
Tim Frey <tim.frey@epfl.ch>
2023-06-16 12:04:28 +0200

    add everything

--- sw/applications/i2s/demo/i2s_test.py
+++ sw/applications/i2s/demo/i2s_test.py
@@ -2,6 +2,54 @@ import serial
 import sys
 import os
 import matplotlib.pyplot as plt
+import scipy
+import sounddevice as sd
+import numpy as np
+
+
+
+
+
+
+def fft_plot(x, fs, title, y_label, filename):
+    X = np.fft.fft(x)
+    f = fs*np.fft.fftfreq(len(x))
+    f_resolution = f[1] - f[0]
+    print("Resolution is: ", f_resolution)
+    i = np.argmax(X[0:len(x)//2])
+
+    print(f"Peak at {f[i]} Hz")
+
+    fig, axs = plt.subplots(2, tight_layout=True)
+    fig.suptitle(f"{title}")
+
+    t = np.arange(len(x))/fs
+    axs[0].plot(t, x, linewidth=0.5)
+    axs[0].set_title("time series")
+    axs[0].set_xlim((0, (len(x)-1)/fs))
+    axs[0].set_xlabel("t [s]")
+    axs[0].set_ylabel(f"{y_label}")
+
+    axs[1].plot(f, abs(X), ".", markersize=3)
+    axs[1].set_xlim((0, 2e3))
+    axs[1].set_title("FFT")
+    axs[1].set_xlabel("f [Hz]")
+    axs[1].set_ylabel("Magnitude")
+
+    fig.savefig(f"../report/figures/{filename}.pdf")
+    fig.show()
+
+
+# name = file name without extension
+
+
+
+
+CLK = int(20e6)
+DIV = 8
+fs = CLK // DIV // 64
+
+
 
 argLen = len(sys.argv)
 print("Total arguments passed:", argLen)
@@ -39,6 +87,26 @@ else:
 
                     # DO ANYTHING WITH THE DATA HERE
                     # e.g. FFT, etc.
+
+                    # play
+
+                    data = np.array(mylist, dtype=np.int16)  
+                    sample = (data - data.mean())  / 2**12
+                    sd.play(sample, fs)
+
+                    # store as wav
+                    scipy.io.wavfile.write(f"data/{filename}.wav", fs, sample)
+
+                    # FFT
+                    fft_plot(
+                        x=data - data.mean(),
+                        fs=fs,
+                        title="Recording of a 440 Hz note\n(played from a smartphone)",
+                        y_label="Magnitude\n(16bit signed)",
+                        filename="fft_analysis"
+                    )
+
+
 
                     # Plot the data 
                     plt.figure(count)
