85f1aff0940c004a59a8aa4c60a5478a2b00cb46
Tim Frey <tim.frey@epfl.ch>
2023-06-16 12:01:42 +0200

    clean up

--- sw/applications/i2s/demo/i2s_test.py
+++ sw/applications/i2s/demo/i2s_test.py
@@ -9,14 +9,13 @@ print("Total arguments passed:", argLen)
 start = False
 
 if( argLen < 3 ):
-    print ("Usage:", sys.argv[0], "[/dev/ttyUSBx]", "[num of plots]")
+    print ("Usage:", sys.argv[0], "[/dev/ttyUSBx]", "[num of batches]")
 else:
         serialPort = serial.Serial(port = sys.argv[1], baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
         numPlots = int(sys.argv[2])
 
-        serialString = ""                           # Used to hold data coming over UART
-        
+        serialString = ""# Used to hold data coming over UART
         count = 0
         mylist = []
 
@@ -25,9 +24,12 @@ else:
             # Wait until there is data waiting in the serial buffer
             if(serialPort.in_waiting > 0):
 
-                # Read data out of the buffer until a carraige return / new line is found
+                # read a line of data form the serial port
                 serialString = serialPort.readline()
+
+                # wait for index to signal start of data
                 if (b'index' in serialString):
+                    # reset variables
                     start = True
                     mylist = []
                     print("Started recording")
@@ -35,17 +37,21 @@ else:
                     print("Done recording")
                     print(mylist)
 
+                    # DO ANYTHING WITH THE DATA HERE
+                    # e.g. FFT, etc.
+
+                    # Plot the data 
                     plt.figure(count)
                     plt.plot(mylist)
                     plt.show()
+
+                    # restart the process
                     start = False
                     count = count + 1
-
                     if (numPlots == count):
                         break
                 elif (start == True):
                     val = int(serialString.split(b',')[1])
-                    print(val)
                     mylist.append(val)
                 else:
                     print(serialString)
