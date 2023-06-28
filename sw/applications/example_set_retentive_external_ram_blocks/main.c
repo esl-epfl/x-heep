bf52f256048d5ad0e904f58debef9d86673e9407
Juan Sapriza <juan.sapriza@epfl.ch>
2023-06-28 15:44:18 +0200

    Removed extra \n

--- sw/applications/example_set_retentive_external_ram_blocks/main.c
+++ sw/applications/example_set_retentive_external_ram_blocks/main.c
@@ -1,5 +1,5 @@
 // Copyright EPFL contributors.
-// Licensed under the Apache License, Version 2.0, see LICENSE for details.\n
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
 #include <stdio.h>
