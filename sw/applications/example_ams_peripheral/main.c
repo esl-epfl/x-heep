562b27aa1d34b6200bc8b1a26d028563d456c0af
Juan Sapriza <juan.sapriza@epfl.ch>
2023-06-28 15:21:16 +0200

    Recovered removed printfs from ams peripheral example. They are left as compulsory printfs.

--- sw/applications/example_ams_peripheral/main.c
+++ sw/applications/example_ams_peripheral/main.c
@@ -19,21 +19,28 @@ int main(int argc, char *argv[])
         unsigned int AMS_START_ADDRESS = EXT_PERIPHERAL_START_ADDRESS + 0x1000;
         mmio_region_t ams_base_addr = mmio_region_from_addr((uintptr_t)AMS_START_ADDRESS);
 
+        printf("0");
         mmio_region_write32(ams_base_addr, AMS_SEL_REG_OFFSET, 0);
 
+        printf("1");
         mmio_region_write32(ams_base_addr, AMS_SEL_REG_OFFSET, 1);
 
         for (int i = 0 ; i < 10000 ; i++) {
                 asm volatile ("nop");
         }
 
+        printf("2");
         mmio_region_write32(ams_base_addr, AMS_SEL_REG_OFFSET, 2);
 
         for (int i = 0 ; i < 10000 ; i++) {
                 asm volatile ("nop");
         }
 
+        printf("3");
         mmio_region_write32(ams_base_addr, AMS_SEL_REG_OFFSET, 3);
+
+
+        printf("END\n");
 
         return EXIT_SUCCESS;
 }
