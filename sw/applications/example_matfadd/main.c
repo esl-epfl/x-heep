efc0d63885f33e38c23e5cbbbd195647603a0125
Schiavone Pasquale Davide <schiavon@eslsrv12.intranet.epfl.ch>
2023-06-30 09:31:35 +0200

    add print float

--- sw/applications/example_matfadd/main.c
+++ sw/applications/example_matfadd/main.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include "csr.h"
 #include "matrixAdd32.h"
+#include <math.h>
 
 #define FS_INITIAL 0x01
 
@@ -30,6 +31,79 @@ uint32_t check_results(float *  C, int N, int M);
 
 float m_c[HEIGHT*WIDTH];
 
+void swap(char *a, char *b)
+{
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// A function to print a floating point number using putchar
+void putfloat(float x, int p)
+{
+    // Check if x is negative
+    if (x < 0)
+    {
+        // Print a minus sign
+        putchar('-');
+        // Make x positive
+        x = -x;
+    }
+
+    // Convert the integer part of x into a string of digits
+    long i = (long)x; // Get the integer part
+    char int_str[20]; // An array to store the digits
+    int len = 0; // The length of the string
+    do
+    {
+        // Get the last digit and store it in the array
+        int_str[len] = '0' + i % 10;
+        len++;
+        // Remove the last digit from i
+        i /= 10;
+    } while (i > 0);
+
+    // Reverse the string of digits
+    for (int j = 0; j < len / 2; j++)
+    {
+        // Swap the elements at both ends
+        swap(&int_str[j], &int_str[len - 1 - j]);
+    }
+
+    // Print the string of digits
+    for (int j = 0; j < len; j++)
+    {
+        putchar(int_str[j]);
+    }
+
+    // Print a decimal point
+    putchar('.');
+
+    // Convert the fractional part of x into a string of digits
+    float f = x - (long)x; // Get the fractional part
+    char frac_str[20]; // An array to store the digits
+    len = 0; // The length of the string
+    while (p--)
+    {
+        // Get the first digit after the decimal point and store it in the array
+        f = (f - (long)f) * 10;
+        frac_str[len] = '0' + (long)f;
+        len++;
+        // Round up if necessary
+        if (fabs(f - (long)f) >= 0.5f)
+        {
+            frac_str[len - 1]++;
+        }
+    }
+
+    // Print the string of digits
+    for (int j = 0; j < len; j++)
+    {
+        putchar(frac_str[j]);
+    }
+}
+
+
 int main()
 {
     int N = WIDTH;
@@ -52,6 +126,12 @@ int main()
     errors = check_results(m_c, N, M);
 
     PRINTF("program finished with %d errors and %d cycles\n\r", errors, cycles);
+
+#ifdef ENABLE_PRINTF
+    printMatrix(m_c,N,M);
+#endif
+
+
     return errors;
 }
 
@@ -61,6 +141,18 @@ void __attribute__ ((noinline)) matrixAdd(float *  A, float *  B, float *  C, in
         for(int j = 0; j < M; j++) {
             C[i*N+j] = A[i*WIDTH+j] + B[i*WIDTH+j];
         }
+    }
+}
+
+void __attribute__ ((noinline)) printMatrix(float *  C, int N, int M)
+{
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) {
+            putfloat(C[i*N+j], 2);
+            if( j != M -1)
+                printf(", ");
+        }
+        printf("\n");
     }
 }
 
