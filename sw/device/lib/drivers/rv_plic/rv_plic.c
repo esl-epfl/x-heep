dbe3ad5ea44cd85a9ab506b245f8d43f367a905b
Juan Sapriza <juan.sapriza@epfl.ch>
2023-07-19 13:50:55 +0200

    First modifications

--- sw/device/lib/drivers/rv_plic/rv_plic.c
+++ sw/device/lib/drivers/rv_plic/rv_plic.c
@@ -68,13 +68,15 @@ const uint32_t plicMaxPriority = RV_PLIC_PRIO0_PRIO0_MASK;
  * index is generated. This index will be used to address the proper
  * handler function inside this array.
 */
-handler_funct_t handlers[] = {&handler_irq_uart, 
-                              &handler_irq_gpio, 
-                              &handler_irq_i2c, 
-                              &handler_irq_spi,
-                              &handler_irq_i2s,
-                              &handler_irq_dma,
-                              &handler_irq_ext};
+// handler_funct_t handlers[] = {&handler_irq_uart,
+//                               &handler_irq_gpio,
+//                               &handler_irq_i2c,
+//                               &handler_irq_spi,
+//                               &handler_irq_i2s,
+//                               &handler_irq_dma,
+//                               &handler_irq_ext};
+
+handler_funct_t handlers[QTY_INTR];
 
 /****************************************************************************/
 /**                                                                        **/
@@ -103,7 +105,7 @@ static irq_sources_t plic_get_irq_src_type(uint32_t irq_id);
 static ptrdiff_t plic_offset_from_reg0(uint32_t irq);
 
 /**
- * 
+ *
  * Get an IE, IP, LE register bit index from an IRQ source ID.
  *
  * With more than 32 IRQ sources, there is a multiple of these registers to
@@ -125,7 +127,7 @@ static uint8_t plic_irq_bit_index(uint32_t irq);
 /**                                                                        **/
 /****************************************************************************/
 
-/* 
+/*
   Flag to handle the wait for interrupt.
   Set to 1 after an interrupt occours and the core is in wait for interrupt
   so the execution of the program can continue.
@@ -138,34 +140,6 @@ uint8_t plic_intr_flag = 0;
 /*                           EXPORTED FUNCTIONS                             */
 /**                                                                        **/
 /****************************************************************************/
-
-__attribute__((weak, optimize("O0"))) void handler_irq_uart(uint32_t id) {
-
-} 
-
-__attribute__((weak, optimize("O0"))) void handler_irq_gpio(uint32_t id) {
-  
-}
-
-__attribute__((weak, optimize("O0"))) void handler_irq_i2c(uint32_t id) {
-  
-}
-
-__attribute__((weak, optimize("O0"))) void handler_irq_spi(uint32_t id) {
-  
-}
-
-__attribute__((weak, optimize("O0"))) void handler_irq_i2s(uint32_t id) {
-
-} 
-
-__attribute__((weak, optimize("O0"))) void handler_irq_dma(uint32_t id) {
-
-}
-
-__attribute__((weak, optimize("O0"))) void handler_irq_ext(uint32_t id) {
-
-}
 
 void handler_irq_external(void)
 {
@@ -185,22 +159,22 @@ void handler_irq_external(void)
 
 
 /*!
-  Resets relevant registers of the PLIC (Level/Edge, 
+  Resets relevant registers of the PLIC (Level/Edge,
   priority, target, threshold, interrupts).
   It writes the default value 0 in them and read back
   the value into the proper register struct
 */
 plic_result_t plic_Init(void)
 {
-  
-  /* Clears all the Level/Edge registers */ 
+
+  /* Clears all the Level/Edge registers */
   for(uint8_t i=0; i<RV_PLIC_LE_MULTIREG_COUNT; i++)
   {
     (&rv_plic_peri->LE0)[i] = 0;
   }
 
 
-  /* Clears all the priority registers */ 
+  /* Clears all the priority registers */
   for(uint8_t i=0; i<RV_PLIC_PARAM_NUM_SRC; i++)
   {
     (&rv_plic_peri->PRIO0)[i] = 0;
@@ -211,7 +185,7 @@ plic_result_t plic_Init(void)
   {
     (&rv_plic_peri->IE00)[i] = 0;
   }
-  
+
   /* Clears all the threshold registers */
   rv_plic_peri->THRESHOLD0 = 0;
   if(rv_plic_peri->THRESHOLD0 != 0)
@@ -219,13 +193,24 @@ plic_result_t plic_Init(void)
     return kPlicError;
   }
 
-
   /* clears software interrupts registers */
   rv_plic_peri->MSIP0 = 0;
   if(rv_plic_peri->MSIP0 != 0)
   {
     return kPlicError;
   }
+
+
+
+
+
+
+
+
+
+
+
+
 
   return kPlicOk;
 
@@ -246,7 +231,7 @@ plic_result_t plic_irq_set_enabled(uint32_t irq,
     return kPlicBadArg;
   }
 
-  // Get the offset of the register in which to write given the irq ID 
+  // Get the offset of the register in which to write given the irq ID
   ptrdiff_t offset = plic_offset_from_reg0(irq);
 
   // Get the destination bit in which to write
@@ -254,9 +239,9 @@ plic_result_t plic_irq_set_enabled(uint32_t irq,
 
   // Writes `state` in the amount of bits defined by the mask
   // at position `bit_index` inside the proper Interrupt Enable register
-  (&rv_plic_peri->IE00)[offset] = bitfield_write((&rv_plic_peri->IE00)[offset], 
-                                                  BIT_MASK_1, 
-                                                  bit_index, 
+  (&rv_plic_peri->IE00)[offset] = bitfield_write((&rv_plic_peri->IE00)[offset],
+                                                  BIT_MASK_1,
+                                                  bit_index,
                                                   state);
 
   return kPlicOk;
@@ -270,14 +255,14 @@ plic_result_t plic_irq_get_enabled(uint32_t irq,
   {
     return kPlicBadArg;
   }
-  
-  // Get the destination register 
+
+  // Get the destination register
   ptrdiff_t offset = plic_offset_from_reg0(irq);
 
   // Get the destination bit in which to write
   uint16_t bit_index = plic_irq_bit_index(irq);
 
-  // Reads the enabled/disabled bit 
+  // Reads the enabled/disabled bit
   *state = bitfield_read((&rv_plic_peri->IE00)[offset], BIT_MASK_1, bit_index);
 
   return kPlicOk;
@@ -293,7 +278,7 @@ plic_result_t plic_irq_set_trigger(uint32_t irq,
     return kPlicBadArg;
   }
 
-  // Get the destination register 
+  // Get the destination register
   ptrdiff_t offset = plic_offset_from_reg0(irq);
 
   // Get the destination bit in which to write
@@ -346,7 +331,7 @@ plic_result_t plic_irq_is_pending(uint32_t irq,
     return kPlicBadArg;
   }
 
-  // Get the destination register 
+  // Get the destination register
   ptrdiff_t offset = plic_offset_from_reg0(irq);
 
   // Get the destination bit in which to write
@@ -358,22 +343,22 @@ plic_result_t plic_irq_is_pending(uint32_t irq,
 }
 
 
-plic_result_t plic_irq_claim(uint32_t *claim_data) 
+plic_result_t plic_irq_claim(uint32_t *claim_data)
 {
-  if (claim_data == NULL) 
+  if (claim_data == NULL)
   {
     return kPlicBadArg;
   }
-  
+
   *claim_data = rv_plic_peri->CC0;
 
   return kPlicOk;
 }
 
 
-plic_result_t plic_irq_complete(const uint32_t *complete_data) 
+plic_result_t plic_irq_complete(const uint32_t *complete_data)
 {
-  if (complete_data == NULL) 
+  if (complete_data == NULL)
   {
     return kPlicBadArg;
   }
@@ -423,7 +408,7 @@ static irq_sources_t plic_get_irq_src_type(uint32_t irq_id)
   else if (irq_id <= GPIO_ID_END)
   {
     return IRQ_GPIO_SRC;
-  } 
+  }
   else if (irq_id <= I2C_ID_END)
   {
     return IRQ_I2C_SRC;
@@ -432,7 +417,7 @@ static irq_sources_t plic_get_irq_src_type(uint32_t irq_id)
   {
     return IRQ_SPI_SRC;
   }
-  else if (irq_id == I2S_ID) 
+  else if (irq_id == I2S_ID)
   {
     return IRQ_I2S_SRC;
   }
@@ -440,19 +425,19 @@ static irq_sources_t plic_get_irq_src_type(uint32_t irq_id)
   {
     return IRQ_DMA_SRC;
   }
-  else 
+  else
   {
     return IRQ_EXT_SRC;
   }
 
 }
 
-static ptrdiff_t plic_offset_from_reg0(uint32_t irq) 
+static ptrdiff_t plic_offset_from_reg0(uint32_t irq)
 {
   return irq / RV_PLIC_PARAM_REG_WIDTH;
 }
 
-static uint8_t plic_irq_bit_index(uint32_t irq) 
+static uint8_t plic_irq_bit_index(uint32_t irq)
 {
   return irq % RV_PLIC_PARAM_REG_WIDTH;
 }
