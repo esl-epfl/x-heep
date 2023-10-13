6d196c15d9d9d51132cdb69e0a1763395d30b1db
Schiavone Pasquale Davide <schiavon@eslsrv13.intranet.epfl.ch>
2023-10-13 15:54:14 +0200

    fix other spi_read_like apps

--- sw/applications/example_spi_host_dma/main.c
+++ sw/applications/example_spi_host_dma/main.c
@@ -67,6 +67,15 @@ uint32_t copy_data[COPY_DATA_NUM] __attribute__ ((aligned (4)))  = { 0 };
 
 #define COPY_DATA_TYPE (COPY_DATA_NUM/(sizeof(uint32_t)/sizeof(DATA_TYPE)))
 
+extern const uint32_t _lma_vma_data_offset;
+
+uint32_t * get_data_address_lma(uint32_t* data_address_vma){
+
+    uint32_t* data_address_lma = (uint32_t*) ((uint32_t)&(_lma_vma_data_offset) + (uint32_t)(data_address_vma));
+    //set MS 8 bits to 0 as the flash only uses 24b
+    data_address_lma = (uint32_t*) ((uint32_t)(data_address_lma) & 0x00FFFFFF);
+    return data_address_lma;
+}
 
 int main(int argc, char *argv[])
 {
@@ -74,6 +83,7 @@ int main(int argc, char *argv[])
     soc_ctrl_t soc_ctrl;
     soc_ctrl.base_addr = mmio_region_from_addr((uintptr_t)SOC_CTRL_START_ADDRESS);
     uint32_t read_byte_cmd;
+    uint32_t* flash_data_lma = get_data_address_lma(flash_data);
 
     if ( get_spi_flash_mode(&soc_ctrl) == SOC_CTRL_SPI_FLASH_MODE_SPIMEMIO )
     {
@@ -218,8 +228,9 @@ int main(int argc, char *argv[])
 
     #if SPI_DATA_TYPE == DMA_DATA_TYPE_DATA_TYPE_VALUE_DMA_32BIT_WORD
 
+
         if(get_spi_flash_mode(&soc_ctrl) != SOC_CTRL_SPI_FLASH_MODE_SPIMEMIO)
-            read_byte_cmd = ((REVERT_24b_ADDR(flash_data) << 8) | 0x03); // The address bytes sent through the SPI to the Flash are in reverse order
+            read_byte_cmd = ((REVERT_24b_ADDR(flash_data_lma) << 8) | 0x03); // The address bytes sent through the SPI to the Flash are in reverse order
         else
             // we read the data from the FLASH address 0x0, which corresponds to FLASH_MEM_START_ADDRESS
             read_byte_cmd = ((REVERT_24b_ADDR(0x0) << 8) | 0x03); // The address bytes sent through the SPI to the Flash are in reverse order
@@ -243,7 +254,7 @@ int main(int argc, char *argv[])
             .speed      = kSpiSpeedStandard,
             .direction  = kSpiDirRxOnly
         });
-        DATA_TYPE* flash_ptr = (DATA_TYPE *)flash_data;
+        DATA_TYPE* flash_ptr = (DATA_TYPE *)flash_data_lma;
         for (int i = 0; i<COPY_DATA_NUM; i++) { // Multiple 8 or 16-bit transactions, just to try a new mode, we could treat it as int32
             // Request the same data multiple times
             if(get_spi_flash_mode(&soc_ctrl) != SOC_CTRL_SPI_FLASH_MODE_SPIMEMIO)
