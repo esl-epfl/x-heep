21a2648b6d0a8610553c6affeee0523e65a8c709
Mattia Consani <mattia.consani@epfl.ch>
2023-11-15 15:29:27 +0100

    move profiling to another branch

--- sw/applications/bsp_flash_profiling/analysis/gen_test_buffer.py
+++
@@ -1,9 +0,0 @@
-# Generate 1KB of different bytes in hexadecimal notation
-hex_data = [f'0x{i:08X},' if (i + 1) % 8 != 0 else f'0x{i:08X},\n' for i in range(0xFFFFFFFF, 0xFFFFFBFE, -1)]  # Repeat 0x00 to 0xFF four times
-hex_data = ' '.join(hex_data)  # Convert the list to a space-separated string
-
-# Write the data to a file
-with open('hex_data.txt', 'w') as file:
-    file.write(hex_data)
-
-print("1KB of different bytes in hexadecimal notation has been written to 'hex_data.txt'.")
