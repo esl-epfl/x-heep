17900a44932107b8889504023fe7ce2fbd2eeaf7
Schiavone Pasquale Davide <schiavon@eslsrv13.intranet.epfl.ch>
2024-02-01 12:26:28 +0100

    better readability for function offset

--- sw/device/lib/runtime/core_v_mini_mcu.c
+++ sw/device/lib/runtime/core_v_mini_mcu.c
@@ -12,10 +12,11 @@ uint32_t * heep_get_flash_address_offset(uint32_t* data_address_lma);
 // LINKER symbol, the value is defined in the LINKER script
 extern const uint32_t _lma_vma_data_offset;
 
-// this translates the virtual address to logical address for the FLASH
+// this translates the virtual address to load address for the FLASH
 uint32_t * heep_get_data_address_lma(uint32_t* data_address_vma){
 
-    uint32_t* data_address_lma = (uint32_t*) ((uint32_t)&(_lma_vma_data_offset) + (uint32_t)(data_address_vma));
+    uint32_t offset = (uint32_t)&_lma_vma_data_offset; // use the & operator to get the address
+    uint32_t* data_address_lma = (uint32_t*) (offset + (uint32_t)(data_address_vma));
 
     return data_address_lma;
 }
