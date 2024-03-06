c88507c11e783848d7365e93bd57e8a9da380ad4
Llorenç Muela <llorenc-robert.muelahausmann@epfl.ch>
2024-03-06 21:33:53 +0100

    Changed all bitfield read/write in spi_host.c file

--- sw/device/lib/drivers/spi_host/spi_host.c
+++ sw/device/lib/drivers/spi_host/spi_host.c
@@ -66,26 +66,6 @@
 /**                                                                        **/
 /****************************************************************************/
 
-/**
- * Control Block (CB) of the SPI peripheral.
- * Has variables and constant necessary/useful for its control.
- */
-static struct
-{
-    /**
-    * Flag to lower as soon as a transaction is launched, and raised by the
-    * interrupt handler once it has finished. Only used when the end event is
-    * set to INTR_WAIT.
-    */
-    uint8_t intrFlag;
-
-    /**
-     * memory mapped structure of an SPI.
-     */
-    spi_host* peri;
-
-} spi_cb;
-
 /****************************************************************************/
 /**                                                                        **/
 /*                           EXPORTED FUNCTIONS                             */
@@ -96,32 +76,42 @@ static struct
 
 volatile uint8_t spi_get_tx_queue_depth(const spi_host_t *spi) {
     volatile uint32_t status_reg = spi_get_status(spi);
-    return bitfield_field32_read(status_reg, SPI_HOST_STATUS_TXQD_FIELD);
+    // return bitfield_field32_read(status_reg, SPI_HOST_STATUS_TXQD_FIELD);
+    return bitfield_read(status_reg, SPI_HOST_STATUS_TXQD_MASK, SPI_HOST_STATUS_TXQD_OFFSET);
 }
 
 volatile spi_ch_status_t spi_get_tx_channel_status(const spi_host_t *spi) {
     volatile uint32_t status_reg = spi_get_status(spi);
     spi_ch_status_t ch_status = {
-        .empty = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_TXEMPTY_BIT),
-        .full = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_TXFULL_BIT),
-        .stall = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_TXSTALL_BIT),
-        .wm = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_TXWM_BIT)
+        // .empty = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_TXEMPTY_BIT),
+        // .full = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_TXFULL_BIT),
+        // .stall = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_TXSTALL_BIT),
+        // .wm = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_TXWM_BIT)
+        .empty = bitfield_read(status_reg, BIT_MASK_1, SPI_HOST_STATUS_TXEMPTY_BIT),
+        .full = bitfield_read(status_reg, BIT_MASK_1, SPI_HOST_STATUS_TXFULL_BIT),
+        .stall = bitfield_read(status_reg, BIT_MASK_1, SPI_HOST_STATUS_TXSTALL_BIT),
+        .wm = bitfield_read(status_reg, BIT_MASK_1, SPI_HOST_STATUS_TXWM_BIT)
     };
     return ch_status;
 }
 
 volatile uint8_t spi_get_rx_queue_depth(const spi_host_t *spi) {
     volatile uint32_t status_reg = spi_get_status(spi);
-    return bitfield_field32_read(status_reg, SPI_HOST_STATUS_RXQD_FIELD);
+    // return bitfield_field32_read(status_reg, SPI_HOST_STATUS_RXQD_FIELD);
+    return bitfield_read(status_reg, SPI_HOST_STATUS_RXQD_MASK, SPI_HOST_STATUS_RXQD_OFFSET);
 }
 
 volatile spi_ch_status_t spi_get_rx_channel_status(const spi_host_t *spi) {
     volatile uint32_t status_reg = spi_get_status(spi);
     spi_ch_status_t ch_status = {
-        .empty = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_RXEMPTY_BIT),
-        .full = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_RXFULL_BIT),
-        .stall = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_RXSTALL_BIT),
-        .wm = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_RXWM_BIT)
+        // .empty = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_RXEMPTY_BIT),
+        // .full = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_RXFULL_BIT),
+        // .stall = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_RXSTALL_BIT),
+        // .wm = bitfield_bit32_read(status_reg, SPI_HOST_STATUS_RXWM_BIT)
+        .empty = bitfield_read(status_reg, BIT_MASK_1, SPI_HOST_STATUS_RXEMPTY_BIT),
+        .full = bitfield_read(status_reg, BIT_MASK_1, SPI_HOST_STATUS_RXFULL_BIT),
+        .stall = bitfield_read(status_reg, BIT_MASK_1, SPI_HOST_STATUS_RXSTALL_BIT),
+        .wm = bitfield_read(status_reg, BIT_MASK_1, SPI_HOST_STATUS_RXWM_BIT)
     };
     return ch_status;
 }
@@ -136,7 +126,8 @@ volatile uint32_t spi_get_csid(const spi_host_t *spi) {
 void spi_sw_reset(const spi_host_t *spi) {
     // volatile uint32_t ctrl_reg = mmio_region_read32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET);
     volatile uint32_t ctrl_reg = spi_host_peri->CONTROL;
-    ctrl_reg = bitfield_bit32_write(ctrl_reg, SPI_HOST_CONTROL_SW_RST_BIT, 1);
+    // ctrl_reg = bitfield_bit32_write(ctrl_reg, SPI_HOST_CONTROL_SW_RST_BIT, 1);
+    ctrl_reg = bitfield_write(ctrl_reg, BIT_MASK_1, SPI_HOST_CONTROL_SW_RST_BIT, 1);
     // mmio_region_write32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET, ctrl_reg);
     spi_host_peri->CONTROL = ctrl_reg;
 }
@@ -144,7 +135,8 @@ void spi_sw_reset(const spi_host_t *spi) {
 void spi_set_enable(const spi_host_t *spi, bool enable) {
     // volatile uint32_t ctrl_reg = mmio_region_read32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET);
     volatile uint32_t ctrl_reg = spi_host_peri->CONTROL;
-    ctrl_reg = bitfield_bit32_write(ctrl_reg, SPI_HOST_CONTROL_SPIEN_BIT, enable);
+    // ctrl_reg = bitfield_bit32_write(ctrl_reg, SPI_HOST_CONTROL_SPIEN_BIT, enable);
+    ctrl_reg = bitfield_write(ctrl_reg, BIT_MASK_1, SPI_HOST_CONTROL_SPIEN_BIT, enable);
     // mmio_region_write32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET, ctrl_reg);
     spi_host_peri->CONTROL = ctrl_reg;
 }
@@ -152,7 +144,8 @@ void spi_set_enable(const spi_host_t *spi, bool enable) {
 void spi_set_tx_watermark(const spi_host_t *spi, uint8_t watermark) {
     // volatile uint32_t ctrl_reg = mmio_region_read32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET);
     volatile uint32_t ctrl_reg = spi_host_peri->CONTROL;
-    ctrl_reg = bitfield_field32_write(ctrl_reg, SPI_HOST_CONTROL_TX_WATERMARK_FIELD, watermark);
+    // ctrl_reg = bitfield_field32_write(ctrl_reg, SPI_HOST_CONTROL_TX_WATERMARK_FIELD, watermark);
+    ctrl_reg = bitfield_write(ctrl_reg, SPI_HOST_CONTROL_TX_WATERMARK_MASK, SPI_HOST_CONTROL_TX_WATERMARK_OFFSET, watermark);
     // mmio_region_write32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET, ctrl_reg);
     spi_host_peri->CONTROL = ctrl_reg;
 }
@@ -160,7 +153,8 @@ void spi_set_tx_watermark(const spi_host_t *spi, uint8_t watermark) {
 void spi_set_rx_watermark(const spi_host_t *spi, uint8_t watermark) {
     // volatile uint32_t ctrl_reg = mmio_region_read32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET);
     volatile uint32_t ctrl_reg = spi_host_peri->CONTROL;
-    ctrl_reg = bitfield_field32_write(ctrl_reg, SPI_HOST_CONTROL_RX_WATERMARK_FIELD, watermark);
+    // ctrl_reg = bitfield_field32_write(ctrl_reg, SPI_HOST_CONTROL_RX_WATERMARK_FIELD, watermark);
+    ctrl_reg = bitfield_write(ctrl_reg, SPI_HOST_CONTROL_RX_WATERMARK_MASK, SPI_HOST_CONTROL_RX_WATERMARK_OFFSET, watermark);
     // mmio_region_write32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET, ctrl_reg);
     spi_host_peri->CONTROL = ctrl_reg;
 }
@@ -203,7 +197,8 @@ void spi_read_word(const spi_host_t *spi, uint32_t* dst) {
 void spi_enable_evt_intr(const spi_host_t *spi, bool enable) {
     // volatile uint32_t intr_enable_reg = mmio_region_read32(spi->base_addr, SPI_HOST_INTR_ENABLE_REG_OFFSET);
     volatile uint32_t intr_enable_reg = spi_host_peri->INTR_ENABLE;
-    intr_enable_reg = bitfield_bit32_write(intr_enable_reg, SPI_HOST_INTR_ENABLE_SPI_EVENT_BIT, enable);
+    // intr_enable_reg = bitfield_bit32_write(intr_enable_reg, SPI_HOST_INTR_ENABLE_SPI_EVENT_BIT, enable);
+    intr_enable_reg = bitfield_write(intr_enable_reg, BIT_MASK_1, SPI_HOST_INTR_ENABLE_SPI_EVENT_BIT, enable);
     // mmio_region_write32(spi->base_addr, SPI_HOST_INTR_ENABLE_REG_OFFSET, intr_enable_reg);
     spi_host_peri->INTR_ENABLE = intr_enable_reg;
 }
@@ -211,7 +206,8 @@ void spi_enable_evt_intr(const spi_host_t *spi, bool enable) {
 void spi_enable_error_intr(const spi_host_t *spi, bool enable) {
     // volatile uint32_t intr_enable_reg = mmio_region_read32(spi->base_addr, SPI_HOST_INTR_ENABLE_REG_OFFSET);
     volatile uint32_t intr_enable_reg = spi_host_peri->INTR_ENABLE;
-    intr_enable_reg = bitfield_bit32_write(intr_enable_reg, SPI_HOST_INTR_STATE_ERROR_BIT, enable);
+    // intr_enable_reg = bitfield_bit32_write(intr_enable_reg, SPI_HOST_INTR_STATE_ERROR_BIT, enable);
+    intr_enable_reg = bitfield_write(intr_enable_reg, BIT_MASK_1, SPI_HOST_INTR_STATE_ERROR_BIT, enable);
     // mmio_region_write32(spi->base_addr, SPI_HOST_INTR_ENABLE_REG_OFFSET, intr_enable_reg);
     spi_host_peri->INTR_ENABLE = intr_enable_reg;
 }
@@ -219,7 +215,8 @@ void spi_enable_error_intr(const spi_host_t *spi, bool enable) {
 void spi_enable_rxwm_intr(const spi_host_t *spi, bool enable) {
     // volatile uint32_t intr_enable_reg = mmio_region_read32(spi->base_addr, SPI_HOST_EVENT_ENABLE_REG_OFFSET);
     volatile uint32_t intr_enable_reg = spi_host_peri->EVENT_ENABLE;
-    intr_enable_reg = bitfield_bit32_write(intr_enable_reg, SPI_HOST_EVENT_ENABLE_RXWM_BIT, enable);
+    // intr_enable_reg = bitfield_bit32_write(intr_enable_reg, SPI_HOST_EVENT_ENABLE_RXWM_BIT, enable);
+    intr_enable_reg = bitfield_write(intr_enable_reg, BIT_MASK_1, SPI_HOST_EVENT_ENABLE_RXWM_BIT, enable);
     // mmio_region_write32(spi->base_addr, SPI_HOST_EVENT_ENABLE_REG_OFFSET, intr_enable_reg);
     spi_host_peri->EVENT_ENABLE = intr_enable_reg;
 }
@@ -227,7 +224,8 @@ void spi_enable_rxwm_intr(const spi_host_t *spi, bool enable) {
 void spi_enable_txempty_intr(const spi_host_t *spi, bool enable) {
     // volatile uint32_t intr_enable_reg = mmio_region_read32(spi->base_addr, SPI_HOST_EVENT_ENABLE_REG_OFFSET);
     volatile uint32_t intr_enable_reg = spi_host_peri->EVENT_ENABLE;
-    intr_enable_reg = bitfield_bit32_write(intr_enable_reg, SPI_HOST_EVENT_ENABLE_TXEMPTY_BIT, enable);
+    // intr_enable_reg = bitfield_bit32_write(intr_enable_reg, SPI_HOST_EVENT_ENABLE_TXEMPTY_BIT, enable);
+    intr_enable_reg = bitfield_write(intr_enable_reg, BIT_MASK_1, SPI_HOST_EVENT_ENABLE_TXEMPTY_BIT, enable);
     // mmio_region_write32(spi->base_addr, SPI_HOST_EVENT_ENABLE_REG_OFFSET, intr_enable_reg);
     spi_host_peri->EVENT_ENABLE = intr_enable_reg;
 }
@@ -235,7 +233,8 @@ void spi_enable_txempty_intr(const spi_host_t *spi, bool enable) {
 void spi_output_enable(const spi_host_t *spi, bool enable){
     // volatile uint32_t output_enable_reg = mmio_region_read32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET);
     volatile uint32_t output_enable_reg = spi_host_peri->CONTROL;
-    output_enable_reg = bitfield_bit32_write(output_enable_reg, SPI_HOST_CONTROL_OUTPUT_EN_BIT, enable);
+    // output_enable_reg = bitfield_bit32_write(output_enable_reg, SPI_HOST_CONTROL_OUTPUT_EN_BIT, enable);
+    output_enable_reg = bitfield_write(output_enable_reg, BIT_MASK_1, SPI_HOST_CONTROL_OUTPUT_EN_BIT, enable);
     // mmio_region_write32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET, output_enable_reg);
     spi_host_peri->CONTROL = output_enable_reg;
 }
