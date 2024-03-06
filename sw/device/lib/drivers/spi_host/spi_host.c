acccc16d54dca9de72014a4a4335a367af182f52
Llorenç Muela <llorenc-robert.muelahausmann@epfl.ch>
2024-03-06 21:13:33 +0100

    Changed all mmio in spi_host.c file

--- sw/device/lib/drivers/spi_host/spi_host.c
+++ sw/device/lib/drivers/spi_host/spi_host.c
@@ -127,83 +127,117 @@ volatile spi_ch_status_t spi_get_rx_channel_status(const spi_host_t *spi) {
 }
 
 volatile uint32_t spi_get_csid(const spi_host_t *spi) {
-    return mmio_region_read32(spi->base_addr, SPI_HOST_CSID_REG_OFFSET);
+    // return mmio_region_read32(spi->base_addr, SPI_HOST_CSID_REG_OFFSET);
+    return spi_host_peri->CSID;
 }
 
 // SPI set functions
 
 void spi_sw_reset(const spi_host_t *spi) {
-    volatile uint32_t ctrl_reg = mmio_region_read32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET);
+    // volatile uint32_t ctrl_reg = mmio_region_read32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET);
+    volatile uint32_t ctrl_reg = spi_host_peri->CONTROL;
     ctrl_reg = bitfield_bit32_write(ctrl_reg, SPI_HOST_CONTROL_SW_RST_BIT, 1);
-    mmio_region_write32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET, ctrl_reg);
+    // mmio_region_write32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET, ctrl_reg);
+    spi_host_peri->CONTROL = ctrl_reg;
 }
 
 void spi_set_enable(const spi_host_t *spi, bool enable) {
-    volatile uint32_t ctrl_reg = mmio_region_read32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET);
+    // volatile uint32_t ctrl_reg = mmio_region_read32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET);
+    volatile uint32_t ctrl_reg = spi_host_peri->CONTROL;
     ctrl_reg = bitfield_bit32_write(ctrl_reg, SPI_HOST_CONTROL_SPIEN_BIT, enable);
-    mmio_region_write32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET, ctrl_reg);
+    // mmio_region_write32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET, ctrl_reg);
+    spi_host_peri->CONTROL = ctrl_reg;
 }
 
 void spi_set_tx_watermark(const spi_host_t *spi, uint8_t watermark) {
-    volatile uint32_t ctrl_reg = mmio_region_read32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET);
+    // volatile uint32_t ctrl_reg = mmio_region_read32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET);
+    volatile uint32_t ctrl_reg = spi_host_peri->CONTROL;
     ctrl_reg = bitfield_field32_write(ctrl_reg, SPI_HOST_CONTROL_TX_WATERMARK_FIELD, watermark);
-    mmio_region_write32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET, ctrl_reg);
+    // mmio_region_write32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET, ctrl_reg);
+    spi_host_peri->CONTROL = ctrl_reg;
 }
 
 void spi_set_rx_watermark(const spi_host_t *spi, uint8_t watermark) {
-    volatile uint32_t ctrl_reg = mmio_region_read32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET);
+    // volatile uint32_t ctrl_reg = mmio_region_read32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET);
+    volatile uint32_t ctrl_reg = spi_host_peri->CONTROL;
     ctrl_reg = bitfield_field32_write(ctrl_reg, SPI_HOST_CONTROL_RX_WATERMARK_FIELD, watermark);
-    mmio_region_write32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET, ctrl_reg);
+    // mmio_region_write32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET, ctrl_reg);
+    spi_host_peri->CONTROL = ctrl_reg;
 }
 
 void spi_set_configopts(const spi_host_t *spi, uint32_t csid, const uint32_t conf_reg) {
-    mmio_region_write32(spi->base_addr, sizeof(uint32_t) * csid + SPI_HOST_CONFIGOPTS_0_REG_OFFSET, conf_reg);
+    // mmio_region_write32(spi->base_addr, sizeof(uint32_t) * csid + SPI_HOST_CONFIGOPTS_0_REG_OFFSET, conf_reg);
+    switch (csid)
+    {
+    case 0:
+        spi_host_peri->CONFIGOPTS0 = conf_reg;
+        break;
+    case 1:
+        spi_host_peri->CONFIGOPTS1 = conf_reg;
+        break;
+    default:
+        break;
+    }
 }
 
 void spi_set_csid(const spi_host_t* spi, uint32_t csid) {
-    mmio_region_write32(spi->base_addr, SPI_HOST_CSID_REG_OFFSET, csid);
+    // mmio_region_write32(spi->base_addr, SPI_HOST_CSID_REG_OFFSET, csid);
+    spi_host_peri->CSID = csid;
 }
 
 void spi_set_command(const spi_host_t *spi, const uint32_t cmd_reg) {
-    mmio_region_write32(spi->base_addr, SPI_HOST_COMMAND_REG_OFFSET, cmd_reg);
+    // mmio_region_write32(spi->base_addr, SPI_HOST_COMMAND_REG_OFFSET, cmd_reg);
+    spi_host_peri->COMMAND = cmd_reg;
 }
 
 void spi_write_word(const spi_host_t *spi, uint32_t wdata) {
-    mmio_region_write32(spi->base_addr, SPI_HOST_TXDATA_REG_OFFSET, wdata);
+    // mmio_region_write32(spi->base_addr, SPI_HOST_TXDATA_REG_OFFSET, wdata);
+    spi_host_peri->TXDATA = wdata;
 }
 
 void spi_read_word(const spi_host_t *spi, uint32_t* dst) {
-    *dst = mmio_region_read32(spi->base_addr, SPI_HOST_RXDATA_REG_OFFSET);
+    // *dst = mmio_region_read32(spi->base_addr, SPI_HOST_RXDATA_REG_OFFSET);
+    *dst = spi_host_peri->RXDATA;
 }
 
 void spi_enable_evt_intr(const spi_host_t *spi, bool enable) {
-    volatile uint32_t intr_enable_reg = mmio_region_read32(spi->base_addr, SPI_HOST_INTR_ENABLE_REG_OFFSET);
+    // volatile uint32_t intr_enable_reg = mmio_region_read32(spi->base_addr, SPI_HOST_INTR_ENABLE_REG_OFFSET);
+    volatile uint32_t intr_enable_reg = spi_host_peri->INTR_ENABLE;
     intr_enable_reg = bitfield_bit32_write(intr_enable_reg, SPI_HOST_INTR_ENABLE_SPI_EVENT_BIT, enable);
-    mmio_region_write32(spi->base_addr, SPI_HOST_INTR_ENABLE_REG_OFFSET, intr_enable_reg);
+    // mmio_region_write32(spi->base_addr, SPI_HOST_INTR_ENABLE_REG_OFFSET, intr_enable_reg);
+    spi_host_peri->INTR_ENABLE = intr_enable_reg;
 }
 
 void spi_enable_error_intr(const spi_host_t *spi, bool enable) {
-    volatile uint32_t intr_enable_reg = mmio_region_read32(spi->base_addr, SPI_HOST_INTR_ENABLE_REG_OFFSET);
+    // volatile uint32_t intr_enable_reg = mmio_region_read32(spi->base_addr, SPI_HOST_INTR_ENABLE_REG_OFFSET);
+    volatile uint32_t intr_enable_reg = spi_host_peri->INTR_ENABLE;
     intr_enable_reg = bitfield_bit32_write(intr_enable_reg, SPI_HOST_INTR_STATE_ERROR_BIT, enable);
-    mmio_region_write32(spi->base_addr, SPI_HOST_INTR_ENABLE_REG_OFFSET, intr_enable_reg);
+    // mmio_region_write32(spi->base_addr, SPI_HOST_INTR_ENABLE_REG_OFFSET, intr_enable_reg);
+    spi_host_peri->INTR_ENABLE = intr_enable_reg;
 }
 
 void spi_enable_rxwm_intr(const spi_host_t *spi, bool enable) {
-    volatile uint32_t intr_enable_reg = mmio_region_read32(spi->base_addr, SPI_HOST_EVENT_ENABLE_REG_OFFSET);
+    // volatile uint32_t intr_enable_reg = mmio_region_read32(spi->base_addr, SPI_HOST_EVENT_ENABLE_REG_OFFSET);
+    volatile uint32_t intr_enable_reg = spi_host_peri->EVENT_ENABLE;
     intr_enable_reg = bitfield_bit32_write(intr_enable_reg, SPI_HOST_EVENT_ENABLE_RXWM_BIT, enable);
-    mmio_region_write32(spi->base_addr, SPI_HOST_EVENT_ENABLE_REG_OFFSET, intr_enable_reg);
+    // mmio_region_write32(spi->base_addr, SPI_HOST_EVENT_ENABLE_REG_OFFSET, intr_enable_reg);
+    spi_host_peri->EVENT_ENABLE = intr_enable_reg;
 }
 
 void spi_enable_txempty_intr(const spi_host_t *spi, bool enable) {
-    volatile uint32_t intr_enable_reg = mmio_region_read32(spi->base_addr, SPI_HOST_EVENT_ENABLE_REG_OFFSET);
+    // volatile uint32_t intr_enable_reg = mmio_region_read32(spi->base_addr, SPI_HOST_EVENT_ENABLE_REG_OFFSET);
+    volatile uint32_t intr_enable_reg = spi_host_peri->EVENT_ENABLE;
     intr_enable_reg = bitfield_bit32_write(intr_enable_reg, SPI_HOST_EVENT_ENABLE_TXEMPTY_BIT, enable);
-    mmio_region_write32(spi->base_addr, SPI_HOST_EVENT_ENABLE_REG_OFFSET, intr_enable_reg);
+    // mmio_region_write32(spi->base_addr, SPI_HOST_EVENT_ENABLE_REG_OFFSET, intr_enable_reg);
+    spi_host_peri->EVENT_ENABLE = intr_enable_reg;
 }
 
 void spi_output_enable(const spi_host_t *spi, bool enable){
-    volatile uint32_t output_enable_reg = mmio_region_read32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET);
+    // volatile uint32_t output_enable_reg = mmio_region_read32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET);
+    volatile uint32_t output_enable_reg = spi_host_peri->CONTROL;
     output_enable_reg = bitfield_bit32_write(output_enable_reg, SPI_HOST_CONTROL_OUTPUT_EN_BIT, enable);
-    mmio_region_write32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET, output_enable_reg);
+    // mmio_region_write32(spi->base_addr, SPI_HOST_CONTROL_REG_OFFSET, output_enable_reg);
+    spi_host_peri->CONTROL = output_enable_reg;
 }
 
 __attribute__((weak, optimize("O0"))) void handler_irq_spi(uint32_t id)
