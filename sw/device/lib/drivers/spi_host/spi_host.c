d886f27f6958936ef5987be9eb1db0959f1db642
Llorenç Muela <llorenc-robert.muelahausmann@epfl.ch>
2024-03-12 12:23:45 +0100

    Implemented casting to spi_host struct

--- sw/device/lib/drivers/spi_host/spi_host.c
+++ sw/device/lib/drivers/spi_host/spi_host.c
@@ -107,36 +107,36 @@ volatile spi_ch_status_t spi_get_rx_channel_status(const spi_host_t *spi) {
 }
 
 volatile uint32_t spi_get_csid(const spi_host_t *spi) {
-    return spi_host_peri->CSID;
+    return spi_cast_struct(spi)->CSID;
 }
 
 // SPI set functions
 
 void spi_sw_reset(const spi_host_t *spi) {
-    volatile uint32_t ctrl_reg = spi_host_peri->CONTROL;
+    volatile uint32_t ctrl_reg = spi_cast_struct(spi)->CONTROL;
     ctrl_reg = bitfield_write(ctrl_reg, BIT_MASK_1, SPI_HOST_CONTROL_SW_RST_BIT, 1);
-    spi_host_peri->CONTROL = ctrl_reg;
+    spi_cast_struct(spi)->CONTROL = ctrl_reg;
 }
 
 void spi_set_enable(const spi_host_t *spi, bool enable) {
-    volatile uint32_t ctrl_reg = spi_host_peri->CONTROL;
+    volatile uint32_t ctrl_reg = spi_cast_struct(spi)->CONTROL;
     ctrl_reg = bitfield_write(ctrl_reg, BIT_MASK_1, SPI_HOST_CONTROL_SPIEN_BIT, enable);
-    spi_host_peri->CONTROL = ctrl_reg;
+    spi_cast_struct(spi)->CONTROL = ctrl_reg;
 }
 
 spi_watermark_flags_e spi_set_tx_watermark(const spi_host_t *spi, uint8_t watermark) {
     if (watermark > SPI_HOST_PARAM_TX_DEPTH) return SPI_WATERMARK_EXCEEDS;
-    volatile uint32_t ctrl_reg = spi_host_peri->CONTROL;
+    volatile uint32_t ctrl_reg = spi_cast_struct(spi)->CONTROL;
     ctrl_reg = bitfield_write(ctrl_reg, SPI_HOST_CONTROL_TX_WATERMARK_MASK, SPI_HOST_CONTROL_TX_WATERMARK_OFFSET, watermark);
-    spi_host_peri->CONTROL = ctrl_reg;
+    spi_cast_struct(spi)->CONTROL = ctrl_reg;
     return SPI_WATERMARK_OK;
 }
 
 spi_watermark_flags_e spi_set_rx_watermark(const spi_host_t *spi, uint8_t watermark) {
     if (watermark > SPI_HOST_PARAM_RX_DEPTH) return SPI_WATERMARK_EXCEEDS;
-    volatile uint32_t ctrl_reg = spi_host_peri->CONTROL;
+    volatile uint32_t ctrl_reg = spi_cast_struct(spi)->CONTROL;
     ctrl_reg = bitfield_write(ctrl_reg, SPI_HOST_CONTROL_RX_WATERMARK_MASK, SPI_HOST_CONTROL_RX_WATERMARK_OFFSET, watermark);
-    spi_host_peri->CONTROL = ctrl_reg;
+    spi_cast_struct(spi)->CONTROL = ctrl_reg;
     return SPI_WATERMARK_OK;
 }
 
@@ -146,10 +146,10 @@ spi_configopts_flags_e spi_set_configopts(const spi_host_t *spi, uint32_t csid, 
     switch (csid)
     {
     case 0:
-        spi_host_peri->CONFIGOPTS0 = conf_reg;
+        spi_cast_struct(spi)->CONFIGOPTS0 = conf_reg;
         break;
     case 1:
-        spi_host_peri->CONFIGOPTS1 = conf_reg;
+        spi_cast_struct(spi)->CONFIGOPTS1 = conf_reg;
         break;
     default:
         return SPI_CONFIGOPTS_CSID_INVALID;
@@ -160,7 +160,7 @@ spi_configopts_flags_e spi_set_configopts(const spi_host_t *spi, uint32_t csid, 
 
 spi_csid_flags_e spi_set_csid(const spi_host_t* spi, uint32_t csid) {
     if (csid >= SPI_HOST_PARAM_NUM_C_S) return SPI_CSID_INVALID;
-    spi_host_peri->CSID = csid;
+    spi_cast_struct(spi)->CSID = csid;
     return SPI_CSID_OK;
 }
 
@@ -168,50 +168,50 @@ spi_command_flags_e spi_set_command(const spi_host_t *spi, const uint32_t cmd_re
     if (bitfield_read(spi_get_status(spi), SPI_HOST_STATUS_CMDQD_MASK, SPI_HOST_STATUS_CMDQD_OFFSET) >= SPI_HOST_PARAM_CMD_DEPTH) return SPI_COMMAND_QUEUE_FULL;
     // TODO: add a definition for the number 3
     if (bitfield_read(cmd_reg, SPI_HOST_COMMAND_SPEED_MASK, SPI_HOST_COMMAND_SPEED_OFFSET) == 3) return SPI_COMMAND_SPEED_INVALID;
-    spi_host_peri->COMMAND = cmd_reg;
+    spi_cast_struct(spi)->COMMAND = cmd_reg;
     return SPI_COMMAND_OK;
 }
 
 spi_read_write_flags_e spi_write_word(const spi_host_t *spi, uint32_t wdata) {
     if (spi_get_tx_queue_depth(spi) >= SPI_HOST_PARAM_TX_DEPTH) return SPI_READ_WRITE_QUEUE_FULL;
-    spi_host_peri->TXDATA = wdata;
+    spi_cast_struct(spi)->TXDATA = wdata;
     return SPI_READ_WRITE_OK;
 }
 
 spi_read_write_flags_e spi_read_word(const spi_host_t *spi, uint32_t* dst) {
     if (spi_get_rx_queue_depth(spi) == 0) return SPI_READ_WRITE_QUEUE_EMPTY;
-    *dst = spi_host_peri->RXDATA;
+    *dst = spi_cast_struct(spi)->RXDATA;
     return SPI_READ_WRITE_OK;
 }
 
 void spi_enable_evt_intr(const spi_host_t *spi, bool enable) {
-    volatile uint32_t intr_enable_reg = spi_host_peri->INTR_ENABLE;
+    volatile uint32_t intr_enable_reg = spi_cast_struct(spi)->INTR_ENABLE;
     intr_enable_reg = bitfield_write(intr_enable_reg, BIT_MASK_1, SPI_HOST_INTR_ENABLE_SPI_EVENT_BIT, enable);
-    spi_host_peri->INTR_ENABLE = intr_enable_reg;
+    spi_cast_struct(spi)->INTR_ENABLE = intr_enable_reg;
 }
 
 void spi_enable_error_intr(const spi_host_t *spi, bool enable) {
-    volatile uint32_t intr_enable_reg = spi_host_peri->INTR_ENABLE;
+    volatile uint32_t intr_enable_reg = spi_cast_struct(spi)->INTR_ENABLE;
     intr_enable_reg = bitfield_write(intr_enable_reg, BIT_MASK_1, SPI_HOST_INTR_STATE_ERROR_BIT, enable);
-    spi_host_peri->INTR_ENABLE = intr_enable_reg;
+    spi_cast_struct(spi)->INTR_ENABLE = intr_enable_reg;
 }
 
 void spi_enable_rxwm_intr(const spi_host_t *spi, bool enable) {
-    volatile uint32_t intr_enable_reg = spi_host_peri->EVENT_ENABLE;
+    volatile uint32_t intr_enable_reg = spi_cast_struct(spi)->EVENT_ENABLE;
     intr_enable_reg = bitfield_write(intr_enable_reg, BIT_MASK_1, SPI_HOST_EVENT_ENABLE_RXWM_BIT, enable);
-    spi_host_peri->EVENT_ENABLE = intr_enable_reg;
+    spi_cast_struct(spi)->EVENT_ENABLE = intr_enable_reg;
 }
 
 void spi_enable_txempty_intr(const spi_host_t *spi, bool enable) {
-    volatile uint32_t intr_enable_reg = spi_host_peri->EVENT_ENABLE;
+    volatile uint32_t intr_enable_reg = spi_cast_struct(spi)->EVENT_ENABLE;
     intr_enable_reg = bitfield_write(intr_enable_reg, BIT_MASK_1, SPI_HOST_EVENT_ENABLE_TXEMPTY_BIT, enable);
-    spi_host_peri->EVENT_ENABLE = intr_enable_reg;
+    spi_cast_struct(spi)->EVENT_ENABLE = intr_enable_reg;
 }
 
 void spi_output_enable(const spi_host_t *spi, bool enable){
-    volatile uint32_t output_enable_reg = spi_host_peri->CONTROL;
+    volatile uint32_t output_enable_reg = spi_cast_struct(spi)->CONTROL;
     output_enable_reg = bitfield_write(output_enable_reg, BIT_MASK_1, SPI_HOST_CONTROL_OUTPUT_EN_BIT, enable);
-    spi_host_peri->CONTROL = output_enable_reg;
+    spi_cast_struct(spi)->CONTROL = output_enable_reg;
 }
 
 __attribute__((weak, optimize("O0"))) void handler_irq_spi(uint32_t id)
