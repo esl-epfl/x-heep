ecbaf7d53dcd99b11d14e99abb083a84d5c0158f
Luigi2898 <luigi.giuffrida.98@gmail.com>
2024-05-29 16:20:58 +0200

    Add HAL to example app

--- sw/applications/example_dma_signed/main.c
+++ sw/applications/example_dma_signed/main.c
@@ -11,33 +11,20 @@
 #include "csr.h"
 #include "rv_plic.h"
 
-#define DMA_REGISTER_SIZE_BYTES sizeof(int)
-#define DMA_SELECTION_OFFSET_START 0
-
-static inline void write_register(uint32_t p_val,
-                                  uint32_t p_offset,
-                                  uint32_t p_mask,
-                                  uint8_t p_sel,
-                                  dma *peri)
-{
-    /*
-     * The index is computed to avoid needing to access the structure
-     * as a structure.
-     */
-    uint8_t index = p_offset / DMA_REGISTER_SIZE_BYTES;
-    /*
-     * An intermediate variable "value" is used to prevent writing twice into
-     * the register.
-     */
-    uint32_t value = ((uint32_t *)peri)[index];
-    value &= ~(p_mask << p_sel);
-    value |= (p_val & p_mask) << p_sel;
-    ((uint32_t *)peri)[index] = value;
-}
-
 uint32_t incr[] = {1, 2, 4};
 
 uint32_t data_type[] = {DMA_DATA_TYPE_BYTE, DMA_DATA_TYPE_HALF_WORD, DMA_DATA_TYPE_WORD};
+
+#define wait_dma                              \
+    while (!dma_is_ready())                   \
+    {                                         \
+        CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8); \
+        if (dma_is_ready() == 0)              \
+        {                                     \
+            wait_for_interrupt();             \
+        }                                     \
+        CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);   \
+    }
 
 #define launch(src_type, dst_type, dma, DMA_SRC_TYPE, DMA_DST_TYPE) \
     src_type src[5] = {-1, -2, -3, -4, -5};                         \
@@ -53,15 +40,7 @@ uint32_t data_type[] = {DMA_DATA_TYPE_BYTE, DMA_DATA_TYPE_HALF_WORD, DMA_DATA_TY
     dma->MODE = DMA_TRANS_MODE_SINGLE;                              \
     dma->SIZE_D2 = 0;                                               \
     dma->SIZE_D1 = 5 * sizeof(dst[0]);                              \
-    while (!dma_is_ready())                                         \
-    {                                                               \
-        CSR_CLEAR_BITS(CSR_REG_MSTATUS, 0x8);                       \
-        if (dma_is_ready() == 0)                                    \
-        {                                                           \
-            wait_for_interrupt();                                   \
-        }                                                           \
-        CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);                         \
-    }
+    wait_dma;
 
 #define check_results                         \
     for (int i = 0; i < 5; i++)               \
@@ -74,6 +53,35 @@ uint32_t data_type[] = {DMA_DATA_TYPE_BYTE, DMA_DATA_TYPE_HALF_WORD, DMA_DATA_TY
             return EXIT_FAILURE;              \
         }                                     \
     }
+
+#define launch_with_HAL(src_TYPE, dst_TYPE, dma, DMA_SRC_TYPE, DMA_DST_TYPE) \
+    src_TYPE src[5] = {-1, -2, -3, -4, -5};                                  \
+    dst_TYPE dst[5] = {0};                                                   \
+    dma_target_t src_target = {0};                                           \
+    src_target.ptr = src;                                                    \
+    src_target.inc_du = 1;                                                   \
+    src_target.size_du = 5;                                                  \
+    src_target.type = DMA_SRC_TYPE;                                          \
+    dma_target_t dst_target = {0};                                           \
+    dst_target.ptr = dst;                                                    \
+    dst_target.inc_du = 1;                                                   \
+    dst_target.size_du = 5;                                                  \
+    dst_target.type = DMA_DST_TYPE;                                          \
+    dma_trans_t trans = {0};                                                 \
+    trans.src = &src_target;                                                 \
+    trans.dst = &dst_target;                                                 \
+    trans.src_type = DMA_SRC_TYPE;                                           \
+    trans.dst_type = DMA_DST_TYPE;                                           \
+    trans.sign_ext = 1;                                                      \
+    trans.mode = DMA_TRANS_MODE_SINGLE;                                      \
+    if (dma_validate_transaction(&trans, DMA_DO_NOT_ENABLE_REALIGN, 1) != 0) \
+    {                                                                        \
+        printf("DMA FAIL");                                                  \
+        return EXIT_FAILURE;                                                 \
+    }                                                                        \
+    dma_load_transaction(&trans);                                            \
+    dma_launch(&trans);                                                      \
+    wait_dma;
 
 int main(int argc, char *argv[])
 {
@@ -148,6 +156,74 @@ int main(int argc, char *argv[])
     } while (0);
 
     printf("Test int32_t -> int32_t succeded\n");
+
+    dma_init(peri);
+
+    do
+    {
+        // Launch the DMA
+        launch_with_HAL(int8_t, int8_t, peri, DMA_DATA_TYPE_BYTE, DMA_DATA_TYPE_BYTE);
+
+        // Check the results
+        check_results;
+    } while (0);
+ 
+    printf("Test int8_t -> int8_t succeded with HAL\n");
+
+    do
+    {
+        // Launch the DMA
+        launch_with_HAL(int8_t, int16_t, peri, DMA_DATA_TYPE_BYTE, DMA_DATA_TYPE_HALF_WORD);
+
+        // Check the results
+        check_results;
+    } while (0);
+
+    printf("Test int8_t -> int16_t succeded with HAL\n");
+
+    do
+    {
+        // Launch the DMA
+        launch_with_HAL(int8_t, int32_t, peri, DMA_DATA_TYPE_BYTE, DMA_DATA_TYPE_WORD);
+
+        // Check the results
+        check_results;
+    } while (0);
+
+    printf("Test int8_t -> int32_t succeded with HAL\n");
+
+    do
+    {
+        // Launch the DMA
+        launch_with_HAL(int16_t, int16_t, peri, DMA_DATA_TYPE_HALF_WORD, DMA_DATA_TYPE_HALF_WORD);
+
+        // Check the results
+        check_results;
+    } while (0);
+
+    printf("Test int16_t -> int16_t succeded with HAL\n");
+
+    do
+    {
+        // Launch the DMA
+        launch_with_HAL(int16_t, int32_t, peri, DMA_DATA_TYPE_HALF_WORD, DMA_DATA_TYPE_WORD);
+
+        // Check the results
+        check_results;
+    } while (0);
+
+    printf("Test int16_t -> int32_t succeded with HAL\n");
+
+    do
+    {
+        // Launch the DMA
+        launch_with_HAL(int32_t, int32_t, peri, DMA_DATA_TYPE_WORD, DMA_DATA_TYPE_WORD);
+
+        // Check the results
+        check_results;
+    } while (0);
+
+    printf("Test int32_t -> int32_t succeded with HAL\n");
 
     printf("All tests succeded\n");
 
