f6869d2de6f73080dd28e9b7bcce5b1dc5bd3d0a
Juan Sapriza <juan.sapriza@epfl.ch>
2024-06-03 12:37:32 +0200

    Changed names and added PRINTF macro

--- sw/applications/example_asm/main.c
+++ sw/applications/example_asm/main.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include "constants.h"
+#include "x-heep.h"
+
+/* By default, printfs are activated for FPGA and disabled for simulation. */
+#define PRINTF_IN_FPGA  1
+#define PRINTF_IN_SIM   0
+
+#if TARGET_SIM && PRINTF_IN_SIM
+        #define PRINTF(fmt, ...)    printf(fmt, ## __VA_ARGS__)
+#elif TARGET_PYNQ_Z2 && PRINTF_IN_FPGA
+    #define PRINTF(fmt, ...)    printf(fmt, ## __VA_ARGS__)
+#else
+    #define PRINTF(...)
+#endif
 
 // Declaration of the assembly function for the little Juan
 extern int add_asm_function(int a, int b);
-extern int multiply_by_const( int a);
+extern int mul_by_const_asm_function( int a);
 
 int main() {
     int num1 = 10;
     int num2 = 20;
     int sum = add_asm_function(num1, num2);
-    int mul = multiply_by_const(num2);
+    int mul = mul_by_const_asm_function(num2);
 
-    printf("%d+%d=%d\n", num1, num2, sum);
-    printf("%d*%d=%d\n", num2, MULTIPLY_CONSTANT, mul );
-    return 0;   
+    PRINTF("%d+%d=%d\n", num1, num2, sum);
+    PRINTF("%d*%d=%d\n", num2, MULTIPLY_CONSTANT, mul );
+    
+    return EXIT_SUCCESS;   
 }
