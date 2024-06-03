11882960cea2cd20174f8fb1c611915048ad1f03
Juan Sapriza <juan.sapriza@epfl.ch>
2024-06-03 12:34:56 +0200

    Added compatibility with .S files

--- sw/applications/example_asm/add.s
+++ sw/applications/example_asm/add.s
@@ -1,6 +1,6 @@
     .section .text
-    .globl add
+    .globl add_asm_function
 
-add:
+add_asm_function:
     add a0, a0, a1   # Add the values in a0 and a1, store the result in a0
     ret              # Return from the function
--- sw/applications/example_asm/main.c
+++ sw/applications/example_asm/main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include "constants.h"
 
 // Declaration of the assembly function for the little Juan
-extern int add(int a, int b);
+extern int add_asm_function(int a, int b);
+extern int multiply_by_const( int a);
 
 int main() {
     int num1 = 10;
     int num2 = 20;
-    int result = add(num1, num2);
+    int sum = add_asm_function(num1, num2);
+    int mul = multiply_by_const(num2);
 
-    printf("%d+%d=%d\n", num1, num2, result);
-    return 0;
+    printf("%d+%d=%d\n", num1, num2, sum);
+    printf("%d*%d=%d\n", num2, MULTIPLY_CONSTANT, mul );
+    return 0;   
 }
