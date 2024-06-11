4900da1c11fb164277d783c53b37fb971a54c0f4
Juan Sapriza <juan.sapriza@epfl.ch>
2024-06-11 09:29:30 +0200

    Changed the default PRINTF_IN_SIM to 0 in all examples

--- sw/applications/example_im2col/im2col_lib.h
+++ sw/applications/example_im2col/im2col_lib.h
@@ -23,7 +23,7 @@
 
 // By default, printfs are activated for FPGA and for simulation.
 #define PRINTF_IN_FPGA  1
-#define PRINTF_IN_SIM   1
+#define PRINTF_IN_SIM   0
 #define DEBUG 0 // Set to 1 to enable debug prints
 #define TIMING 0 // Set to 1 to enable timing measurements
 
