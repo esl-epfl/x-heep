22f65141b5a6a27c22495fb3b75b5c0bf04a89c1
Juan Sapriza <juan.sapriza@epfl.ch>
2024-06-14 12:16:43 +0200

    Fixe copyright

--- sw/applications/example_dma_sdk/main.c
+++ sw/applications/example_dma_sdk/main.c
@@ -1,4 +1,4 @@
-// Copyright 2024 EPFL and Politecnico di Torino.
+// Copyright 2024 EPFL
 // Solderpad Hardware License, Version 2.1, see LICENSE.md for details.
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 //
