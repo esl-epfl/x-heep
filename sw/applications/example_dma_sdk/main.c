9071874ee4488273d5199014a0354b4b67b316a7
Luigi2898 <luigi.giuffrida.98@gmail.com>
2024-07-22 20:09:06 +0200

    Update example with new SDK macros

--- sw/applications/example_dma_sdk/main.c
+++ sw/applications/example_dma_sdk/main.c
@@ -20,23 +20,23 @@
 #include "csr.h"
 
 /* By default, printfs are activated for FPGA and disabled for simulation. */
-#define PRINTF_IN_FPGA  1
-#define PRINTF_IN_SIM   1
+#define PRINTF_IN_FPGA 1
+#define PRINTF_IN_SIM 0
 
 #if TARGET_SIM && PRINTF_IN_SIM
-        #define PRINTF(fmt, ...)    printf(fmt, ## __VA_ARGS__)
+#define PRINTF(fmt, ...) printf(fmt, ##__VA_ARGS__)
 #elif PRINTF_IN_FPGA && !TARGET_SIM
-    #define PRINTF(fmt, ...)    printf(fmt, ## __VA_ARGS__)
+#define PRINTF(fmt, ...) printf(fmt, ##__VA_ARGS__)
 #else
-    #define PRINTF(...)
+#define PRINTF(...)
 #endif
 
-#define SOURCE_BUFFER_SIZE_32b  5
-#define SOURCE_BUFFER_SIZE_16b  5
-#define SOURCE_BUFFER_SIZE_8b   5
-#define CONST_VALUE_32B         123
-#define CONST_VALUE_16B         123
-#define CONST_VALUE_8B          123
+#define SOURCE_BUFFER_SIZE_32b 5
+#define SOURCE_BUFFER_SIZE_16b 5
+#define SOURCE_BUFFER_SIZE_8b 5
+#define CONST_VALUE_32B 123
+#define CONST_VALUE_16B 123
+#define CONST_VALUE_8B 123
 
 static uint32_t source_32b[SOURCE_BUFFER_SIZE_32b];
 static uint32_t destin_32b[SOURCE_BUFFER_SIZE_32b];
@@ -55,22 +55,21 @@ uint32_t i;
 uint32_t errors = 0;
 
 #define FS_INITIAL 0x01
-int main(){
+int main()
+{
 
     CSR_SET_BITS(CSR_REG_MSTATUS, (FS_INITIAL << 13));
     dma_sdk_init();
 
-    dma *the_dma = dma_peri(0);
-
+    volatile dma *the_dma = dma_peri(0);
 
     DMA_FILL(source_32b, &value_32b, SOURCE_BUFFER_SIZE_32b, uint32_t, uint32_t, 0, the_dma);
     DMA_WAIT(0);
     DMA_COPY(destin_32b, source_32b, SOURCE_BUFFER_SIZE_32b, uint32_t, uint32_t, 0, the_dma);
     DMA_WAIT(0);
-    // dma_fill_32b( &source_32b, &value_32b, SOURCE_BUFFER_SIZE_32b, 0);
-    // dma_copy_32b( &destin_32b, &source_32b, SOURCE_BUFFER_SIZE_32b, 0);
 
-    for( i = 0; i < SOURCE_BUFFER_SIZE_32b; i++){
+    for (i = 0; i < SOURCE_BUFFER_SIZE_32b; i++)
+    {
         errors += destin_32b[i] != CONST_VALUE_32B;
     }
 
@@ -79,24 +78,22 @@ int main(){
     DMA_COPY(destin_16b, source_16b, SOURCE_BUFFER_SIZE_16b, uint16_t, uint16_t, 0, the_dma);
     DMA_WAIT(0);
 
-    for( i = 0; i < SOURCE_BUFFER_SIZE_16b; i++){
+    for (i = 0; i < SOURCE_BUFFER_SIZE_16b; i++)
+    {
         errors += destin_16b[i] != CONST_VALUE_16B;
     }
-    // printf("Errors:%d\n\r",errors );
 
     DMA_FILL(source_8b, &value_8b, SOURCE_BUFFER_SIZE_8b, uint8_t, uint8_t, 0, the_dma);
     DMA_WAIT(0);
     DMA_COPY(destin_8b, source_8b, SOURCE_BUFFER_SIZE_8b, uint8_t, uint8_t, 0, the_dma);
     DMA_WAIT(0);
-
-    // // dma_fill_8b( &source_8b, &value_8b, SOURCE_BUFFER_SIZE_8b, 0);
-    // // dma_copy_8b( &destin_8b, &source_8b, SOURCE_BUFFER_SIZE_8b, 0);
-
-    for( i = 0; i < SOURCE_BUFFER_SIZE_8b; i++){
+    
+    for (i = 0; i < SOURCE_BUFFER_SIZE_8b; i++)
+    {
         errors += destin_8b[i] != CONST_VALUE_8B;
     }
 
-    PRINTF("Errors:%d\n\r",errors );
+    PRINTF("Errors:%d\n\r", errors);
 
     return errors ? EXIT_FAILURE : EXIT_SUCCESS;
 }
