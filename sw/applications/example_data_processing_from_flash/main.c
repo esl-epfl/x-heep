abc26aa9263d3cb1fe306e12d9d4f14c7cfcfcc0
FrancescoPoluzzi <francesco.poluzzi@epfl.ch>
2024-07-29 18:16:27 +0200

    Added automatic data/goldenmodel generation

--- sw/applications/example_data_processing_from_flash/main.c
+++ sw/applications/example_data_processing_from_flash/main.c
@@ -10,11 +10,11 @@
  * @file main.c
  * @brief Example of data processing (matrix multiplication) reading data from flash memory
  *
- * Simple example that read a matrix from flash memory in 2 step and performs
+ * Simple example that read a matrix from flash memory in many step and performs
  * matrix multiplication. This is useful for applications where the
  * data size does not fit in the available SRAM memory, so some data needs to be
  * stored as "flash_only" and read trough the spi interface. This usually requires
- * filling a buffer and tiling the operation.
+ * filling a buffer and tiling the data processing.
 */
 
 #include <stdio.h>
@@ -25,6 +25,8 @@
 #include "w25q128jw.h"
 #include "main.h"
 #include "dma_sdk.h"
+
+#define TILING_ROWS 2
 
  /* By default, printfs are activated for FPGA and disabled for simulation. */
 #define PRINTF_IN_FPGA  1
@@ -37,10 +39,11 @@
     #define PRINTF(...)
 #endif
 
-int32_t buffer_data[8] = {0};
-int32_t output_matrix[16] = {0};
+int32_t buffer_data[MATRIX_SIZE*TILING_ROWS] = {0};
+int32_t output_matrix[MATRIX_SIZE*MATRIX_SIZE] = {0};
 
 int main(int argc, char *argv[]) {
+
     soc_ctrl_t soc_ctrl;
     soc_ctrl.base_addr = mmio_region_from_addr((uintptr_t)SOC_CTRL_START_ADDRESS);
 
@@ -59,27 +62,25 @@ int main(int argc, char *argv[]) {
     spi_host_t* spi = spi_flash;
 
     // Init SPI host and SPI<->Flash bridge parameters
-    if (w25q128jw_init(spi) != FLASH_OK) return EXIT_FAILURE;
-
-    // read first half matrix A from flash and perform matmul
-    if(fill_buffer(&A[0], buffer_data, 8)!=FLASH_OK){
-        PRINTF("Error reading from flash\n");
+    if (w25q128jw_init(spi) != FLASH_OK){
+        PRINTF("Error initializing SPI flash\n");
         return EXIT_FAILURE;
-    }
-    matmul(buffer_data, B, output_matrix, 2, 4, 4);
+    } 
 
-    // read second half matrix A from flash and perform matmul
-    if (fill_buffer(&A[8], buffer_data, 8)!=FLASH_OK){
-        PRINTF("Error reading from flash\n");
-        return EXIT_FAILURE;
-    }
-    matmul(buffer_data, B, &output_matrix[8], 2, 4, 4);
-
-    for(int i = 0; i < 16; i++){
-        if (output_matrix[i] != C[i]){
-             PRINTF("Result[%d][%d]:golden model   %d : %d\n", (i/4), (i%4), output_matrix[i], C[i]);
+    for (int i = 0; i < MATRIX_SIZE; i+=TILING_ROWS) {
+        // read first half matrix A from flash and perform matmul
+        if(fill_buffer(&A[i*MATRIX_SIZE], buffer_data, MATRIX_SIZE*TILING_ROWS)!=FLASH_OK){
+            PRINTF("Error reading from flash\n");
             return EXIT_FAILURE;
         }
+        matmul(buffer_data, B, &output_matrix[i*MATRIX_SIZE], TILING_ROWS, MATRIX_SIZE, MATRIX_SIZE);
+    }
+
+    for(int i = 0; i < MATRIX_SIZE*MATRIX_SIZE; i++){
+        if (output_matrix[i] != C[i]){
+             PRINTF("Result[%d][%d]:golden model   %d : %d\n", (i/MATRIX_SIZE), (i % MATRIX_SIZE), output_matrix[i], C[i]);
+            return EXIT_FAILURE;
+       }
     }
     PRINTF("All tests passed!\n");
     return EXIT_SUCCESS;
