11899582fb64a0e349c42e346d3d200a71441900
FrancescoPoluzzi <francesco.poluzzi@epfl.ch>
2024-08-21 17:27:57 +0200

    update exmaple_timer_sdk app

--- sw/applications/example_timer_sdk/main.c
+++ sw/applications/example_timer_sdk/main.c
@@ -5,12 +5,11 @@
 // File: example_timer_sdk.c
 // Author: Juan Sapriza, Francesco Poluzzi
 // Date: 23/07/2024
-// Author: Juan Sapriza, Francesco Poluzzi
-// Date: 23/07/2024
 // Description: Example application to test the Timer SDK. Will count the time to execute a few short tasks.
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <math.h>
 #include "core_v_mini_mcu.h"
 #include "timer_sdk.h"
 #include "x-heep.h"
@@ -21,9 +20,9 @@
 #define PRINTF_IN_SIM   0
 
 /* Error tolerances for the tests. */
-#define CYCLE_TOLERANCE  1         // cycles tolerance for simple timer reads
-#define INTERRUPT_TOLERANCE 50     // cycles tolerance for timer interrupt
-#define TIMER_WAIT_TOLERANCE 30   // cycles tolerance for timer wait
+#define CYCLE_TOLERANCE  2         // cycles tolerance for simple timer reads
+#define INTERRUPT_TOLERANCE 60     // cycles tolerance for timer interrupt
+#define TIMER_WAIT_TOLERANCE 20   // milliseconds tolerance for timer wait
 
 #if TARGET_SIM && PRINTF_IN_SIM
         #define PRINTF(fmt, ...)    printf(fmt, ## __VA_ARGS__)
@@ -43,12 +42,13 @@ int main(){
     uint32_t i = 0;
     uint32_t timer_cycles;
     uint32_t nop_cycles[4];
-    // Get current Clock Frequency
+
+    // Get current Frequency
     soc_ctrl_t soc_ctrl;
     soc_ctrl.base_addr = mmio_region_from_addr((uintptr_t)SOC_CTRL_START_ADDRESS);
     uint32_t freq_hz = soc_ctrl_get_frequency(&soc_ctrl);
 
-    timer_cycles_init();               // Init the timer SDK for clock cycles
+    timer_cycles_init();         // Init the timer SDK for clock cycles
     timer_start();              // Start counting the time
     nop_cycles[0] = timer_stop();  // Stop counting the time
     PRINTF("0 NOPs:\t%d cc\n\r", nop_cycles[0] );
@@ -80,30 +80,30 @@ int main(){
 
     timer_cycles_init();
     timer_irq_enable();
-    timer_arm_start(1000);
-    timer_start();              
+    timer_arm_start(1000);            
     asm volatile ("wfi");       // Wait for interrupt
     timer_cycles = timer_stop();  
     if(abs(timer_cycles-1000) < INTERRUPT_TOLERANCE){ 
-        PRINTF("Timer thershold interrupt working\n" );
+        PRINTF("Timer threshold interrupt working\n" );
     } else {
-        PRINTF("Timer thershold interrupt failed\n\r");
+        PRINTF("Timer threshold interrupt failed\n\r");
         return EXIT_FAILURE;
     }
     
-    timer_microseconds_init();           // Init the timer SDK for microseconds
+    timer_cycles_init();           // Init the timer SDK for microseconds
     timer_start(); 
     for(i = 0; i < 1000; i++){
         asm volatile ("nop");
     }
     timer_cycles = timer_stop();  
-    PRINTF("Microseconds for 1000 NOPs:\t%d μs\n\r", timer_cycles );
+    PRINTF("Microseconds for 1000 NOPs:\t%d μs\n\r", (uint32_t)get_time(timer_cycles) );
 
     PRINTF("Wait 5 seconds\n\r");
-    timer_wait_ms(5000);       // Wait for 5 seconds
-    timer_cycles = timer_stop(); 
+    timer_wait_us(5000000);       // Wait for 5 seconds 
+    timer_cycles = timer_stop();     
     PRINTF("Done\n\r");
-    if(abs(timer_cycles-5000*1000)*(freq_hz/FREQ_1MHz) > TIMER_WAIT_TOLERANCE){ 
+
+    if(abs(timer_cycles-(5*freq_hz)) > TIMER_WAIT_TOLERANCE){ 
         PRINTF("Timer wait failed\n\r");
         return EXIT_FAILURE;
     }
@@ -111,3 +111,4 @@ int main(){
     PRINTF("All tests passed\n\r");
     return EXIT_SUCCESS;
 }
+
