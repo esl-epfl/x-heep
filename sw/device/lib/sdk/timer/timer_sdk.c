5f7becd9939933910acd01fc14382ad1029338bb
FrancescoPoluzzi <francesco.poluzzi@epfl.ch>
2024-08-21 17:26:19 +0200

    added documentation and performed requested fixes on sdk

--- sw/device/lib/sdk/timer/timer_sdk.c
+++ sw/device/lib/sdk/timer/timer_sdk.c
@@ -5,8 +5,6 @@
 // File: timer_sdk.c
 // Author: Michele Caon, Francesco Poluzzi
 // Date: 23/07/2024
-// Author: Michele Caon, Francesco Poluzzi
-// Date: 23/07/2024
 // Description: Timer functions
 
 #include <stdint.h>
@@ -21,7 +19,6 @@
 /******************************/
 
 // Timer value
-uint32_t timer_value = 0;
 uint32_t hw_timer_value = 0;
 
 rv_timer_t timer;
@@ -42,7 +39,7 @@ rv_timer_tick_params_t tick_params;
 /* ---- FUNCTION IMPLEMENTATION ---- */
 /*************************************/
 
-uint32_t hw_timer_get_cycles()
+uint32_t timer_get_cycles()
 {
     uint64_t cycle_count;
     rv_timer_counter_read(&timer, 0, &cycle_count);
@@ -59,11 +56,10 @@ void timer_irq_clear()
   rv_timer_irq_clear(&timer, 0, 0);
 }
 
-
 void timer_arm_start(uint32_t threshold)
 {
     rv_timer_arm(&timer, 0, 0, threshold);
-    rv_timer_counter_set_enabled(&timer, 0, kRvTimerEnabled);
+    timer_start();
 }
 
 void timer_arm_stop()
@@ -71,15 +67,28 @@ void timer_arm_stop()
     rv_timer_counter_set_enabled(&timer, 0, kRvTimerDisabled);
 }
 
-void hw_timer_start()
+
+void timer_arm_set(uint32_t threshold)
 {
-    hw_timer_value = -hw_timer_get_cycles();
+    rv_timer_arm(&timer, 0, 0, threshold);
 }
 
-uint32_t hw_timer_stop()
+void timer_start()
 {
-    hw_timer_value += hw_timer_get_cycles();
-    return hw_timer_value;
+    hw_timer_value = -timer_get_cycles();
+    rv_timer_counter_set_enabled(&timer, 0, kRvTimerEnabled);
+}
+
+void timer_reset()
+{
+    rv_timer_reset(&timer);
+}
+
+uint32_t timer_stop()
+{
+    hw_timer_value += timer_get_cycles();
+    rv_timer_counter_set_enabled(&timer, 0, kRvTimerDisabled);
+    return  hw_timer_value;
 }
 
 // Initialize the timer
@@ -91,34 +100,23 @@ void timer_cycles_init()
     uint32_t freq_hz = soc_ctrl_get_frequency(&soc_ctrl);
 
     // Initialize the timer
+    timer_reset();
     rv_timer_init(timer_base, timer_cfg, &timer);
     rv_timer_approximate_tick_params(freq_hz, freq_hz, &tick_params);
     rv_timer_set_tick_params(&timer, 0, tick_params);
-    rv_timer_counter_set_enabled(&timer, 0, kRvTimerEnabled);
 }
 
 // Initialize the timer
-void  timer_microseconds_init()
+void timer_wait_us(uint32_t us)
 {
     // Get current Frequency
     soc_ctrl_t soc_ctrl;
     soc_ctrl.base_addr = mmio_region_from_addr((uintptr_t)SOC_CTRL_START_ADDRESS);
     uint32_t freq_hz = soc_ctrl_get_frequency(&soc_ctrl);
 
-    // Initialize the timer
-    rv_timer_init(timer_base, timer_cfg, &timer);
-    rv_timer_approximate_tick_params(freq_hz, FREQ_1MHz, &tick_params);
-    rv_timer_set_tick_params(&timer, 0, tick_params);
-    rv_timer_counter_set_enabled(&timer, 0, kRvTimerEnabled);
-}
-
-// Initialize the timer
-void timer_wait_ms(uint32_t ms)
-{
-     timer_microseconds_init();
+    timer_cycles_init();
     timer_irq_enable();
-    timer_arm_start(ms*1000);
-    timer_start();              
+    timer_arm_start(us*(freq_hz/1000000)-50); // 50 cycles for taking into account initialization        
     asm volatile ("wfi");
     timer_irq_clear();
     return;
@@ -131,4 +129,13 @@ void enable_timer_interrupt()
     // Set mie.MEIE bit to one to enable machine-level timer interrupts
     const uint32_t mask = 1 << 7;
     CSR_SET_BITS(CSR_REG_MIE, mask);
+}
+
+float get_time(uint32_t cycles){
+    // Get current Frequency
+    soc_ctrl_t soc_ctrl;
+    soc_ctrl.base_addr = mmio_region_from_addr((uintptr_t)SOC_CTRL_START_ADDRESS);
+    uint32_t freq_hz = soc_ctrl_get_frequency(&soc_ctrl);
+
+    return (float)cycles/((float)freq_hz/1000000);
 }
--- sw/device/lib/sdk/timer/timer_sdk.h
+++ sw/device/lib/sdk/timer/timer_sdk.h
@@ -6,8 +6,6 @@
 // File: timer_sdk.h
 // Authors: Michele Caon, Luigi Giuffrida, Francesco Poluzzi
 // Date: 23/07/2024
-// Authors: Michele Caon, Luigi Giuffrida, Francesco Poluzzi
-// Date: 23/07/2024
 // Description: Execution time measurements utilities
 
 #ifndef TIMER_SDK_H_
@@ -29,7 +27,6 @@
 /******************************/
 
 // Timer value
-extern uint32_t timer_value;
 extern uint32_t hw_timer_value;
 
 extern rv_timer_t timer;
@@ -43,21 +40,26 @@ extern rv_timer_t timer;
  * 
 * @return int64_t Current value of the HW timer
  */
-uint32_t hw_timer_get_cycles();
+uint32_t timer_get_cycles();
 
 /**
- * @brief Start the HW timer
+ * @brief Start the timer
  * 
  */
-void hw_timer_start();
+void timer_start();
+
+/**
+ * @brief Stop and reset the timer to 0
+ * 
+ */
+void timer_reset();
 
 /**
  * @brief Stop the HW timer
  * 
  * @return int64_t Elapsed time in clock cycles
  */
-uint32_t hw_timer_stop();
-
+uint32_t timer_stop();
 
 /**
  * @brief Initialize the timer for counting clock cycles
@@ -65,40 +67,6 @@ uint32_t hw_timer_stop();
  * 
  */
 void timer_cycles_init();
-
-/**
- * @brief Initialize the timer for counting microseconds
- * 
- */
-void  timer_microseconds_init();
-
-/**
- * @brief Get the current value of the MCYCLE CSR
- * 
- * @return int64_t Current value of the MCYCLE CSR
- */
-inline uint32_t timer_get_cycles() {
-    uint32_t cycle_count;
-    CSR_READ(CSR_REG_MCYCLE, &cycle_count);
-    return cycle_count;
-}
-
-/**
- * @brief Start the timer
- * 
- */
-inline void timer_start() {
-    hw_timer_start();
-}
-
-/**
- * @brief Stop the timer
- * 
- * @return int64_t Elapsed time in clock cycles
- */
-inline uint32_t timer_stop() {
-    return hw_timer_stop();
-}
 
 /**
  * @brief Enable the timer IRQ
@@ -112,6 +80,7 @@ void timer_irq_clear();
 
 /**
  * @brief Arms the timer to go off once the counter value is greater than or equal to threshold
+ * and starts the timer
  */
 void timer_arm_start(uint32_t threshold);
 
@@ -121,11 +90,10 @@ void timer_arm_start(uint32_t threshold);
 void timer_arm_stop();
 
 /**
- * @brief Wait for a certain amount of milliseconds. Not precise with small
- * numbers of milliseconds. 
- * You need to enable timer interrupts with enable_timer_interrupt() before using this function
+ * @brief Set the timer to go off once the counter value is greater than or equal to threshold,
+ * without starting the timer
  */
-void timer_wait_ms(uint32_t ms);
+void timer_arm_set(uint32_t threshold);
 
 /**
  * @brief Enable the timer machine-level interrupts for X-Heep
@@ -133,15 +101,21 @@ void timer_wait_ms(uint32_t ms);
 void enable_timer_interrupt();
 
 /**
- * @brief Wait for a certain amount of milliseconds. Not precise with small
- * numbers of milliseconds. 
+ * @brief Wait for a certain amount of microseconds. 
  * You need to enable timer interrupts with enable_timer_interrupt() before using this function
  */
-void timer_wait_ms(uint32_t ms);
+void timer_wait_us(uint32_t ms);
 
 /**
  * @brief Enable the timer machine-level interrupts for X-Heep
  */
 void enable_timer_interrupt();
+
+/**
+ * @brief Get the time taken to execute a certain number of cycles
+ * 
+ * @return float value representing the time taken in microseconds
+ */
+float get_time(uint32_t cycles);
 
 #endif /* TIMER_SDK_H_ */
