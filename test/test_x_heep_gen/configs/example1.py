31bba355a01cfa267828fb2e14f759e55fc2859a
Pacsort17 <roman.paccaud@epfl.ch>
2025-04-14 22:50:08 +0200

    fix: forgot to push example

--- test/test_x_heep_gen/configs/example1.py
+++ test/test_x_heep_gen/configs/example1.py
@@ -1,6 +1,31 @@
 from x_heep_gen.linker_section import LinkerSection
 from x_heep_gen.system import XHeep, BusType
-from x_heep_gen.peripherals import PeripheralName, PeripheralDomain
+from x_heep_gen.peripherals.base_peripherals import (
+    BasePeripheralDomain,
+    SOC_ctrl,
+    Bootrom,
+    SPI_flash,
+    SPI_memio,
+    DMA,
+    Power_manager,
+    RV_timer_ao,
+    Fast_intr_ctrl,
+    Ext_peripheral,
+    Pad_control,
+    GPIO_ao,
+    UART,
+)
+from x_heep_gen.peripherals.user_peripherals import (
+    UserPeripheralDomain,
+    RV_plic,
+    SPI_host,
+    GPIO,
+    I2C,
+    RV_timer,
+    SPI2,
+    PDM2PCM,
+    I2S,
+)
 
 
 def config():
@@ -11,21 +36,27 @@ def config():
     system.add_linker_section(LinkerSection.by_size("code", 0, 0x00000C800))
     system.add_linker_section(LinkerSection("data", 0x00000C800, None))
 
-    # Peripherals
-    peripheral_domain = system.configure_peripherals()
-    # Only one input, offset of the peripheral in peripheral domain. If set to None, the offset will be automatically computed in system.validate()
-    peripheral_domain.configure_rv_plic(0x00000000)
-    peripheral_domain.configure_spi_host(0x00010000)
-    peripheral_domain.configure_gpio(0x00020000)
-    peripheral_domain.configure_i2c(0x00030000)
-    peripheral_domain.configure_rv_timer(0x00040000)
-    peripheral_domain.configure_spi2(0x00050000)
-    peripheral_domain.configure_pdm2pcm(0x00060000)
-    peripheral_domain.configure_i2s(0x00070000)
+    # Peripheral domains initialization
+    base_peripheral_domain = BasePeripheralDomain()
+    user_peripheral_domain = UserPeripheralDomain()
 
-    peripheral_domain.remove_peripheral(
-        PeripheralName.PDM2PCM
-    )  # Not included in mcu_cfg.hjson
+    # Base peripherals. All base peripherals must be added. They can be either added with "add_peripheral" or "add_missing_peripherals" (adds all base peripherals).
+    base_peripheral_domain.add_missing_peripherals()
+
+    # User peripherals. All are optional. They must be added with "add_peripheral".
+    user_peripheral_domain.add_peripheral(RV_plic(0x00000000))
+    user_peripheral_domain.add_peripheral(SPI_host(0x00010000))
+    user_peripheral_domain.add_peripheral(GPIO(0x00020000))
+    user_peripheral_domain.add_peripheral(I2C(0x00030000))
+    user_peripheral_domain.add_peripheral(RV_timer(0x00040000))
+    user_peripheral_domain.add_peripheral(SPI2(0x00050000))
+    user_peripheral_domain.add_peripheral(
+        I2S()
+    )  # If no address is provided, the peripheral will be automatically added where there is space.
+
+    # Add the peripheral domains to the system
+    system.add_peripheral_domain(base_peripheral_domain)
+    system.add_peripheral_domain(user_peripheral_domain)
 
     # Here the system is build,
     # The missing gaps are filled, like the missing end address of the data section.
