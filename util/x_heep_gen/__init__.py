a43a2e26689b7e32e2a714be7e20f57ece8550c4
Pacsort17 <roman.paccaud@epfl.ch>
2025-04-23 23:14:13 +0200

    doc: add peripherals to __init__ file to add them to doc

--- util/x_heep_gen/__init__.py
+++ util/x_heep_gen/__init__.py
@@ -2,3 +2,6 @@ from .system import XHeep
 from . import system
 from . import load_config
 from . import ram_bank
+from .peripherals import abstractions
+from .peripherals import base_peripherals
+from .peripherals import user_peripherals
