9850f9c8f5cc48a9852c159150c4eebb41244a0a
LucaCaba <luca.caballerocusin@epfl.ch>
2025-04-25 10:43:11 +0200

    Interrupts

--- sw/applications/0_DOOM/x_buttons.c
+++ sw/applications/0_DOOM/x_buttons.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -6,114 +5,146 @@
 #include "doomkeys.h"
 #include "d_event.h"
 #include "i_system.h"
+#include "pad_control.h"
+#include "pad_control_regs.h"
+#include "core_v_mini_mcu.h"
 
+#include "csr.h"
+#include "rv_plic.h"
 #include "gpio.h"
 
+// --- Private defines ---
+#define GPIO_TB_IN_UP     10
+#define GPIO_TB_IN_DOWN  9
+#define GPIO_TB_IN_LEFT  11
+#define GPIO_TB_IN_RIGHT 12
+#define GPIO_TB_IN_A     13
+#define GPIO_TB_IN_B     14
 
+// --- Private variables ---
+static const uint32_t gpio_tb[6] = {
+    GPIO_TB_IN_UP, GPIO_TB_IN_DOWN, GPIO_TB_IN_LEFT,
+    GPIO_TB_IN_RIGHT, GPIO_TB_IN_A, GPIO_TB_IN_B
+};
 
-//private defines:
-#define GPIO_TB_IN_UP  9 //RASP PI 15
-#define GPIO_TB_IN_DOWN 10 //RASP PI 13
-#define GPIO_TB_IN_LEFT 11 //RASP PI 11
-#define GPIO_TB_IN_RIGHT 12 //RASP PI 8
-#define GPIO_TB_IN_A 13 //AR2
-#define GPIO_TB_IN_B 14 //AR3
+static const char button_map[6] = {
+    KEY_UPARROW, KEY_DOWNARROW, KEY_LEFTARROW, KEY_RIGHTARROW, 'h', 'j'
+};
 
-uint32_t gpio_tb[6] = {GPIO_TB_IN_UP, GPIO_TB_IN_DOWN, GPIO_TB_IN_LEFT, GPIO_TB_IN_RIGHT, GPIO_TB_IN_A, GPIO_TB_IN_B};
+static bool button_prev_state[6] = {1, 1, 1, 1, 1, 1};
 
+// --- Forward declarations ---
+static void x_button_common_handler(int button_idx);
 
-//private functions
-int initGPIO(uint32_t gpio_tb);
-int X_ButtonStateRaw(int id);
-
-//private variables
-
-/* Defined Buttons are:
- * 0: UP
- * 1: DOWN
- * 2: LEFT
- * 3: RIGHT
- * 4: SHOOT
- * 5: ACTIVATE (for example lift doors)
- */
-bool button_prev_state[6] = {1,1,1,1,1,1,1}; //inverse logic ! ! !
-bool button_state[6];
-bool button_posedge[6];
-bool button_negedge[6];
-
-
-char button_map[] = {KEY_UPARROW, KEY_DOWNARROW, KEY_LEFTARROW, KEY_RIGHTARROW, 'h', 'j'};
-
-//public function definitions
+// --- Public functions ---
 
 void X_ButtonsInit(void)
 {
-    initGPIO(GPIO_TB_IN_UP);
-    initGPIO(GPIO_TB_IN_DOWN);
-    initGPIO(GPIO_TB_IN_LEFT);
-    initGPIO(GPIO_TB_IN_RIGHT);
-    initGPIO(GPIO_TB_IN_A);
-    initGPIO(GPIO_TB_IN_B);
-}
+    printf("Buttons init\n");
 
-//private function definitions
+    pad_control_t pad_control;
+    pad_control.base_addr = mmio_region_from_addr((uintptr_t)PAD_CONTROL_START_ADDRESS);
 
-int initGPIO(uint32_t gpio_tb)
-{
-    gpio_result_t gpio_res;
-
-    gpio_cfg_t cfg_in = {
-        .pin = gpio_tb,
-        .mode = GpioModeIn,
-        .en_input_sampling = true,
-        .en_intr = true,
-        .intr_type = GpioIntrEdgeFalling
-    };
-
-    gpio_res = gpio_config(cfg_in);
-    if (gpio_res != GpioOk) {
-        PRINTF("Failed\n;");
-        return -1;
+    if (plic_Init() != kPlicOk) {
+        printf("PLIC init failed\n");
+        return;
     }
+
+    for (int i = 0; i < 6; i++) {
+        gpio_cfg_t cfg = {
+            .pin = gpio_tb[i],
+            .mode = GpioModeIn,
+            .en_input_sampling = true,
+            .en_intr = true,
+            .intr_type = GpioIntrEdgeRising
+        };
+
+        if (gpio_config(cfg) != GpioOk) {
+            printf("GPIO config failed for pin %d\n", gpio_tb[i]);
+        }
+
+        plic_irq_set_priority(gpio_tb[i], 1);
+        plic_irq_set_enabled(gpio_tb[i], kPlicToggleEnabled);
+    }
+
+    gpio_assign_irq_handler(GPIO_TB_IN_UP,    button_up_handler);
+    gpio_assign_irq_handler(GPIO_TB_IN_DOWN,  button_down_handler);
+    gpio_assign_irq_handler(GPIO_TB_IN_LEFT,  button_left_handler);
+    gpio_assign_irq_handler(GPIO_TB_IN_RIGHT, button_right_handler);
+    gpio_assign_irq_handler(GPIO_TB_IN_A,     button_a_handler);
+    gpio_assign_irq_handler(GPIO_TB_IN_B,     button_b_handler);
+
+    CSR_SET_BITS(CSR_REG_MSTATUS, 0x8);
+    CSR_SET_BITS(CSR_REG_MIE, (1 << 11));
 }
+
+// --- Private helper ---
+
+static void x_button_common_handler(int idx)
+{
+    static event_t event;
+
+    // Send KeyDown event
+    event.type = ev_keydown;
+    event.data1 = button_map[idx];
+    event.data2 = 0;
+    event.data3 = 0;
+    D_PostEvent(&event);
+
+    // Immediately send KeyUp event
+    event.type = ev_keyup;
+    D_PostEvent(&event);
+
+    printf("Button %d pressed (rising edge)\n", idx);
+}
+
+// --- Interrupt handlers ---
+
+void button_up_handler(void) {
+    x_button_common_handler(0);
+    gpio_intr_clear_stat(GPIO_TB_IN_UP);
+}
+
+void button_down_handler(void) {
+    x_button_common_handler(1);
+    gpio_intr_clear_stat(GPIO_TB_IN_DOWN);
+}
+
+void button_left_handler(void) {
+    x_button_common_handler(2);
+    gpio_intr_clear_stat(GPIO_TB_IN_LEFT);
+}
+
+void button_right_handler(void) {
+    x_button_common_handler(3);
+    gpio_intr_clear_stat(GPIO_TB_IN_RIGHT);
+}
+
+void button_a_handler(void) {
+    x_button_common_handler(4);
+    gpio_intr_clear_stat(GPIO_TB_IN_A);
+}
+
+void button_b_handler(void) {
+    x_button_common_handler(5);
+    gpio_intr_clear_stat(GPIO_TB_IN_B);
+}
+
+// --- Optional helpers ---
 
 int X_ButtonStateRaw(int id)
 {
     bool state;
     gpio_read(gpio_tb[id], &state);
-    return !state; //inverse logic ! ! !
-}
-
-void X_ReadButtons(void)
-{
-    static event_t event;
-
-    for(uint8_t iter=0; iter<6; iter++)
-    {
-        gpio_read(gpio_tb[iter], &button_state[iter]);
-        button_posedge[iter] = !button_state[iter] && button_prev_state[iter];
-        button_negedge[iter] = button_state[iter] && !button_prev_state[iter];
-
-        if(button_posedge[iter]){
-            event.type = ev_keydown;
-            event.data1 = button_map[iter];
-            event.data2 = 0;
-            event.data3 = 0;
-            D_PostEvent(&event);
-        } else if(button_negedge[iter]){
-            event.type = ev_keyup;
-            event.data1 = button_map[iter];
-            event.data2 = 0;
-            event.data3 = 0;
-            D_PostEvent(&event);
-        }
-
-        button_prev_state[iter] = button_state[iter];
-    }
-        
+    return !state;
 }
 
 int X_ButtonState(int num)
 {
-    return !button_prev_state[num]; //inverse logic ! ! !
+    return !button_prev_state[num];
+}
+
+void X_ReadButtons(void)
+{
+    // Not needed anymore, handled by interrupts
 }
--- sw/applications/0_DOOM/x_buttons.h
+++ sw/applications/0_DOOM/x_buttons.h
@@ -1,15 +1,34 @@
 #ifndef X_BUTTONS_H
 #define X_BUTTONS_H
 
-// Include any necessary headers here
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-// Define any constants or macros here
-
-// Declare any global variables here
-
-// Declare any function prototypes here
+// Initialize all buttons (configure GPIO, setup interrupts)
 void X_ButtonsInit(void);
+
+// (Optional) Manually read buttons (not used if interrupts are working)
 void X_ReadButtons(void);
+
+// Get the current state of a button (pressed or not)
+// 0 = pressed, 1 = released (inverse logic!)
 int X_ButtonState(int num);
+
+// Get the raw GPIO read of a button (optional)
+int X_ButtonStateRaw(int id);
+
+void button_up_handler(void);
+void button_down_handler(void);
+void button_left_handler(void);
+void button_right_handler(void);
+void button_a_handler(void);
+void button_b_handler(void);
+
+//static void button_post_event(int button_idx);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // X_BUTTONS_H
