23bc92838579033b1085e45ecbafa950d3f7fc48
JeremieMoullet <jeremie.moullet@epfl.ch>
2025-05-07 19:07:00 +0200

    fix of copyright

--- sw/applications/example_CIC/main.c
+++ sw/applications/example_CIC/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 EPFL
+ * Copyright 2025 EPFL
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Author: Pierre Guillod <pierre.guillod@epfl.ch>, EPFL, STI-SEL
+ * Author: Jérémie Moullet <jeremie.moullet@epfl.ch>, EPFL, STI-SEL
  */
 
 #include <stdio.h>
