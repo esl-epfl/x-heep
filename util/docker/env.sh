0ed98c1fd22cfba96c65e9ce1c5421bc44bc6519
Michele Caon <st.miky77@gmail.com>
2025-10-20 17:15:54 +0200

    Update Docker build scripts
    - add missing waiver in cv32e40p

--- util/docker/env.sh
+++ util/docker/env.sh
@@ -5,9 +5,9 @@ source /opt/conda/etc/profile.d/conda.sh
 conda activate core-v-mini-mcu
 
 # Aliases to initialize SW build environment
-alias init_gcc='export RISCV=/tools/riscv; COMPILER=gcc; export COMPILER_PREFIX=riscv32-unknown-; export ARCH=rv32imc_zicsr'
-alias init_corev='export RISCV=/tools/riscv-corev; COMPILER=gcc; export COMPILER_PREFIX=riscv32-corev-; export ARCH=rv32imc_zicsr'
-alias init_clang='export RISCV=/tools/riscv-clang; COMPILER=clang; export COMPILER_PREFIX=riscv32-unknown-; export ARCH=rv32imc'
+alias init_gcc='export RISCV=/tools/riscv; export COMPILER=gcc; export COMPILER_PREFIX=riscv32-unknown-; export ARCH=rv32imc_zicsr'
+alias init_clang='export RISCV=/tools/riscv-clang; export COMPILER=clang; export COMPILER_PREFIX=riscv32-unknown-; export ARCH=rv32imc'
+alias init_corev='export RISCV=/tools/riscv-corev; export COMPILER=gcc; export COMPILER_PREFIX=riscv32-corev-; export ARCH=rv32imc_zicsr_xcvhwlp_xcvmem_xcvmac_xcvbi_xcvalu_xcvsimd_xcvbitmanip'
 
 # Default to GCC toolchain
 init_gcc
